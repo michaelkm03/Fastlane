d32221f000bcf5e52637e3b07c5ebf5489835da4
David Keegan <git@davidkeegan.com>
2014-01-07 22:17:41 -0800

    - Starting to add the new post view controller. - Added VConstants.h to hold constant values.

---
+++ victorious/victorious/VConstants.h
@@ -0,0 +1,10 @@
+//
+//  VConstants.h
+//  victorious
+//
+//  Created by David Keegan on 1/7/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+static NSUInteger const VConstantsMessageLength = 140;
+static NSUInteger const VConstantsForumTitleLength = 65;
