1996a6422cb2bb82f221023456be2c526261e0d6
Gary Philipp <garyphilipp@Gary-Development.local>
2014-01-09 19:27:22 -0800

    VCommentCell hooked up

--- victorious/victorious/UI/Streams Subview/VCommentCell.m
+++ victorious/victorious/UI/Streams Subview/VCommentCell.m
@@ -7,15 +7,19 @@
 //
 
 #import "VCommentCell.h"
-#import "VComment.h"
 #import "UIImageView+AFNetworking.h"
-#import "VMessage.h"
+#import "VComment+RestKit.h"
+#import "VMessage+RestKit.h"
+#import "VMedia+RestKit.h"
+#import "VUser+RestKit.h"
+#import "NSDate+timeSince.h"
 
 @interface VCommentCell()
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *avatarImageView;
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *mediaPreview;
 @end
 
 @implementation VCommentCell
@@ -37,7 +41,14 @@
     else if([commentOrMessage isKindOfClass:[VMessage class]])
     {
         VMessage *message = (VMessage *)self.commentOrMessage;
+        
+        self.dateLabel.text = [message.postedAt timeSince];
+        [self.avatarImageView setImageWithURL:[NSURL URLWithString:message.user.pictureUrl]
+                             placeholderImage:[UIImage imageNamed:@"profile_thumb"]];
+        self.usernameLabel.text = message.user.name;
         self.messageLabel.text = message.text;
+        [self.mediaPreview setImageWithURL:[NSURL URLWithString:message.media.previewImage]
+                          placeholderImage:[UIImage imageNamed:@"MenuVideos"]];
     }
 }
 
