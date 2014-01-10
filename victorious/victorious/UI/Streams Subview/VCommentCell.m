032bf1beb94ed873f87424e45488a798f70cc751
Gary Philipp <garyphilipp@Gary-Development.local>
2014-01-10 13:28:28 -0800

    CommentCell WIP

--- victorious/victorious/UI/Streams Subview/VCommentCell.m
+++ victorious/victorious/UI/Streams Subview/VCommentCell.m
@@ -6,7 +6,10 @@
 //  Copyright (c) 2013 Victorious, Inc. All rights reserved.
 //
 
+@import MediaPlayer;
+
 #import "VCommentCell.h"
+#import "VConstants.h"
 #import "UIImageView+AFNetworking.h"
 #import "VComment+RestKit.h"
 #import "VMessage+RestKit.h"
@@ -20,6 +23,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *messageLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *mediaPreview;
+@property (weak, nonatomic) IBOutlet UIView* movieView;
+
+@property (nonatomic, strong) MPMoviePlayerController* mpController;
 @end
 
 @implementation VCommentCell
@@ -47,9 +53,36 @@
                              placeholderImage:[UIImage imageNamed:@"profile_thumb"]];
         self.usernameLabel.text = message.user.name;
         self.messageLabel.text = message.text;
-        [self.mediaPreview setImageWithURL:[NSURL URLWithString:message.media.previewImage]
-                          placeholderImage:[UIImage imageNamed:@"MenuVideos"]];
+        
+        if (message.media.mediaUrl)
+        {
+            if ([message.media.mediaType isEqualToString:VConstantsMediaTypeVideo])
+            {
+                self.mediaPreview.hidden = NO;
+                self.movieView.hidden = YES;
+
+                self.mpController = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL URLWithString:message.media.mediaUrl]];
+                [self.movieView addSubview:self.mpController.view];
+            }
+            else
+            {
+                self.mediaPreview.hidden = NO;
+                self.movieView.hidden = YES;
+                [self.mediaPreview setImageWithURL:[NSURL URLWithString:message.media.previewImage]
+                                  placeholderImage:[UIImage imageNamed:@"MenuVideos"]];
+            }
+        }
+        else
+        {
+            self.mediaPreview.hidden = YES;
+            self.movieView.hidden = YES;
+        }
     }
+}
+
+- (IBAction)displayVideoMedia:(id)sender
+{
+    
 }
 
 @end
