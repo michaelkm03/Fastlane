60407b5602a4b63f4b5f8d5b24cf16a1295a6e1c
Will Long <will@getvictorious.com>
2014-01-11 17:27:56 -0800

    Adding play button to project, switching polls to use play button image, modifying VOM+Users

--- victorious/victorious/Managers/VObjectManager+Users.m
+++ victorious/victorious/Managers/VObjectManager+Users.m
@@ -22,44 +22,43 @@
                               withSuccessBlock:(SuccessBlock)success
                                      failBlock:(FailBlock)fail
 {
-
-//    return nil;
-
-//    DISPATCH_QUEUE_SERIAL
+    //    return nil;
+    //    DISPATCH_QUEUE_SERIAL
     
-    @synchronized(self.userRelationships)
+    NSMutableArray* relationships = [self.userRelationships objectForKey:userId];
+    
+    //There's nothing to add and we're already fetching the object
+    if (!relationshipObject && relationships)
+        return nil;
+    
+    //We're already fetching this ID, just add the object and return
+    if (relationships && [relationships isKindOfClass:[NSMutableArray class]])
     {
-        NSMutableArray* relationships = [self.userRelationships objectForKey:userId];
-
-        //There's nothing to add and we're already fetching the object
-        if (!relationshipObject && relationships)
-            return nil;
-        
-        //We're already fetching this ID, just add the object and return
-        if (relationships && [relationships isKindOfClass:[NSMutableArray class]])
-        {
-            VLog(@"Found relationships: %@", relationships);
-            [relationships addObject:relationshipObject];
-            [self.userRelationships setObject:relationships forKey:userId];
-            
-            return nil;
-        }
-        
-        relationships = [[NSMutableArray alloc] init];
+        VLog(@"Found relationships: %@", relationships);
         [relationships addObject:relationshipObject];
         [self.userRelationships setObject:relationships forKey:userId];
-        VLog(@"Added object: %@.  All relationships: %@ ", [relationshipObject class], self.userRelationships);
+        
+        return nil;
     }
-
+    
+    relationships = [[NSMutableArray alloc] init];
+    [relationships addObject:relationshipObject];
+    [self.userRelationships setObject:relationships forKey:userId];
+    VLog(@"Added object: %@.  All relationships: %@ ", [relationshipObject class], self.userRelationships);
+    
     NSString* path = userId ? [NSString stringWithFormat:@"/api/userinfo/fetch/%@", userId] : @"/api/userinfo/fetch";
     
     SuccessBlock fullSuccess = ^(NSArray* resultObjects)
     {
         [self addRelationshipsForUsers:resultObjects];
         if (success)
+        {
             success(resultObjects);
+        }
+        VUser* user = [resultObjects firstObject];
+        VLog(@"posted sequences: %@", user.postedSequences);
     };
-    
+
     return [self GET:path
               object:nil
           parameters:nil
@@ -70,29 +69,26 @@
 
 - (void)addRelationshipsForUsers:(NSArray*)users
 {
-    @synchronized(self.userRelationships)
+    for (VUser* user in users)
     {
-        for (VUser* user in users)
+        NSArray* relationships = [self.userRelationships objectForKey:user.remoteId];
+        for (id relationshipObject in relationships)
         {
-            NSArray* relationships = [self.userRelationships objectForKey:user.remoteId];
-            for (id relationshipObject in relationships)
-            {
-                if ([relationshipObject isKindOfClass:[VComment class]])
-                    ((VComment*)relationshipObject).user = user;
-                
-                else if ([relationshipObject isKindOfClass:[VMessage class]])
-                    ((VMessage*)relationshipObject).user = user;
-                
-                else if ([relationshipObject isKindOfClass:[VSequence class]])
-                    ((VSequence*)relationshipObject).user = user;
-                
-                else if ([relationshipObject isKindOfClass:[VConversation class]])
-                    [((VConversation*)relationshipObject) addUsersObject:user];
-            }
+            if ([relationshipObject isKindOfClass:[VComment class]])
+                ((VComment*)relationshipObject).user = user;
             
-            [self.userRelationships removeObjectForKey:user.remoteId];
+            else if ([relationshipObject isKindOfClass:[VMessage class]])
+                ((VMessage*)relationshipObject).user = user;
+            
+            else if ([relationshipObject isKindOfClass:[VSequence class]])
+                ((VSequence*)relationshipObject).user = user;
+            
+            else if ([relationshipObject isKindOfClass:[VConversation class]])
+                [((VConversation*)relationshipObject) addUsersObject:user];
         }
     }
+    [self.managedObjectStore.persistentStoreManagedObjectContext save:nil];
+
 }
 
 @end
