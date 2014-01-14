7293e35eb5cc5326473ffb599acfe91a1ab7b18a
Gary Philipp <garyphilipp@Gary-Development.local>
2014-01-13 16:39:19 -0800

    performFetch should be performed on context’s context

--- victorious/victorious/UI/VMessageViewController.m
+++ victorious/victorious/UI/VMessageViewController.m
@@ -62,11 +62,6 @@ const   CGFloat     kMessageRowHeight           =   80;
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return self.messages.count;
