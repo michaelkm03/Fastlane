b9fae790eade337649a03f3d3763d3a6e178fa97
David Keegan <git@davidkeegan.com>
2014-01-15 21:13:03 -0800

    - Only the left avatar comment cell is used now - Fixed the height of the comment cells for media or not

--- victorious/victorious/UI/Streams Subview/VStreamsCommentsController.m
+++ victorious/victorious/UI/Streams Subview/VStreamsCommentsController.m
@@ -291,20 +291,20 @@ static NSString* CommentCache = @"CommentCache";
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     VComment *comment = [self.fetchedResultsController objectAtIndexPath:indexPath];
-    return comment.mediaUrl ? kCommentRowWithMediaHeight : kCommentRowHeight;
+    return [comment.mediaUrl length] ? kCommentRowWithMediaHeight : kCommentRowHeight;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = nil;
-    VComment *comment = [self.fetchedResultsController objectAtIndexPath:indexPath];
-    if([comment.user isEqualToUser:[VObjectManager sharedManager].mainUser])
-    {
-        cell = [self.tableView dequeueReusableCellWithIdentifier:kOtherCommentCellIdentifier forIndexPath:indexPath];
-    }else
-    {
+//    VComment *comment = [self.fetchedResultsController objectAtIndexPath:indexPath];
+//    if([comment.user isEqualToUser:[VObjectManager sharedManager].mainUser])
+//    {
+//        cell = [self.tableView dequeueReusableCellWithIdentifier:kOtherCommentCellIdentifier forIndexPath:indexPath];
+//    }else
+//    {
         cell = [self.tableView dequeueReusableCellWithIdentifier:kCommentCellIdentifier forIndexPath:indexPath];
-    }
+//    }
     [self configureCell:cell atIndexPath:indexPath];
     
     return cell;
