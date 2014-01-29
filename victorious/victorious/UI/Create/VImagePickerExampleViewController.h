068060b9ccf1ff9ba9b8bdef50f973ed3735f81b
Will Long <will@getvictorious.com>
2014-01-29 14:08:27 -0800

    Adding VImagePickerController example

---
+++ victorious/victorious/UI/Create/VImagePickerExampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  VImagePickerExampleViewController.h
+//  victorious
+//
+//  Created by Will Long on 1/29/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VImagePickerExampleViewController : UIViewController
+
+@end
