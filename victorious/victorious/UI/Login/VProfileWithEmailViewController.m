91e28f2e61f1df42f5dbe1f8bb617d5267d45ac2
goWorld <goworld@goWorld.local>
2014-01-28 17:38:08 -0800

    Worked on Profile from Email View Controller

--- victorious/victorious/UI/Login/VProfileWithEmailViewController.m
+++ victorious/victorious/UI/Login/VProfileWithEmailViewController.m
@@ -7,14 +7,19 @@
 //
 
 #import "VProfileWithEmailViewController.h"
+#import "VConstants.h"
 
 @import CoreLocation;
 
-@interface VProfileWithEmailViewController ()   <CLLocationManagerDelegate>
+@interface VProfileWithEmailViewController ()   <UITextFieldDelegate, UITextViewDelegate, CLLocationManagerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (nonatomic, strong) CLLocationManager*    locationManager;
 @property (nonatomic, strong) CLGeocoder*           geoCoder;
 
-@property (nonatomic, strong) IBOutlet    UITextField*    locationTextField;
+@property (nonatomic, weak) IBOutlet    UIImageView*    profileImageView;
+@property (nonatomic, weak) IBOutlet    UIButton*       cameraButton;
+@property (nonatomic, weak) IBOutlet    UITextField*    usernameTextField;
+@property (nonatomic, weak) IBOutlet    UITextField*    locationTextField;
+@property (nonatomic, weak) IBOutlet    UITextView*     taglineTextView;
 @end
 
 @implementation VProfileWithEmailViewController
@@ -29,6 +34,51 @@
         self.locationManager.delegate = self;
         [self.locationManager startMonitoringSignificantLocationChanges];
     }
+    
+    self.usernameTextField.delegate = self;
+    self.locationTextField.delegate = self;
+    self.taglineTextView.delegate = self;
+}
+
+#pragma mark - Actions
+
+- (IBAction)takePicture:(id)sender
+{
+    UIImagePickerController*    picker = [[UIImagePickerController alloc] init];
+
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        picker.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
+    }
+    else
+    {
+        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+ 
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    if ([textField isEqual:self.usernameTextField])
+        [self.locationTextField becomeFirstResponder];
+    else if ([textField isEqual:self.locationTextField])
+        [self.taglineTextView becomeFirstResponder];
+    else
+        [self.taglineTextView resignFirstResponder];
+    
+    return YES;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [[self view] endEditing:YES];
 }
 
 #pragma mark - CCLocationManagerDelegate
@@ -48,4 +98,25 @@
         self.geoCoder = nil;
     }];
 }
+
+#pragma mark - UIImagePickerControllerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage* imageToSave = (UIImage *)info[UIImagePickerControllerEditedImage] ?: (UIImage *)info[UIImagePickerControllerOriginalImage];
+    self.profileImageView.image = imageToSave;
+    
+    NSString*   mediaType   =   nil;
+    NSData*     media = UIImagePNGRepresentation(imageToSave);
+    if (media)
+        mediaType = VConstantMediaExtensionPNG;
+
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 @end
