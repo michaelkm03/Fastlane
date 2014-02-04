01279e8380d4b37b10eb3087014e5549bb7a955f
Will Long <will@getvictorious.com>
2014-02-04 10:51:34 -0800

    Converting all API dates to GMT

--- victorious/victorious/Managers/VObjectManager.m
+++ victorious/victorious/Managers/VObjectManager.m
@@ -318,14 +318,17 @@
     [client setDefaultHeader:@"Date" value:currentDate];
 }
 
-- (NSString *)rFC2822DateTimeString {
-    
+- (NSString *)rFC2822DateTimeString
+{
     static NSDateFormatter *sRFC2822DateFormatter = nil;
     
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         sRFC2822DateFormatter = [[NSDateFormatter alloc] init];
         sRFC2822DateFormatter.dateFormat = @"EEE, dd MMM yyyy HH:mm:ss Z"; //RFC2822-Format
+        
+        NSTimeZone *gmt = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+        [sRFC2822DateFormatter setTimeZone:gmt];
     });
     
     return [sRFC2822DateFormatter stringFromDate:[NSDate date]];
