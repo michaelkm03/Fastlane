7e08ccb265addc4b7e6f028392cd9ebabab17644
Will Long <will@getvictorious.com>
2014-02-12 14:00:23 -0800

    Fixing logout crash

--- victorious/victorious/Managers/VObjectManager+Login.m
+++ victorious/victorious/Managers/VObjectManager+Login.m
@@ -194,7 +194,7 @@ NSString *kLoggedInChangedNotification = @"LoggedInChangedNotification";
         //Warning: Sometimes empty payloads will appear as Array objects. Use the following line at your own risk.
         //NSDictionary* payload = fullResponse[@"payload"];
         NSManagedObjectContext* context = self.managedObjectStore.persistentStoreManagedObjectContext;
-        [context deleteObject:[self mainUser]];
+        [context deleteObject:[context objectWithID:[self mainUser].objectID]];
         [context save:nil];
         [[NSNotificationCenter defaultCenter] postNotificationName:kLoggedInChangedNotification object:nil];
     };
