ea2a19248b0c0c9c51680c46338f80f6747d409e
Will Long <will@getvictorious.com>
2014-03-28 11:10:25 -0700

    Fixing youtube video

--- victorious/victorious/UI/Content View/VContentViewController+Videos.m
+++ victorious/victorious/UI/Content View/VContentViewController+Videos.m
@@ -78,7 +78,6 @@
 {
     self.mpPlayerContainmentView.frame = CGRectMake(0, 0, 0, 0);
     self.mpPlayerContainmentView.hidden = NO;
-    self.remixButton.hidden = NO;
     
     CGFloat duration = .5f;
     
@@ -92,6 +91,7 @@
                      completion:^(BOOL finished)
      {
          [self.mpController play];
+         self.remixButton.hidden = NO;
      }];
 }
 
