924d95df4f527d00c8cbf5c88031bb3102d485b2
Will Long <will@getvictorious.com>
2014-05-12 11:13:33 -0700

    Commenting out next page until the major bug is fixed

--- victorious/victorious/UI/Base Classes/VStreamTableViewController.m
+++ victorious/victorious/UI/Base Classes/VStreamTableViewController.m
@@ -305,24 +305,25 @@
 
 - (void)loadNextPageAction
 {
-    RKManagedObjectRequestOperation* operation = [[VObjectManager sharedManager] loadNextPageOfSequenceFilter:[self currentFilter]
-                                             successBlock:^(NSOperation* operation, id fullResponse, NSArray* resultObjects)
-     {
-         [self.bottomRefreshIndicator stopAnimating];
-         self.fetchedResultsController.delegate = self;
-         [self performFetch];
-     }
-                                                failBlock:^(NSOperation* operation, NSError* error)
-     {
-         [self.bottomRefreshIndicator stopAnimating];
-         self.fetchedResultsController.delegate = self ;
-     }];
-    
-    if (operation)
-    {
-        self.fetchedResultsController.delegate = nil;
-        [self.bottomRefreshIndicator startAnimating];
-    }
+#warning The next page action will cause the tableview to sometimes freak out. Use at your own risk.
+//    RKManagedObjectRequestOperation* operation = [[VObjectManager sharedManager] loadNextPageOfSequenceFilter:[self currentFilter]
+//                                             successBlock:^(NSOperation* operation, id fullResponse, NSArray* resultObjects)
+//     {
+//         [self.bottomRefreshIndicator stopAnimating];
+//         self.fetchedResultsController.delegate = self;
+//         [self performFetch];
+//     }
+//                                                failBlock:^(NSOperation* operation, NSError* error)
+//     {
+//         [self.bottomRefreshIndicator stopAnimating];
+//         self.fetchedResultsController.delegate = self ;
+//     }];
+//    
+//    if (operation)
+//    {
+//        self.fetchedResultsController.delegate = nil;
+//        [self.bottomRefreshIndicator startAnimating];
+//    }
 }
 
 #pragma mark - Predicates
