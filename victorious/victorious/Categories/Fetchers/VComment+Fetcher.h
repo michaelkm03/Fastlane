7d70fb254ec95f712790b407eb2529187a0f0df6
Will Long <will@getvictorious.com>
2014-05-13 11:26:12 -0700

    Fixing comments cells iOS-402/373

---
+++ victorious/victorious/Categories/Fetchers/VComment+Fetcher.h
@@ -0,0 +1,16 @@
+//
+//  VComment+Fetcher.h
+//  victorious
+//
+//  Created by Will Long on 5/13/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import "VComment.h"
+
+@interface VComment (Fetcher)
+
+- (BOOL)hasMedia;
+- (NSURL*)previewImageURL;
+
+@end
---
+++ victorious/victorious/Categories/Fetchers/VComment+Fetcher.m
@@ -0,0 +1,30 @@
+//
+//  VComment+Fetcher.m
+//  victorious
+//
+//  Created by Will Long on 5/13/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import "VComment+Fetcher.h"
+#import "NSString+VParseHelp.h"
+
+@implementation VComment (Fetcher)
+
+- (BOOL)hasMedia
+{
+    return (self.mediaUrl && ![self.mediaUrl isEmpty]) || (self.thumbnailUrl && ![self.thumbnailUrl isEmpty]);
+}
+
+- (NSURL*)previewImageURL
+{
+    NSURL* url;
+    if (self.thumbnailUrl && ![self.thumbnailUrl isEmpty])
+        url = [[NSURL alloc] initWithString:self.thumbnailUrl];
+    else if (self.mediaUrl && ![self.mediaUrl isEmpty] && [self.mediaType isEqualToString:VConstantsMediaTypeImage] )
+        url = [[NSURL alloc] initWithString:self.thumbnailUrl];
+
+    return url;
+}
+
+@end
