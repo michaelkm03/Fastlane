005a456687642950509b5cbab123e0a82a6eb170
Josh Hinman <josh@getvictorious.com>
2014-05-13 18:44:33 -0700

    Remove unneccesary layout code

--- victorious/victorious/UI/Content View/VContentViewController+Images.m
+++ victorious/victorious/UI/Content View/VContentViewController+Images.m
@@ -28,23 +28,7 @@
     
     [self.previewImage setImageWithURLRequest:request
                              placeholderImage:self.backgroundImage.image
-                                      success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image)
-     {
-         CGFloat yRatio = 1;
-         CGFloat xRatio = 1;
-         self.previewImage.image = image;
-         if (self.previewImage.image.size.height < self.previewImage.image.size.width)
-         {
-             yRatio = self.previewImage.image.size.height / self.previewImage.image.size.width;
-         }
-         else if (self.previewImage.image.size.height > self.previewImage.image.size.width)
-         {
-             xRatio = self.previewImage.image.size.width / self.previewImage.image.size.height;
-         }
-         CGFloat videoHeight = self.mediaView.frame.size.width * yRatio;
-         CGFloat videoWidth = self.mediaView.frame.size.width * xRatio;
-         self.previewImage.frame = CGRectMake(0, 0, videoWidth, videoHeight);
-     }
+                                      success:nil
                                       failure:nil];
     
     self.previewImage.hidden = NO;
