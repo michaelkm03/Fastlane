12ebe62ad289fc11a613b4c721c482d74c290035
Gary Philipp <garyphilipp@garydevelopment.home>
2014-05-19 15:06:09 -0700

    Fixed a UI refresh from 5 sec to 1 sec, which is more in sync with elsewhere in the app

--- victorious/victorious/UI/VMessageViewController.m
+++ victorious/victorious/UI/VMessageViewController.m
@@ -93,7 +93,7 @@ const   CGFloat     kMessageRowHeight           =   80;
 
 - (void)delayedRefresh
 {
-    double delayInSeconds = 5.0;
+    double delayInSeconds = 1.0f;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void)
                    {
