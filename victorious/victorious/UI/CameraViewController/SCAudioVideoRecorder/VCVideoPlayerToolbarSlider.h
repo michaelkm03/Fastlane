234af944b2e08d65e91a987eee4bb33252f68189
Josh Hinman <josh@getvictorious.com>
2014-06-04 19:15:34 -0700

    Remove download progress indication in video players because it didn't really work

---
+++ victorious/victorious/UI/CameraViewController/SCAudioVideoRecorder/VCVideoPlayerToolbarSlider.h
@@ -0,0 +1,16 @@
+//
+//  VCVideoPlayerToolbarSlider.h
+//  victorious
+//
+//  Created by Josh Hinman on 6/4/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ A subclass of UISlider with a few visual tweaks
+ */
+@interface VCVideoPlayerToolbarSlider : UISlider
+
+@end
