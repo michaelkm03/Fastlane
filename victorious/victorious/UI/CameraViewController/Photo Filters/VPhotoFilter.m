71a815d8edd10dece1d7bca13c7cfbc4b45be7a6
Josh Hinman <josh@getvictorious.com>
2014-07-31 16:22:18 -0700

    Center vignette effect

--- victorious/victorious/UI/CameraViewController/Photo Filters/VPhotoFilter.m
+++ victorious/victorious/UI/CameraViewController/Photo Filters/VPhotoFilter.m
@@ -26,16 +26,20 @@
 
 - (UIImage *)imageByFilteringImage:(UIImage *)sourceImage
 {
+    CGRect canvas = CGRectMake(0, 0, sourceImage.size.width * sourceImage.scale, sourceImage.size.height * sourceImage.scale);
     CIImage *filteredImage = [CIImage v_imageWithUImage:sourceImage];
     for (CIFilter *filter in self.components)
     {
         [filter setValue:filteredImage forKey:kCIInputImageKey];
+        if ([[filter inputKeys] containsObject:kCIInputCenterKey])
+        {
+            [filter setValue:[CIVector vectorWithCGPoint:CGPointMake(CGRectGetMidX(canvas), CGRectGetMidY(canvas))] forKeyPath:kCIInputCenterKey];
+        }
         filteredImage = filter.outputImage;
     }
     
     CIContext *context = [CIContext contextWithOptions:@{}];
-    CGImageRef finishedImage = [context createCGImage:filteredImage
-                                             fromRect:CGRectMake(0, 0, sourceImage.size.width * sourceImage.scale, sourceImage.size.height * sourceImage.scale)];
+    CGImageRef finishedImage = [context createCGImage:filteredImage fromRect:canvas];
     UIImage *retVal = [UIImage imageWithCGImage:finishedImage];
     CGImageRelease(finishedImage);
     return retVal;
