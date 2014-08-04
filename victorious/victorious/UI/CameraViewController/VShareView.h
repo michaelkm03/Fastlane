bfe78bb1a930ff8fbbb21db23485f8d99e868d81
Will Long <will@getvictorious.com>
2014-08-04 11:51:04 -0700

    Share view logic

---
+++ victorious/victorious/UI/CameraViewController/VShareView.h
@@ -0,0 +1,13 @@
+//
+//  VShareView.h
+//  victorious
+//
+//  Created by Will Long on 8/4/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VShareView : UIView
+
+@end
---
+++ victorious/victorious/UI/CameraViewController/VShareView.m
@@ -0,0 +1,31 @@
+//
+//  VShareView.m
+//  victorious
+//
+//  Created by Will Long on 8/4/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import "VShareView.h"
+
+@implementation VShareView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
