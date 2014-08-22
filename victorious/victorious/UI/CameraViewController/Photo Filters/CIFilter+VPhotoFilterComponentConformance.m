887fb19b6038e41b066136e0b86f7c9abc1a14b9
Josh Hinman <josh@getvictorious.com>
2014-08-21 19:14:25 -0700

    Set proper radius on vignette filters

--- victorious/victorious/UI/CameraViewController/Photo Filters/CIFilter+VPhotoFilterComponentConformance.m
+++ victorious/victorious/UI/CameraViewController/Photo Filters/CIFilter+VPhotoFilterComponentConformance.m
@@ -8,6 +8,12 @@
 
 #import "CIFilter+VPhotoFilterComponentConformance.h"
 
+#ifdef __LP64__
+#define CGFLOATVALUE doubleValue
+#else
+#define CGFLOATVALUE floatValue
+#endif
+
 @implementation CIFilter (VPhotoFilterComponentConformance)
 
 - (CIImage *)imageByFilteringImage:(CIImage *)inputImage size:(CGSize)size orientation:(UIImageOrientation)orientation
@@ -16,9 +22,24 @@
     [self setValue:inputImage forKey:kCIInputImageKey];
     if ([[self inputKeys] containsObject:kCIInputCenterKey])
     {
-        [self setValue:[CIVector vectorWithCGPoint:CGPointMake(CGRectGetMidX(canvas), CGRectGetMidY(canvas))] forKeyPath:kCIInputCenterKey];
+        [self setValue:[CIVector vectorWithCGPoint:CGPointMake(CGRectGetMidX(canvas), CGRectGetMidY(canvas))] forKey:kCIInputCenterKey];
     }
-    return self.outputImage;
+    
+    NSNumber *previousRadius = nil;
+    if ([[self name] hasPrefix:@"CIVignette"]) // For vignette filters, convert radius from percent to absolute
+    {
+        previousRadius = [self valueForKey:kCIInputRadiusKey];
+        [self setValue:@(previousRadius.CGFLOATVALUE * MAX(size.width, size.height)) forKeyPath:kCIInputRadiusKey];
+    }
+    
+    CIImage *outputImage = self.outputImage;
+    
+    if (previousRadius)
+    {
+        [self setValue:previousRadius forKeyPath:kCIInputRadiusKey];
+    }
+    
+    return outputImage;
 }
 
 @end
