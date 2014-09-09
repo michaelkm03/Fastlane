4b6ba2d97508fc733799317d22a03767d97a08a6
Michael Sena <michaelsena@getvictorious.com>
2014-09-09 15:48:58 -0700

    Checking in progress

--- victorious/victorious/UI/Common/UICollectionView/Layouts/VCollapsingFlowLayout.m
+++ victorious/victorious/UI/Common/UICollectionView/Layouts/VCollapsingFlowLayout.m
@@ -8,47 +8,60 @@
 
 #import "VCollapsingFlowLayout.h"
 
+typedef NS_ENUM(NSInteger, kVContentViewState)
+{
+    kVContentViewStateFullSize,
+    kVContentViewStateShrinking,
+    kVContentViewStateFloating
+};
+
+@interface VCollapsingFlowLayout ()
+
+@property (nonatomic, assign) CGFloat catchPoint;
+
+@end
+
 @implementation VCollapsingFlowLayout
+
+#pragma mark - UICollectionViewFlowLayout
 
 - (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds
 {
-#warning Optimize this to only invalidate layout when we are collapsing
     return YES;
 }
 
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
 {
-    NSArray *defaultAttributes = [super layoutAttributesForElementsInRect:rect];
+    NSMutableArray *attributes = [[super layoutAttributesForElementsInRect:rect] mutableCopy];
     
-    UICollectionViewLayoutAttributes *layoutAttributesForRealTimeComments = [defaultAttributes objectAtIndex:1];
+    UICollectionViewLayoutAttributes *layoutAttributesForRealTimeComments = [attributes objectAtIndex:1];
     
-    CGFloat catchPoint = CGRectGetHeight(layoutAttributesForRealTimeComments.frame);
+    self.catchPoint = CGRectGetHeight(layoutAttributesForRealTimeComments.frame);
     
-    [defaultAttributes enumerateObjectsUsingBlock:^(UICollectionViewLayoutAttributes *layoutAttributes, NSUInteger idx, BOOL *stop)
+    NSLog(@"current content offset: %@", NSStringFromCGPoint(self.collectionView.contentOffset));
+    
+    [attributes enumerateObjectsUsingBlock:^(UICollectionViewLayoutAttributes *layoutAttributes, NSUInteger idx, BOOL *stop)
     {
-        if (self.collectionView.contentOffset.y < catchPoint)
+        if (self.collectionView.contentOffset.y < self.catchPoint)
         {
-            if (idx == 0)
+            if ([layoutAttributes.indexPath compare:[self contentViewIndexPath]] == NSOrderedSame)
             {
-                layoutAttributes.frame = CGRectMake(0, self.collectionView.contentOffset.y, 320, 320);
+                [self layoutAttributesForConetntViewState:kVContentViewStateFullSize
+                              withInitialLayoutAttributes:layoutAttributes];
             }
-            else if (idx == 1)
+            else if ([layoutAttributes.indexPath compare:[self realTimeCommentsIndexPath]] == NSOrderedSame)
             {
                 layoutAttributes.frame = CGRectMake(0, self.collectionView.contentOffset.y + 320, 320, 110);
             }
         }
         else
         {
-            CGFloat deltaCatchPointToTop = self.collectionView.contentOffset.y - catchPoint;
-            CGFloat percentCompleted = (deltaCatchPointToTop / 320.0f);
-            NSLog(@"%f", percentCompleted);
-            if (idx == 0)
+            if ([layoutAttributes.indexPath compare:[self contentViewIndexPath]] == NSOrderedSame)
             {
-                layoutAttributes.zIndex = 1000;
-                layoutAttributes.center = CGPointMake(layoutAttributes.center.x + fminf((percentCompleted* 100.0f), 100.0f), layoutAttributes.center.y + self.collectionView.contentOffset.y - fminf((percentCompleted * 160.0f), 160.0f));
-                layoutAttributes.transform = CGAffineTransformMakeScale(fmaxf(1.0f - percentCompleted, 0.35f), fmaxf(1.0f - percentCompleted, 0.35f));
+                [self layoutAttributesForConetntViewState:kVContentViewStateShrinking
+                              withInitialLayoutAttributes:layoutAttributes];
             }
-            else if (idx == 1)
+            else if ([layoutAttributes.indexPath compare:[self realTimeCommentsIndexPath]] == NSOrderedSame)
             {
                 layoutAttributes.frame = CGRectMake(0, self.collectionView.contentOffset.y + 320.0f, 320, 110);
             }
@@ -58,9 +71,59 @@
             }
         }
     }];
-    
-    return defaultAttributes;
+    return attributes;
 }
 
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    if ([indexPath compare:[self contentViewIndexPath]] == NSOrderedSame)
+    {
+        return [self layoutAttributesForConetntViewState:kVContentViewStateFloating
+                             withInitialLayoutAttributes:nil];
+    }
+    return [super layoutAttributesForItemAtIndexPath:indexPath];
+}
+
+#pragma mark - Convenience
+
+- (NSIndexPath *)contentViewIndexPath
+{
+    return [NSIndexPath indexPathForRow:0 inSection:0];
+}
+
+- (NSIndexPath *)realTimeCommentsIndexPath
+{
+    return [NSIndexPath indexPathForRow:0 inSection:1];
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForConetntViewState:(kVContentViewState)contentViewState
+                                              withInitialLayoutAttributes:(UICollectionViewLayoutAttributes *)initialLayoutAttributes
+{
+    UICollectionViewLayoutAttributes *layoutAttributes = initialLayoutAttributes;
+    if (!initialLayoutAttributes)
+    {
+        NSIndexPath *contentViewIndexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+        layoutAttributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:contentViewIndexPath];
+        layoutAttributes.center = CGPointMake(160.0f, 160.0f);
+    }
+    
+    switch (contentViewState) {
+        case kVContentViewStateFullSize:
+            layoutAttributes.frame = CGRectMake(0, self.collectionView.contentOffset.y, 320, 320);
+            break;
+        case kVContentViewStateShrinking:
+        case kVContentViewStateFloating:
+        {
+            CGFloat deltaCatchPointToTop = self.collectionView.contentOffset.y - self.catchPoint;
+            CGFloat percentCompleted = (deltaCatchPointToTop / 320.0f);
+            
+            layoutAttributes.zIndex = 1000;
+            layoutAttributes.center = CGPointMake(CGRectGetMidX(self.collectionView.bounds) + fminf((percentCompleted* 100.0f), 100.0f), layoutAttributes.center.y + self.collectionView.contentOffset.y - fminf((percentCompleted * 160.0f), 160.0f));
+            layoutAttributes.transform = CGAffineTransformMakeScale(fmaxf(1.0f - percentCompleted, 0.35f), fmaxf(1.0f - percentCompleted, 0.35f));
+        }
+            break;
+    }
+    return layoutAttributes;
+}
 
 @end
