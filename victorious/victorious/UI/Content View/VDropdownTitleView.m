2fc443e2904f0b861bc06e834489a978dd375a27
Michael Sena <michaelsena@getvictorious.com>
2014-09-09 18:06:27 -0700

    Add drop down title view to project

---
+++ victorious/victorious/UI/Content View/VDropdownTitleView.m
@@ -0,0 +1,31 @@
+//
+//  VDropdownTitleView.m
+//  victorious
+//
+//  Created by Michael Sena on 9/9/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import "VDropdownTitleView.h"
+
+@implementation VDropdownTitleView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
