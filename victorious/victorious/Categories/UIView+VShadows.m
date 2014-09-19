60f8180dcc8b386ef7fb48b2017ae694a287144f
Michael Sena <michaelsena@getvictorious.com>
2014-09-18 17:06:49 -0700

    Interpolating shadows! Because, why not?!

---
+++ victorious/victorious/Categories/UIView+VShadows.m
@@ -0,0 +1,35 @@
+//
+//  UIView+VShadows.m
+//  victorious
+//
+//  Created by Michael Sena on 9/18/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import "UIView+VShadows.h"
+
+@implementation UIView (VShadows)
+
+- (void)v_applyShadowsWithZIndex:(CGFloat)zIndex
+{
+    self.layer.shadowOpacity = 1.0f;
+    self.layer.shadowOffset = CGSizeZero;
+    self.layer.shadowRadius = (zIndex * 1.5f);
+    
+    UIMotionEffectGroup *shadowEffects = [[UIMotionEffectGroup alloc] init];
+    
+    UIInterpolatingMotionEffect *xShadowEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"layer.shadowOffset.width" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+    xShadowEffect.maximumRelativeValue = @(zIndex*10);
+    xShadowEffect.minimumRelativeValue = @(-zIndex*10);
+    
+    UIInterpolatingMotionEffect *yShadowEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"layer.shadowOffset.height" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+    yShadowEffect.maximumRelativeValue = @(zIndex*10);
+    yShadowEffect.minimumRelativeValue = @(-zIndex*10);
+    
+    
+    shadowEffects.motionEffects = @[xShadowEffect,  yShadowEffect];
+    
+    [self addMotionEffect:shadowEffects];
+}
+
+@end
