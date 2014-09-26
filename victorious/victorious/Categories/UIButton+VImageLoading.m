15e8489889e69508a9340a46c1715cf08659c6f3
Will Long <will@getvictorious.com>
2014-09-25 20:01:18 -0700

    Fixing bug with the UIButton+VImageLoading methods

--- victorious/victorious/Categories/UIButton+VImageLoading.m
+++ victorious/victorious/Categories/UIButton+VImageLoading.m
@@ -15,9 +15,11 @@
        placeholderImage:(UIImage *)placeholderImage
                forState:(UIControlState)state
 {
+    [self setImage:placeholderImage forState:state];
+
     if (!url || [url.path isEmpty])
     {
-        [self setImage:placeholderImage forState:state];
+        return;
     }
     
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
