72973979b16ada55397ce88ff38af04595414249
Lawrence Leach <lawrence@lawrenceleach.com>
2014-09-30 20:26:54 -0700

    Removed tabs and put in spaces for indentation on curly brace

--- victorious/victorious/Managers/Object/VObjectManager+Login.m
+++ victorious/victorious/Managers/Object/VObjectManager+Login.m
@@ -55,7 +55,7 @@ static NSString * const kVVideoQualityKey = @"video_quality";
         {
             NSDictionary *dict = @{@"url.appstore": app_store_url};
             [[VSettingManager sharedManager] updateSettingsWithDictionary:dict];
-		}
+        }
 		
         NSDictionary *experiments = payload[kVExperimentsKey];
         if ([experiments isKindOfClass:[NSDictionary class]])
