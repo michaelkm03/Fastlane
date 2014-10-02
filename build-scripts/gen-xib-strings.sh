0363d1c0742dd6a3b8c918a31ef31f205498b5f1
Lawrence Leach <lawrence@lawrenceleach.com>
2014-10-02 16:03:28 -0700

    Finished strings bash script

--- build-scripts/gen-xib-strings.sh
+++ build-scripts/gen-xib-strings.sh
@@ -6,13 +6,54 @@
 ###########
 
 XIB_INFILE=$1
-STR_OUTFILE=$2
+OUTDIR="./xib-strings"
+OUTSUBDIR=$OUTDIR"/en.lproj"
 
-if [ "$XIB_INFILE" == "" || "$STR_OUTFILE" == "" ]; then
-    echo "Usage: `basename $0` [-i <xib-to-stringify>] [-o <name of string file to output>]"
-    echo ""
-	echo "If you leave off the -o param, the script will run through the entire Xcode project."
-	echo "If you only want one file to be converted, be sure to include the -o param."
+
+# Create output directory if it doesn't exist
+mkdir -p "$OUTDIR"
+mkdir -p "$OUTSUBDIR"
+
+
+# Process any xib passed in as a parameter
+if [ "$XIB_INFILE" ]; then
+	echo ""
+	path=${XIB_INFILE/*}
+	base=${XIB_INFILE##*/}
+	fext=${base##*.}
+	if [ $fext = "xib" ]; then
+		echo "Generating Strings File for $XIB_INFILE"
+		stringsFile=${base%.*}.strings
+		echo "$base -> $stringsFile"
+		ibtool --generate-strings-file "$OUTSUBDIR/$stringsFile" "$XIB_INFILE"
+		echo "Finished!"
+	else
+		echo "Error: Input file must be a xib file... exiting now"
+	fi
+	echo ""
     exit 1
 fi
 
+# Sweep through project directory and locate all xib files
+echo ""
+echo "Generating Strings Files..."
+find . -name "*.xib" -print0 | while read -d $'\0' file
+do
+	path=${file/*}
+	base=${file##*/}
+	fext=${base##*.}
+	if [ $fext = "xib" ]; then
+		filename=$(basename $file)
+		stringsFile=${base%.*}.strings
+		echo "$base -> $stringsFile"
+		ibtool --generate-strings-file "$OUTSUBDIR/$stringsFile" "$file"
+	else
+		echo ""
+		echo "Skipping $file (Not a xib file)"
+		echo ""
+	fi
+done
+
+echo ""
+echo "Finished!"
+echo ""
