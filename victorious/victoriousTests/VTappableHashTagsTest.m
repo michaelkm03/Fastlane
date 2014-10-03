0194020528060e72270fa089c1a45b1a85eee8ca
Patrick Lynch <patrick@getvictorious.com>
2014-10-03 10:31:35 -0700

    Added more to VTappableHashtagsTest.

--- victorious/victoriousTests/VTappableHashTagsTest.m
+++ victorious/victoriousTests/VTappableHashTagsTest.m
@@ -12,21 +12,28 @@
 
 @interface MockHashTagsDelegate : NSObject <VTappableHashTagsDelegate>
 
+@property (nonatomic, strong) NSTextStorage *textStorage;
+@property (nonatomic, strong) NSLayoutManager *layoutManager;
+@property (nonatomic, strong) NSTextContainer *textContainer;
+
 @end
 
 @implementation MockHashTagsDelegate
 
-- (NSTextStorage *)textStorage
+- (instancetype)init
 {
-    return nil;
-}
-- (NSLayoutManager *)layoutManager
-{
-    return nil;
-}
-- (NSTextContainer *)textContainer
-{
-    return nil;
+    self = [super init];
+    if (self)
+    {
+        self.layoutManager = [[NSLayoutManager alloc] init];
+        self.textContainer = [[NSTextContainer alloc] initWithSize:CGSizeMake( 320, 320 )];
+        self.textContainer.widthTracksTextView = YES;
+        self.textContainer.heightTracksTextView = YES;
+        [self.layoutManager addTextContainer:self.textContainer];
+        self.textStorage = [[NSTextStorage alloc] init];
+        [self.textStorage addLayoutManager:self.layoutManager];
+    }
+    return self;
 }
 
 @end
@@ -54,9 +61,9 @@
     [super tearDown];
 }
 
-- (void)testDelegate
+- (void)runInvalidDelegateTests
 {
-    XCTAssertFalse( _tappableHashTags.hasValidDelegate );
+    XCTAssertFalse( _tappableHashTags.hasValidDelegate, @"Delegate should be invalid before it is set." );
     
     NSError *error = nil;
     XCTAssertFalse( [_tappableHashTags setDelegate:nil error:&error] );
@@ -69,6 +76,51 @@
     XCTAssertNotNil( error.domain );
     
     XCTAssertFalse( [_tappableHashTags setDelegate:_delegate error:nil] );
+}
+
+- (void)testDelegateInvalidNoTextStorage
+{
+    _delegate.textStorage = nil;
+    [self runInvalidDelegateTests];
+}
+
+- (void)testDelegateInvalidNoLayoutManager
+{
+    _delegate.layoutManager = nil;
+    [self runInvalidDelegateTests];
+}
+
+- (void)testDelegateInvalidNoTextContainer
+{
+    _delegate.textContainer = nil;
+    [self runInvalidDelegateTests];
+}
+
+- (void)testDelegateInvalidNoTextContainerInLayoutMabager
+{
+    [[_delegate layoutManager] removeTextContainerAtIndex:0];
+    [self runInvalidDelegateTests];
+}
+
+- (void)testDelegateInvalidNoLayoutManagerInTextStorage
+{
+    [[_delegate textStorage] removeLayoutManager:_delegate.layoutManager];
+    [self runInvalidDelegateTests];
+}
+
+- (void)testDelegateValid
+{
+    XCTAssertFalse( _tappableHashTags.hasValidDelegate, @"Delegate should be invalid before it is set." );
+    
+    NSError *error = nil;
+    XCTAssertTrue( [_tappableHashTags setDelegate:_delegate error:&error] );
+    XCTAssertNil( error );
+    
+    error = nil;
+    XCTAssertTrue( [_tappableHashTags setDelegate:_delegate error:&error] );
+    XCTAssertNil( error );
+    
+    XCTAssertTrue( [_tappableHashTags setDelegate:_delegate error:nil] );
 }
 
 @end
