6b0a9392ab3a8ddd7a4e514f957be8f514a26c4e
Lawrence Leach <lawrence@lawrenceleach.com>
2014-10-06 10:33:07 -0700

    Created localized versions of ALL xibs and storyboards in the app Killed tabs from gen-xib-strings.sh script Modified the .gitignore to include the xib-strings directory created by the gen-xib-strings.sh script (included a CR at the bottom of the file.)

--- build-scripts/gen-xib-strings.sh
+++ build-scripts/gen-xib-strings.sh
@@ -6,7 +6,7 @@
 ###########
 
 XIB_INFILE=$1
-OUTDIR="./victorious/victorious/Supporting Files"
+OUTDIR="./xib-strings"
 OUTSUBDIR=$OUTDIR"/en.lproj"
 MINFILESIZE=2
 
@@ -18,20 +18,20 @@ mkdir -p "$OUTSUBDIR"
 
 # Process any xib passed in as a parameter
 if [ "$XIB_INFILE" ]; then
-	echo ""
-	path=${XIB_INFILE/*}
-	base=${XIB_INFILE##*/}
-	fext=${base##*.}
-	if [ $fext = "xib" ]; then
-		echo "Generating Strings File for $XIB_INFILE"
-		stringsFile=${base%.*}.strings
-		echo "$base -> $stringsFile"
-		ibtool --generate-strings-file "$OUTSUBDIR/$stringsFile" "$XIB_INFILE"
-		echo "Finished!"
-	else
-		echo "Error: Input file must be a xib file... exiting now"
-	fi
-	echo ""
+    echo ""
+    path=${XIB_INFILE/*}
+    base=${XIB_INFILE##*/}
+    fext=${base##*.}
+    if [ $fext = "xib" ] || [ $fext = "storyboard" ]; then
+        echo "Generating Strings File for $XIB_INFILE"
+        stringsFile=${base%.*}.strings
+        echo "$base -> $stringsFile"
+        ibtool --generate-strings-file "$OUTSUBDIR/$stringsFile" "$XIB_INFILE"
+        echo "Finished!"
+    else
+        echo "Error: Input file must be an xib or storyboard file... exiting now"
+    fi
+    echo ""
     exit 0
 fi
 
@@ -42,61 +42,36 @@ echo "Generating Strings Files"
 # Sweep through project directory and locate all xib files
 echo ""
 echo "xibs..."
-find . -name "*.xib" -print0 | while read -d $'\0' file
+find . \( -name "*.xib" -or -name "*.storyboard" \) -print | grep '/en.lproj/' | while read -d $'\n' file
 do
-	path=${file/*}
-	base=${file##*/}
-	fext=${base##*.}
-	if [ $fext = "xib" ]; then
-		filename=$(basename $file)
-		stringsFile=${base%.*}.strings
-		ibtool --generate-strings-file "$OUTSUBDIR/$stringsFile" "$file"
-		filesize=$(stat -f%z "$OUTSUBDIR/$stringsFile")
-		if [ "$filesize" -le "$MINFILESIZE" ]; then
-			rm "$OUTSUBDIR/$stringsFile"
-			echo ""
-			echo "Error: Cannot generate $base strings file"
-			echo "Reason: File is empty"
-			echo ""
-		else
-			echo "$base -> $stringsFile"
-		fi
-	else
-		echo ""
-		echo "Skipping $file (Not a xib file)"
-		echo ""
-	fi
+    path=${file/*}
+    base=${file##*/}
+    fext=${base##*.}
+
+    if [ $fext = "xib" ] || [ $fext = 'storyboard' ]; then
+        filename=$(basename $file)
+        stringsFile=${base%.*}.strings
+
+        echo "$base -> $stringsFile"
+
+        ibtool --generate-strings-file "$OUTSUBDIR/$stringsFile" "$file"
+        filesize=$(stat -f%z "$OUTSUBDIR/$stringsFile")
+        if [ "$filesize" -le "$MINFILESIZE" ]; then
+            rm "$OUTSUBDIR/$stringsFile"
+            echo ""
+            echo "Error: Cannot generate $base strings file"
+            echo "Reason: Strings file is empty"
+            echo ""
+        else
+            echo "$base -> $stringsFile"
+        fi
+    else
+        echo ""
+        echo "Skipping $file (Not a xib or storyboard file)"
+        echo ""
+    fi
 done
 
-
-# Sweep through project directory and locate all xib files
-echo ""
-echo "Storyboards..."
-find . -name "*.storyboard" -print0 | while read -d $'\0' file
-do
-	path=${file/*}
-	base=${file##*/}
-	fext=${base##*.}
-	if [ $fext = "storyboard" ]; then
-		filename=$(basename $file)
-		stringsFile=${base%.*}.strings
-		ibtool --generate-strings-file "$OUTSUBDIR/$stringsFile" "$file"
-		filesize=$(stat -f%z "$OUTSUBDIR/$stringsFile")
-		if [ "$filesize" -le "$MINFILESIZE" ]; then
-			rm "$OUTSUBDIR/$stringsFile"
-			echo ""
-			echo "Error: Cannot generate $base strings file"
-			echo "Reason: File is empty"
-			echo ""
-		else
-			echo "$base -> $stringsFile"
-		fi
-	else
-		echo ""
-		echo "Skipping $file (Not a Storyboard file)"
-		echo ""
-	fi
-done
 
 echo ""
 echo "Finished!"
