b48b55c4618b7d2b6392411cc1570b47c8aaed0a
Michael Sena <michaelsena@getvictorious.com>
2014-10-09 13:06:24 -0700

    Action sheet business logic + disabled state for action items.

--- victorious/victorious/UI/Common/Action Sheet/VActionSheetViewController.m
+++ victorious/victorious/UI/Common/Action Sheet/VActionSheetViewController.m
@@ -233,6 +233,11 @@ static const UIEdgeInsets kSeparatorInsets = {0.0f, 20.0f, 0.0f, 20.0f};
             actionitemCell.detailTitle = itemForCell.detailText;
             actionitemCell.actionIcon = itemForCell.icon;
             actionitemCell.separatorInsets = self.tableView.separatorInset;
+            actionitemCell.enabled = itemForCell.enabled;
+            if (!actionitemCell.enabled)
+            {
+                
+            }
             actionitemCell.accessorySelectionHandler = ^(void)
             {
                 if (itemForCell.detailSelectionHandler)
