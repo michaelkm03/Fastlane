47021598d767a2fd0288722b152320121624c23b
Will Long <will@getvictorious.com>
2014-10-14 14:50:25 -0700

    Cleaning up the project file hierarchy

---
+++ victorious/victorious/UI/Common/NavHeaders/VNavigationSelectorProtocol.h
@@ -0,0 +1,13 @@
+//
+//  VNavigationSelectorProtocol.h
+//  victorious
+//
+//  Created by Will Long on 10/14/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol VNavigationSelectorProtocol <NSObject>
+
+@end
