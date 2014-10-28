ec13b60fd887cb048d77ab1baefc9f83eee01832
Will Long <will@getvictorious.com>
2014-10-27 17:50:53 -0700

    Following/follower nav headers

---
+++ victorious/victorious/UI/Common/VContainerViewController.h
@@ -0,0 +1,13 @@
+//
+//  VContainerViewController.h
+//  victorious
+//
+//  Created by Will Long on 10/27/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VContainerViewController : UIViewController
+
+@end
---
+++ victorious/victorious/UI/Common/VContainerViewController.m
@@ -0,0 +1,13 @@
+//
+//  VContainerViewController.m
+//  victorious
+//
+//  Created by Will Long on 10/27/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import "VContainerViewController.h"
+
+@implementation VContainerViewController
+
+@end
