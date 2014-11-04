b3242d1bf329905e1e383074c1ad1c9d92f6ebe5
Patrick Lynch <patrick@getvictorious.com>
2014-11-04 15:55:14 -0800

    Created VStreamCollectionCellAnnouncement to show web view.

---
+++ victorious/victorious/UI/Streams/CollectionView/Cells/VStreamCollectionCellAnnouncement.h
@@ -0,0 +1,13 @@
+//
+//  VStreamCollectionCellAnnouncement.h
+//  victorious
+//
+//  Created by Patrick Lynch on 11/4/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import "VStreamCollectionCell.h"
+
+@interface VStreamCollectionCellAnnouncement : VStreamCollectionCell
+
+@end
---
+++ victorious/victorious/UI/Streams/CollectionView/Cells/VStreamCollectionCellAnnouncement.m
@@ -0,0 +1,28 @@
+//
+//  VStreamCollectionCellAnnouncement.m
+//  victorious
+//
+//  Created by Patrick Lynch on 11/4/14.
+//  Copyright (c) 2014 Victorious. All rights reserved.
+//
+
+#import "VStreamCollectionCellAnnouncement.h"
+#import "VWebContentViewController.h"
+
+@interface VStreamCollectionCellAnnouncement()
+
+@property (nonatomic, weak) IBOutlet VWebContentViewController *webContentViewController;
+
+@end
+
+@implementation VStreamCollectionCellAnnouncement
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
