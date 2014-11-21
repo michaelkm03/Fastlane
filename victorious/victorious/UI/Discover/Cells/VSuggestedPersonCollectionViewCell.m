be31afb6566e859d5bc107a2711065c5c72054c0
Patrick Lynch <patrick@getvictorious.com>
2014-11-20 21:05:36 -0800

    Removed VFollowersTextFormatter class because it duplicated functionality in VLargeTextFormatter.

--- victorious/victorious/UI/Discover/Cells/VSuggestedPersonCollectionViewCell.m
+++ victorious/victorious/UI/Discover/Cells/VSuggestedPersonCollectionViewCell.m
@@ -9,8 +9,8 @@
 #import "VSuggestedPersonCollectionViewCell.h"
 #import "VThemeManager.h"
 #import "VDefaultProfileImageView.h"
-#import "VFollowersTextFormatter.h"
 #import "VFollowUserControl.h"
+#import "VLargeNumberFormatter.h"
 
 @interface VSuggestedPersonCollectionViewCell()
 
@@ -24,6 +24,17 @@
 @end
 
 @implementation VSuggestedPersonCollectionViewCell
+
++ (VLargeNumberFormatter *)numberFormatter
+{
+    static VLargeNumberFormatter *formatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^(void)
+                  {
+                      formatter = [[VLargeNumberFormatter alloc] init];
+                  });
+    return formatter;
+}
 
 + (UIImage *)followedImage
 {
@@ -83,7 +94,7 @@
 
 - (void)populateData
 {
-    self.descriptionLabel.text = [VFollowersTextFormatter shortLabelWithNumberOfFollowersObject:self.user.numberOfFollowers];
+    self.descriptionLabel.text = [self followerTextWithNumberOfFollowers:self.user.numberOfFollowers.integerValue];
     
     self.usernameLabel.text = self.user.name;
     
@@ -93,6 +104,31 @@
     }
     
     [self applyTheme];
+}
+
+- (NSString *)followerTextWithNumberOfFollowers:(NSInteger)numberOfFollwers
+{
+    NSString *numberString = [[[self class] numberFormatter] stringForInteger:numberOfFollwers];
+    
+    if ( numberOfFollwers == 0 )
+    {
+        return NSLocalizedString( @"SuggestedFollowersNone", nil);
+    }
+    else if ( numberOfFollwers == 1 )
+    {
+        NSString *format = NSLocalizedString( @"SuggestedFollowersSing", nil);
+        return [NSString stringWithFormat:format, numberString];
+    }
+    else if ( numberOfFollwers >= 1000 )
+    {
+        NSString *format = NSLocalizedString( @"SuggestedFollowersK", nil);
+        return [NSString stringWithFormat:format, numberString];
+    }
+    else
+    {
+        NSString *format = NSLocalizedString( @"SuggestedFollowersPlur", nil);
+        return [NSString stringWithFormat:format, numberString];
+    }
 }
 
 - (void)updateFollowingAnimated:(BOOL)animated
