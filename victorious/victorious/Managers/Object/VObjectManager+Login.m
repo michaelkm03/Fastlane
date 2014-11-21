f3ce1a1aec0a3a0167da7c8f7f06a719f9068b0b
Patrick Lynch <patrick@getvictorious.com>
2014-11-20 20:47:13 -0800

    Undid acceidental renaming of unrelated constants.

--- victorious/victorious/Managers/Object/VObjectManager+Login.m
+++ victorious/victorious/Managers/Object/VObjectManager+Login.m
@@ -32,7 +32,7 @@ NSString *kInitResponseNotification             = @"InitResponseNotification";
 
 static NSString * const kVExperimentsKey        = @"experiments";
 static NSString * const kVAppearanceKey         = @"appearance";
-static NSString * const kVideoQualityKey       = @"video_quality";
+static NSString * const kVVideoQualityKey       = @"video_quality";
 static NSString * const kVAppTrackingKey        = @"video_quality";
 
 #pragma mark - Init
@@ -73,7 +73,7 @@ static NSString * const kVAppTrackingKey        = @"video_quality";
 
 - (void)updateSettings:(VSettingManager *)settingsManager withResponsePayload:(NSDictionary *)payload
 {
-    NSDictionary *videoQuality = payload[kVideoQualityKey];
+    NSDictionary *videoQuality = payload[kVVideoQualityKey];
     if ([videoQuality isKindOfClass:[NSDictionary class]])
     {
         [settingsManager updateSettingsWithDictionary:videoQuality];
