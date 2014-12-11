c8be0871d9fd523175c31ccb88fbcf099ce62a03
Patrick Lynch <patrick@getvictorious.com>
2014-12-11 10:00:45 -0800

    Turned off debug settings.

--- victorious/victorious/Managers/VPurchaseDebugSettings.h
+++ victorious/victorious/Managers/VPurchaseDebugSettings.h
@@ -6,19 +6,15 @@
 //  Copyright (c) 2014 Victorious. All rights reserved.
 //
 
-#define FORCE_SIMULATE_STOREKIT             1
+#define FORCE_SIMULATE_STOREKIT             0
 #define SIMULATE_STOREKIT                   FORCE_SIMULATE_STOREKIT || TARGET_IPHONE_SIMULATOR
-#define SIMULATE_PURCHASE_ERROR             1
+#define SIMULATE_PURCHASE_ERROR             0
 #define SIMULATE_FETCH_PRODUCTS_ERROR       0
 #define SIMULATE_RESTORE_PURCHASE_ERROR     0
 #define SIMULATION_DELAY                    1.0f
 #define SIMULATED_RESTORED_PURCHASE_COUNT   0
 #define SIMULATED_PRODUCT_IDENTIFIER        @"test.test.test"
 
-#if SIMULATE_STOREKIT
-#warning VPurchaseManager is simulating one or more StoreKit interactions
-#endif
-
-#if SIMULATE_NO_RESTORED_PRODUCTS || SIMULATE_PURCHASE_ERROR || SIMULATE_FETCH_PRODUCTS_ERROR || SIMULATE_RESTORE_PURCHASE_ERROR
+#if FORCE_SIMULATE_STOREKIT || SIMULATE_NO_RESTORED_PRODUCTS || SIMULATE_PURCHASE_ERROR || SIMULATE_FETCH_PRODUCTS_ERROR || SIMULATE_RESTORE_PURCHASE_ERROR
 #warning VPurchaseManager is simulating one or more failed StoreKit interactions
 #endif
