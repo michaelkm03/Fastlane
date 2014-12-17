a5e95ed87521c37e90f6b5bc07572e8d371a7d72
Patrick Lynch <patrick@getvictorious.com>
2014-12-17 11:26:43 -0800

    Sorted categories folder by name and added comments to NSString+Unicode public methods.

--- victorious/victorious/Categories/NSString+Unicode.swift
+++ victorious/victorious/Categories/NSString+Unicode.swift
@@ -9,7 +9,12 @@
 import Foundation
 
 public extension NSString {
-    
+
+    /**
+     * Returns the actual number of unicode characters present in the string.
+     * Intended for getting accurate character count that accounts for emojis, which
+     * can be counted as 2 or more characters using NSString's `length` property.
+     */
     @objc public var lengthWithUnicode: Int {
         return countElements( self as String )
     }
