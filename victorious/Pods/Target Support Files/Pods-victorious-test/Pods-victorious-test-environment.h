a6b7033e94a3079e5ea70f6ceaca4b1e5f820de4
Michael Sena <michaelsena@getvictorious.com>
2014-12-18 11:45:28 -0800

    Removing marqueeLabel

--- victorious/Pods/Target Support Files/Pods-victorious-test/Pods-victorious-test-environment.h
+++ victorious/Pods/Target Support Files/Pods-victorious-test/Pods-victorious-test-environment.h
@@ -54,12 +54,6 @@
 #define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
 #define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
 
-// MarqueeLabel
-#define COCOAPODS_POD_AVAILABLE_MarqueeLabel
-#define COCOAPODS_VERSION_MAJOR_MarqueeLabel 2
-#define COCOAPODS_VERSION_MINOR_MarqueeLabel 0
-#define COCOAPODS_VERSION_PATCH_MarqueeLabel 8
-
 // Nocilla
 #define COCOAPODS_POD_AVAILABLE_Nocilla
 #define COCOAPODS_VERSION_MAJOR_Nocilla 0
