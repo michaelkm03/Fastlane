caf85bb31d927ba5a7eed9c523fc6642835c6898
Lawrence Leach <lawrence@lawrenceleach.com>
2014-12-19 19:05:43 -0800

    Proper passing of the hashtag to the

--- victorious/victorious/UI/Discover/VDiscoverViewController.m
+++ victorious/victorious/UI/Discover/VDiscoverViewController.m
@@ -300,7 +300,7 @@ static NSString * const kVTrendingTagIdentifier              = @"VTrendingTagCel
             VTrendingTagCell *customCell = (VTrendingTagCell *)[tableView dequeueReusableCellWithIdentifier:kVTrendingTagIdentifier forIndexPath:indexPath];
 
             VHashtag *hashtag = self.trendingTags[ indexPath.row ];
-            [customCell setHashtag:hashtag];
+            [customCell setHashtag:hashtag.tag];
             
             __weak typeof(customCell) weakCell = customCell;
             customCell.subscribeToTagAction = ^(void)
