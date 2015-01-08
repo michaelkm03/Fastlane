0a162a072b23516d07e8a4422213d240510c3c31
Patrick Lynch <patrick@getvictorious.com>
2015-01-07 19:31:15 -0800

    Fixed comments.

--- victorious/victorious/UI/Common/Transitions/VAnimatedTransition.h
+++ victorious/victorious/UI/Common/Transitions/VAnimatedTransition.h
@@ -10,31 +10,45 @@
 #import "VTransitionModel.h"
 
 /**
- *  Any classes that implement an animated transition through VViewControllerTransition
- *  must conform to this protocol.  VViewControllerTransition will configure the transition
- *  and then call these methods accordingly while the transition is being performed.
+ Any classes that implement an animated transition through VViewControllerTransition
+ must conform to this protocol.  VViewControllerTransition will configure the transition
+ and then call these methods accordingly while the transition is being performed.
  */
 @protocol VAnimatedTransition <NSObject>
 
-// Set initial state (if required) before animation will actually start
+/**
+ Set initial state (if required) before animation will actually start
+ */
 - (void)prepareForTransitionIn:(VTransitionModel *)model;
 
-// Do the actual work of the animation, always call `completion` block when finished
+/**
+  Do the actual work of the animation, always call `completion` block when finished
+ */
 - (void)performTransitionIn:(VTransitionModel *)model completion:(void (^)(BOOL))completion;
 
-// Set initial state (if required) before animation will actually start
+/**
+ Set initial state (if required) before animation will actually start
+ */
 - (void)prepareForTransitionOut:(VTransitionModel *)model;
 
-// Do the actual work of the animation, always call `completion` block when finished
+/**
+ Do the actual work of the animation, always call `completion` block when finished
+ */
 - (void)performTransitionOut:(VTransitionModel *)model completion:(void (^)(BOOL))completion;
 
-// To save on performance and memory, return NO here if the transition doesn't need the snapshot
+/**
+ To save on performance and memory, return NO here if the transition doesn't need the snapshot
+ */
 @property (nonatomic, readonly) BOOL requiresImageViewFromOriginViewController;
 
-// To total duration of the transition in animation
+/**
+ To total duration of the transition in animation
+ */
 @property (nonatomic, readonly) NSTimeInterval transitionInDuration;
 
-// To total duration of the transition out animation
+/**
+ To total duration of the transition out animation
+ */
 @property (nonatomic, readonly) NSTimeInterval transitionOutDuration;
 
 @end
--- victorious/victorious/UI/Common/Transitions/VTransitionModel.h
+++ victorious/victorious/UI/Common/Transitions/VTransitionModel.h
@@ -6,37 +6,49 @@
 //  Copyright (c) 2014 Victorious. All rights reserved.
 //
 
+ 
+
 #import <Foundation/Foundation.h>
 
 @protocol VAnimatedTransition;
 
 /**
- *  An object that holds important information about the animation transition that
- *  is taking place, encapsulated here so that it can be pass to transition animation
- *  classes that conform to VAnimatedTransition protocol.
+ An object that holds important information about the animation transition that
+ is taking place, encapsulated here so that it can be pass to transition animation
+ classes that conform to VAnimatedTransition protocol.
  */
 @interface VTransitionModel : NSObject
 
 /**
- *  Creates the transition model from the transition context provided by iOS
- *  and a transition animator provided by you.
+ Creates the transition model from the transition context provided by iOS
+ and a transition animator provided by you.
  */
 - (instancetype)initWithTransitionContext:(id <UIViewControllerContextTransitioning>)transitionContext
                                transition:(id<VAnimatedTransition>)transition;
 
-// The direction of the navigation, i.e. push/pop or present/dismiss
+/**
+ The direction of the navigation, i.e. push/pop or present/dismiss
+ */
 @property (nonatomic, readonly, assign) BOOL isPresenting;
 
-// The view controller from which we came
+/**
+ The view controller from which we came
+ */
 @property (nonatomic, readonly, strong) UIViewController *fromViewController;
 
-// The view controller to which we are going
+/**
+ The view controller to which we are going
+ */
 @property (nonatomic, readonly, strong) UIViewController *toViewController;
 
-// Useful for some animation effects
+/**
+ Useful for some animation effects
+ */
 @property (nonatomic, readonly, strong) UIView *snapshotOfOriginView;
 
-// The total duration of the transition taking place
+/**
+ The total duration of the transition taking place
+ */
 @property (nonatomic, readonly, assign) NSTimeInterval animationDuration;
 
 @end
