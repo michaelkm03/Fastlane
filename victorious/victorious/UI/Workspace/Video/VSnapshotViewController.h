0d71f824518493d974665cbd1a4d831a38519526
Michael Sena <michaelsena@getvictorious.com>
2015-02-16 19:59:24 -0800

    Add snapshotVC

---
+++ victorious/victorious/UI/Workspace/Video/VSnapshotViewController.h
@@ -0,0 +1,13 @@
+//
+//  VSnapshotViewController.h
+//  victorious
+//
+//  Created by Michael Sena on 2/16/15.
+//  Copyright (c) 2015 Victorious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VSnapshotViewController : UIViewController
+
+@end
--- victorious/victorious/UI/Workspace/Video/VVideoSnapshotTool.m
+++ victorious/victorious/UI/Workspace/Video/VVideoSnapshotTool.m
@@ -9,29 +9,48 @@
 #import "VVideoSnapshotTool.h"
 #import "VDependencyManager.h"
 
-@interface VVideoSnapshotTool ()
+// ViewControllers
+#import "VCVideoPlayerViewController.h"
+#import "VSnapshotViewController.h"
+
+@interface VVideoSnapshotTool () <VCVideoPlayerDelegate>
 
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, strong) UIImage *previewImage;
 @property (nonatomic, strong) NSURL *renderedMediaURL;
 
+@property (nonatomic, strong) VCVideoPlayerViewController *videoPlayerViewController;
+@property (nonatomic, strong) VSnapshotViewController *snapshotToolViewController;
 @end
 
 @implementation VVideoSnapshotTool
+
+@synthesize selected = _selected;
 
 - (instancetype)initWithDependencyManager:(VDependencyManager *)dependencyManager
 {
     self = [super init];
     if (self)
     {
-        self.title = [dependencyManager stringForKey:@"title"];
+        _title = [dependencyManager stringForKey:@"title"];
+        
+        _videoPlayerViewController = [[VCVideoPlayerViewController alloc] initWithNibName:nil bundle:nil];
+        _videoPlayerViewController.shouldFireAnalytics = NO;
+        _videoPlayerViewController.loopWithoutComposition = YES;
+        _videoPlayerViewController.shouldShowToolbar = YES;
+        _videoPlayerViewController.shouldChangeVideoGravityOnDoubleTap = YES;
+        _videoPlayerViewController.videoPlayerLayerVideoGravity = AVLayerVideoGravityResizeAspectFill;
+        
+        _snapshotToolViewController = [[VSnapshotViewController alloc] initWithNibName:nil bundle:nil];
     }
     return self;
 }
 
+#pragma mark - VVideoWorkspaceTool
+
 - (void)setMediaURL:(NSURL *)mediaURL
 {
-    //TODO: configureVCVideoPlayer
+    [self.videoPlayerViewController setItemURL:mediaURL loop:YES];
 }
 
 - (void)exportToURL:(NSURL *)url withCompletion:(void (^)(BOOL, UIImage *, NSError *))completion
@@ -41,6 +60,38 @@
 - (NSURL *)mediaURL
 {
     return self.renderedMediaURL;
+}
+
+#pragma mark - VWorkspaceTool
+
+- (void)setSelected:(BOOL)selected
+{
+    _selected = selected;
+    if (_selected)
+    {
+        [self.videoPlayerViewController.player play];
+    }
+    else
+    {
+        [self.videoPlayerViewController.player pause];
+    }
+}
+
+- (UIViewController *)canvasToolViewController
+{
+    return self.videoPlayerViewController;
+}
+
+- (UIViewController *)inspectorToolViewController
+{
+    return self.snapshotToolViewController;
+}
+
+#pragma mark - VCVideoPlayerDelegate
+
+- (void)videoPlayerReadyToPlay:(VCVideoPlayerViewController *)videoPlayer
+{
+    [videoPlayer.player play];
 }
 
 @end
