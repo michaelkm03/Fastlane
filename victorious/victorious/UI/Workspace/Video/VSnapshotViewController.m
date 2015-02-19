ba9edd7fd6f4208a3d5cb3266f1965d6dfcce175
Michael Sena <michaelsena@getvictorious.com>
2015-02-19 10:23:01 -0800

    Switch to using camera control for snapshot tool

--- victorious/victorious/UI/Workspace/Video/VSnapshotViewController.m
+++ victorious/victorious/UI/Workspace/Video/VSnapshotViewController.m
@@ -8,27 +8,54 @@
 
 #import "VSnapshotViewController.h"
 
+#import "VCameraControl.h"
+
 @interface VSnapshotViewController ()
 
-@property (weak, nonatomic) IBOutlet UIButton *snapshotButton;
+@property (weak, nonatomic) IBOutlet UIView *cameraControlContainer;
+@property (weak, nonatomic) VCameraControl *cameraControl;
 
 @end
 
 @implementation VSnapshotViewController
 
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    VCameraControl *cameraControl = [[VCameraControl alloc] initWithFrame:self.cameraControlContainer.bounds];
+    cameraControl.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.cameraControlContainer addSubview:cameraControl];
+    self.cameraControl = cameraControl;
+    self.cameraControl.captureMode = VCameraControlCaptureModeImage;
+    [self.cameraControl addTarget:self
+                           action:@selector(takeSnapshot:)
+                 forControlEvents:VCameraControlEventWantsStillImage];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.cameraControl restoreCameraControlToDefault];
+}
+
+#pragma mark - Property Accessors
+
 - (void)setButtonEnabled:(BOOL)buttonEnabled
 {
-    self.snapshotButton.enabled = buttonEnabled;
+    self.cameraControl.enabled = buttonEnabled;
 }
 
 - (BOOL)buttonEnabled
 {
-    return self.snapshotButton.enabled;
+    return self.cameraControl.enabled;
 }
 
 #pragma mark - Target/Action
 
-- (IBAction)takeSnapshot:(id)sender
+- (void)takeSnapshot:(VCameraControl *)sender
 {
     [self.delegate snapshotViewControllerWantsSnapshot:self];
 }
