ffcf8107a55d33ba45107fb6937ec5d7b0b51fc0
Michael Sena <michaelsena@getvictorious.com>
2015-02-19 11:14:02 -0800

    UIScrollView+VCenterContent category

---
+++ victorious/victorious/Categories/UIScrollView+VCenterContent.h
@@ -0,0 +1,15 @@
+//
+//  UIScrollView+VCenterContent.h
+//  victorious
+//
+//  Created by Michael Sena on 2/19/15.
+//  Copyright (c) 2015 Victorious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (VCenterContent)
+
+- (void)v_centerContentAnimated:(BOOL)animated;
+
+@end
