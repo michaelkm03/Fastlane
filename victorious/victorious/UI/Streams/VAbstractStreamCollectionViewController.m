279149bad4f27a47755323256f6f597065fe1591
feef <fifihypnophonic@gmail.com>
2015-03-05 12:25:57 -0800

    fix for only one page of content loaded by streamControllers

--- victorious/victorious/UI/Streams/VAbstractStreamCollectionViewController.m
+++ victorious/victorious/UI/Streams/VAbstractStreamCollectionViewController.m
@@ -36,10 +36,10 @@
 
 const CGFloat kVLoadNextPagePoint = .75f;
 
-@interface VAbstractStreamCollectionViewController ()
+@interface VAbstractStreamCollectionViewController () <VScrollPaginatorDelegate>
 
 @property (nonatomic, weak) IBOutlet UICollectionView *collectionView;
-@property (nonatomic, weak) IBOutlet VScrollPaginator *scrollPaginator;
+@property (nonatomic, strong) VScrollPaginator *scrollPaginator;
 @property (nonatomic, strong) UIActivityIndicatorView *bottomActivityIndicator;
 
 @property (nonatomic, strong) VImageSearchResultsFooterView *refreshFooter;
@@ -79,6 +79,8 @@ const CGFloat kVLoadNextPagePoint = .75f;
 
 - (void)commonInit
 {
+    self.scrollPaginator = [[VScrollPaginator alloc] init];
+    self.scrollPaginator.delegate = self;
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.extendedLayoutIncludesOpaqueBars = YES;
     self.navigationBarShouldAutoHide = YES;
