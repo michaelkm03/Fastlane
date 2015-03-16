2f9973d14336b033fc414efc230a75ea8b14cffd
Michael Sena <michaelsena@getvictorious.com>
2015-03-16 11:34:36 -0700

    Comments

--- victorious/victorious/UI/Workspace/Video/VTrimLoopingPlayerViewController.h
+++ victorious/victorious/UI/Workspace/Video/VTrimLoopingPlayerViewController.h
@@ -10,14 +10,30 @@
 
 @import CoreMedia;
 
+/**
+ *  VTrimLoopingPlayerViewController loops a video at a specified mediaURL. 
+ *  With an optional trim range within the uration of the video to loop over.
+ */
 @interface VTrimLoopingPlayerViewController : UIViewController
 
+/**
+ *  The media URL to load the video from.
+ */
 @property (nonatomic, copy) NSURL *mediaURL;
 
+/**
+ *  A trim range to loop over within the entire duration of the video.
+ */
 @property (nonatomic, assign) CMTimeRange trimRange;
 
+/**
+ *  Whether or not to mute the looping player.
+ */
 @property (nonatomic, assign, getter=isMuted) BOOL muted;
 
+/**
+ *  A custom frame duration, uses the video's native frame duration if never assigned.
+ */
 @property (nonatomic, assign) CMTime frameDuration;
 
 @end
