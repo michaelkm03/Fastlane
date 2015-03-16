5be6fea2e2f24261edf30720d080b9207fe4008f
Michael Sena <michaelsena@getvictorious.com>
2015-03-16 16:47:49 -0700

    Add some comments.

--- victorious/victorious/UI/Workspace/VWorkspaceToolButton.h
+++ victorious/victorious/UI/Workspace/VWorkspaceToolButton.h
@@ -6,16 +6,28 @@
 //  Copyright (c) 2015 Victorious. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 #import "VWorkspaceTool.h"
 
+/**
+ *  VWorkspaceToolButtons are used to signify selection of a particular tool
+ *  within the workspace. Draws a colored circle behind an icon for the tool.
+ *  Color swaps between selected and unselected variants.
+ */
 @interface VWorkspaceToolButton : UIButton
 
+/**
+ *  Assigna tool to this property and the tool button will grab an icon from this tool.
+ */
 @property (nonatomic, weak) id <VWorkspaceTool> tool;
 
+/**
+ *  A color representing the selected state.
+ */
 @property (nonatomic, copy) UIColor *selectedColor;
 
+/**
+ *  A color representing the unselected state.
+ */
 @property (nonatomic, copy) UIColor *unselectedColor;
 
 @end
