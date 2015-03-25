60fd817a3fa463a05cda78f2f53b71992acacc73
Josh Hinman <josh@getvictorious.com>
2015-03-24 21:32:47 -0700

    Fix ISO-2168: Clicking Remix # on Action Sheet Leads to Black "Remixes" Stream

--- victorious/victorious/UI/Streams/VStreamCollectionViewController.h
+++ victorious/victorious/UI/Streams/VStreamCollectionViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 
 #import "VAbstractStreamCollectionViewController.h"
+#import "VDependencyManager.h"
 #import "VHasManagedDependencies.h"
 #import "VSequenceActionsDelegate.h"
 #import "VNewContentViewController.h"
@@ -42,5 +43,16 @@ const CGFloat VStreamCollectionViewControllerCreateButtonHeight; ///< The height
  *  visible area is greater than or equal to this value.
  */
 @property (nonatomic, assign) float trackingMinRequiredCellVisibilityRatio;
+
+@end
+
+#pragma mark - 
+
+@interface VDependencyManager (VStreamCollectionViewController)
+
+/**
+ Returns a stream of remixes for the given sequence.
+ */
+- (VStreamCollectionViewController *)remixStreamForSequence:(VSequence *)sequence;
 
 @end
