64ff2c859306313d3c2bb562bb8e75f066b49115
Patrick Lynch <patrick@getvictorious.com>
2015-04-02 15:10:32 -0700

    Added comments for VContentTextCell.

--- victorious/victorious/UI/Content View/Cells/VContentTextCell.h
+++ victorious/victorious/UI/Content View/Cells/VContentTextCell.h
@@ -8,10 +8,16 @@
 
 #import "VContentCell.h"
 
+/**
+ A collection view cell used to display a text post in `VNewContentViewConroller`.
+ */
 @interface VContentTextCell : VContentCell
 
 @property (nonatomic, strong) VDependencyManager *dependencyManager;
 
+/**
+ Sets the text content and color from the text post to display as created by the user.
+ */
 - (void)setTextContent:(NSString *)text withBackgroundColor:(UIColor *)backgroundColor;
 
 @end
