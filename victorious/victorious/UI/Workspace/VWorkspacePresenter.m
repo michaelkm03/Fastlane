eea59c5642a43ffdbf506b67dd24520231498070
Michael Sena <michaelsena@getvictorious.com>
2015-04-02 19:16:46 -0700

    Land on filter after camera

--- victorious/victorious/UI/Workspace/VWorkspacePresenter.m
+++ victorious/victorious/UI/Workspace/VWorkspacePresenter.m
@@ -101,7 +101,7 @@
 - (void)presentCreateFlowWithInitialCaptureState:(VWorkspaceFlowControllerInitialCaptureState)initialCaptureState
 {
     [self presentCreateFlowWithInitialCaptureState:initialCaptureState
-                             initialImageEditState:VImageToolControllerInitialImageEditStateText
+                             initialImageEditState:VImageToolControllerInitialImageEditStateFilter
                           andInitialVideoEditState:VVideoToolControllerInitialVideoEditStateVideo];
 }
 
