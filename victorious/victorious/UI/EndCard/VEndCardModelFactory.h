2d72dd39c2ea5e64a0cecd5868a1e3f891156713
Patrick Lynch <patrick@getvictorious.com>
2015-04-07 12:36:24 -0700

    Added domments in header for newly created class.

--- victorious/victorious/UI/EndCard/VEndCardModelFactory.h
+++ victorious/victorious/UI/EndCard/VEndCardModelFactory.h
@@ -15,6 +15,11 @@
 
 - (instancetype)initWithDependencyManager:(VDependencyManager *)dependencyManager NS_DESIGNATED_INITIALIZER;
 
+/**
+ Creates a VEndCardModel object based on the provided sequence.
+ Returns nil if there is no endcard with a valid "next sequence" with which
+ to populate the end card UI.
+ */
 - (VEndCardModel *)createWithSequence:(VSequence *)sequence;
 
 @end
