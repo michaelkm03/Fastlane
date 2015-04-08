a118043671cbc62bc8acf4e7fbb101c2d00599ed
Michael Sena <michaelsena@getvictorious.com>
2015-04-08 12:04:58 -0700

    Further documenting that this is not ready for use

--- victorious/victorious/Managers/Download/VDownloadTaskInformation.h
+++ victorious/victorious/Managers/Download/VDownloadTaskInformation.h
@@ -8,13 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  ATTENTION: FOR DEMO PURPOSES ONLY
+ *
+ *  This has not been fully engineered for general use. Only supports one download task at a time.
+ *
+ */
 @interface VDownloadTaskInformation : NSObject
 
+/**
+ *  ATTENTION: FOR DEMO PURPOSES ONLY
+ */
 - (instancetype)initWithRequest:(NSURLRequest *)request
                downloadLocation:(NSURL *)downloadLocation NS_DESIGNATED_INITIALIZER;
 
+/**
+ *  ATTENTION: FOR DEMO PURPOSES ONLY
+ */
 @property (nonatomic, readonly) NSURLRequest *request;
 
+/**
+ *  ATTENTION: FOR DEMO PURPOSES ONLY
+ */
 @property (nonatomic, readonly) NSURL *downloadLocation;
 
 @end
