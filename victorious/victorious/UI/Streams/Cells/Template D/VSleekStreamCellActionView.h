bd999a1897d33b368e4949e0c25bdf34650ce9f0
Michael Sena <michaelsena@getvictorious.com>
2015-04-14 14:25:00 -0700

    Rename and move sleek cells in to Template D Subfolder/group

---
+++ victorious/victorious/UI/Streams/Cells/Template D/VSleekStreamCellActionView.h
@@ -0,0 +1,19 @@
+//
+//  VSleekStreamCellActionView.h
+//  victorious
+//
+//  Created by Sharif Ahmed on 3/13/15.
+//  Copyright (c) 2015 Victorious. All rights reserved.
+//
+
+#import "VStreamCellActionView.h"
+
+@interface VSleekStreamCellActionView : VStreamCellActionView
+
+- (void)updateCommentsCount:(NSNumber *)commentsCount;
+
+- (void)addCommentsButton;
+- (void)addGifButton;
+- (void)addMemeButton;
+
+@end
---
+++ victorious/victorious/UI/Streams/Cells/Template D/VSleekStreamCellFactory.m
@@ -0,0 +1,99 @@
+//
+//  VSleekStreamCellFactory.m
+//  victorious
+//
+//  Created by Sharif Ahmed on 3/17/15.
+//  Copyright (c) 2015 Victorious. All rights reserved.
+//
+
+#import "VSleekStreamCellFactory.h"
+#import "VSleekStreamCollectionCell.h"
+#import "VSleekStreamCollectionCellPoll.h"
+#import "VSequence+Fetcher.h"
+#import "VStreamCollectionCellWebContent.h"
+#import "VDependencyManager+VBackgroundContainer.h"
+
+@interface VSleekStreamCellFactory ()
+
+@property (nonatomic, readonly) VDependencyManager *dependencyManager;
+
+@end
+
+@implementation VSleekStreamCellFactory
+
+- (instancetype)initWithDependencyManager:(VDependencyManager *)dependencyManager
+{
+    self = [super init];
+    if ( self != nil )
+    {
+        _dependencyManager = dependencyManager;
+    }
+    return self;
+}
+
+- (void)registerCellsWithCollectionView:(UICollectionView *)collectionView
+{
+    [collectionView registerNib:[VSleekStreamCollectionCell nibForCell] forCellWithReuseIdentifier:[VSleekStreamCollectionCell suggestedReuseIdentifier]];
+    [collectionView registerNib:[VSleekStreamCollectionCellPoll nibForCell] forCellWithReuseIdentifier:[VSleekStreamCollectionCellPoll suggestedReuseIdentifier]];
+    [collectionView registerNib:[VStreamCollectionCellWebContent nibForCell] forCellWithReuseIdentifier:[VStreamCollectionCellWebContent suggestedReuseIdentifier]];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForStreamItem:(VStreamItem *)streamItem atIndexPath:(NSIndexPath *)indexPath
+{
+    NSAssert( [streamItem isKindOfClass:[VSequence class]], @"This factory can only handle VSequence objects" );
+    
+    VSequence *sequence = (VSequence *)streamItem;
+    VStreamCollectionCell *cell;
+    
+    if ( [sequence isPoll] )
+    {
+        cell = [collectionView dequeueReusableCellWithReuseIdentifier:[VSleekStreamCollectionCellPoll suggestedReuseIdentifier]
+                                                         forIndexPath:indexPath];
+    }
+    else if ([sequence isPreviewWebContent])
+    {
+        NSString *identifier = [VStreamCollectionCellWebContent suggestedReuseIdentifier];
+        VStreamCollectionCellWebContent *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier
+                                                                                          forIndexPath:indexPath];
+        cell.sequence = sequence;
+        [self.dependencyManager addLoadingBackgroundToBackgroundHost:cell];
+        return cell;
+    }
+    else
+    {
+        cell = [collectionView dequeueReusableCellWithReuseIdentifier:[VSleekStreamCollectionCell suggestedReuseIdentifier]
+                                                         forIndexPath:indexPath];
+    }
+    cell.dependencyManager = self.dependencyManager;
+    cell.sequence = sequence;
+    [self.dependencyManager addLoadingBackgroundToBackgroundHost:cell];
+    
+    return cell;
+}
+
+- (CGSize)sizeWithCollectionViewBounds:(CGRect)bounds ofCellForStreamItem:(VStreamItem *)streamItem
+{
+    NSAssert( [streamItem isKindOfClass:[VSequence class]], @"This factory can only handle VSequence objects" );
+    VSequence *sequence = (VSequence *)streamItem;
+    
+    if ( [sequence isPoll] )
+    {
+        return [VSleekStreamCollectionCellPoll actualSizeWithCollectionViewBounds:bounds sequence:sequence dependencyManager:self.dependencyManager];
+    }
+    else
+    {
+        return [VSleekStreamCollectionCell actualSizeWithCollectionViewBounds:bounds sequence:sequence dependencyManager:self.dependencyManager];
+    }
+}
+
+- (CGFloat)minimumLineSpacing
+{
+    return 1.0f;
+}
+
+- (UIEdgeInsets)sectionInsets
+{
+    return UIEdgeInsetsMake(0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+@end
