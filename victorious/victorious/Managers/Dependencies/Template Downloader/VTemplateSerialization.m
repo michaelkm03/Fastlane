64610264eb162ea06c6efe4221db0ad35ddfee61
Josh Hinman <josh@getvictorious.com>
2015-04-27 21:29:54 -0700

    New class: VTemplateSerialization for deserializing the template from JSON

---
+++ victorious/victorious/Managers/Dependencies/Template Downloader/VTemplateSerialization.m
@@ -0,0 +1,24 @@
+//
+//  VTemplateSerialization.m
+//  victorious
+//
+//  Created by Josh Hinman on 4/26/15.
+//  Copyright (c) 2015 Victorious. All rights reserved.
+//
+
+#import "VConstants.h"
+#import "VTemplateSerialization.h"
+
+@implementation VTemplateSerialization
+
++ (NSDictionary *)templateConfigurationDictionaryWithData:(NSData *)data
+{
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    if ( [json isKindOfClass:[NSDictionary class]] )
+    {
+        return json[kVPayloadKey];
+    }
+    return nil;
+}
+
+@end
