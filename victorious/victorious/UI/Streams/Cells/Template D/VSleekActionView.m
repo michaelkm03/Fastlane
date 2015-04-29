b3283843464fe8aa93f9cad3c68380b2b3bbd079
Michael Sena <michaelsena@getvictorious.com>
2015-04-29 16:51:14 -0700

    Fix accent color

--- victorious/victorious/UI/Streams/Cells/Template D/VSleekActionView.m
+++ victorious/victorious/UI/Streams/Cells/Template D/VSleekActionView.m
@@ -30,7 +30,6 @@ static CGFloat const kCommentSpaceToActions = 22.0f;
 static CGFloat const kInterActionSpace = 25.0f;
 static CGFloat const kCommentWidth = 68.0f;
 static CGFloat const kActionButtonHeight = 31.0f;
-static CGFloat const kActionBackgroundColorConstant = 238.0f / 255.0f;
 
 @interface VSleekActionView ()
 
@@ -213,10 +212,8 @@ static CGFloat const kActionBackgroundColorConstant = 238.0f / 255.0f;
 {
     VRoundedBackgroundButton *actionButton = [[VRoundedBackgroundButton alloc] initWithFrame:CGRectMake(0, 0, kActionButtonHeight, kActionButtonHeight)];
     actionButton.translatesAutoresizingMaskIntoConstraints = NO;
-    CGFloat colorVal = kActionBackgroundColorConstant;
-    actionButton.unselectedColor = [UIColor colorWithRed:colorVal green:colorVal blue:colorVal alpha:1.0f];
     actionButton.selected = NO;
-    actionButton.tintColor = [self.dependencyManager colorForKey:VDependencyManagerMainTextColorKey];
+    actionButton.tintColor = [self.dependencyManager colorForKey:VDependencyManagerSecondaryAccentColorKey];
     [actionButton setImage:[actionImage imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]
                   forState:UIControlStateNormal];
     [actionButton v_addWidthConstraint:kActionButtonHeight];
