1debe37c6c0aa505806496d110242933237999f2
feef <fifihypnophonic@gmail.com>
2015-05-05 11:18:18 -0700

    fixed documentation and removed unnecessary @class declarations

--- victorious/victorious/UI/Streams/DirectoryCells/VDirectoryCellFactory.h
+++ victorious/victorious/UI/Streams/DirectoryCells/VDirectoryCellFactory.h
@@ -9,20 +9,38 @@
 #import <UIKit/UIKit.h>
 #import "VStreamCellFactory.h"
 
-@class VStreamItem, VStream, VDependencyManager, VDirectoryCollectionFlowLayout;
+@class VDependencyManager, VDirectoryCollectionFlowLayout;
 
 @protocol VDirectoryCellFactory <NSObject, VStreamCellFactory>
 
 @required
-
+/**
+    @return A float representing the minimum space between cells
+ */
 - (CGFloat)minimumInterItemSpacing;
 
+/**
+    @return The collection view flow layout that will be used by the VDirectoryViewController's collection view
+ */
 - (VDirectoryCollectionFlowLayout *)collectionViewFlowLayout;
 
 @optional
 
+/**
+    Called by VDirectoryViewController in collectionView:willDisplayCell:atIndexPath.
+    Use this to adjust cell properties right before display.
+ 
+    @param cell The collection view cell that is about to be displayed
+    @param collectionView The collection view that is about to display the cell
+    @param indexPath The index path where the cell will be displayed
+ */
 - (void)prepareCell:(UICollectionViewCell *)cell forDisplayInCollectionView:(UICollectionView *)collectionView atIndexPath:(NSIndexPath *)indexPath;
 
+/**
+    Called by VDirectoryViewController in scrollViewDidScroll:
+ 
+    @param collectionView The collection view that has just scrolled
+ */
 - (void)collectionViewDidScroll:(UICollectionView *)collectionView;
 
 @end
