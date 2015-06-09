f57b80afa26bf200009e2562da62ef4887d0c3e0
feef <fifihypnophonic@gmail.com>
2015-06-09 15:10:19 -0700

    improved autoscrolling animation

--- victorious/victorious/UI/Marquee/VInsetMarqueeController.m
+++ victorious/victorious/UI/Marquee/VInsetMarqueeController.m
@@ -9,8 +9,47 @@
 #import "VInsetMarqueeController.h"
 #import "VInsetMarqueeCollectionViewCell.h"
 #import "VInsetMarqueeStreamItemCell.h"
+#import "VStream+Fetcher.h"
+#import "VTimerManager.h"
+
+static NSUInteger const kAnimationSteps = 44;
+static NSUInteger const kAnimationBounces = 1;
+static CGFloat const kAnimationBounceCoefficient = 0.008;
+
+@interface VInsetMarqueeController ()
+
+@property (nonatomic, assign) CGFloat targetXOffset;
+@property (nonatomic, assign) CGFloat animationDistance;
+@property (nonatomic, assign) NSUInteger animationIndex;
+@property (nonatomic, strong) NSArray *animationValues;
+@property (nonatomic, strong) CADisplayLink *displayLink;
+
+@end
 
 @implementation VInsetMarqueeController
+
+- (instancetype)initWithDependencyManager:(VDependencyManager *)dependencyManager
+{
+    self = [super initWithDependencyManager:dependencyManager];
+    if ( self != nil )
+    {
+        /*
+            See http://khanlou.com/2012/01/cakeyframeanimation-make-it-bounce/
+         */
+        int steps = kAnimationSteps;
+        NSMutableArray *values = [NSMutableArray arrayWithCapacity:steps];
+        double value = 0;
+        float e = 2.71;
+        NSUInteger bounces = kAnimationBounces * 2 + 1;
+        for (int t = 0; t < steps; t++)
+        {
+            value = pow(e, -kAnimationBounceCoefficient * t * t) * cos(bounces * M_PI_2 * t / kAnimationSteps);
+            [values addObject:[NSNumber numberWithFloat:value]];
+        }
+        _animationValues = values;
+    }
+    return self;
+}
 
 - (void)registerCollectionViewCellWithCollectionView:(UICollectionView *)collectionView
 {
@@ -24,6 +63,58 @@
     collectionViewCell.dependencyManager = self.dependencyManager;
     [self enableTimer];
     return collectionViewCell;
+}
+
+- (void)selectNextTab
+{
+    CGFloat pageWidth = CGRectGetWidth(self.collectionView.bounds);
+    NSUInteger currentPage = ( self.collectionView.contentOffset.x / pageWidth ) + 1;
+    if (currentPage == self.stream.marqueeItems.count)
+    {
+        currentPage = 0;
+    }
+    
+    [self animateToHorizontalOffset:currentPage * pageWidth];
+}
+
+- (void)animateToHorizontalOffset:(CGFloat)offset
+{
+    self.targetXOffset = offset;
+    self.animationDistance = self.targetXOffset - self.collectionView.contentOffset.x;
+    self.animationIndex = 0;
+    self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateAnimation)];
+    [self.displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+}
+
+- (void)updateAnimation
+{
+    CGPoint offset = self.collectionView.contentOffset;
+    if ( self.animationIndex >= kAnimationSteps)
+    {
+        offset.x = self.targetXOffset;
+        [self endAnimation];
+    }
+    else
+    {
+        CGFloat multiplier = [self.animationValues[self.animationIndex] floatValue];
+        offset.x = self.targetXOffset - multiplier * self.animationDistance;
+    }
+    self.animationIndex++;
+    self.collectionView.contentOffset = offset;
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [self endAnimation];
+}
+
+- (void)endAnimation
+{
+    if ( self.displayLink != nil )
+    {
+        [self.displayLink removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+        self.displayLink = nil;
+    }
 }
 
 - (CGSize)desiredSizeWithCollectionViewBounds:(CGRect)bounds
