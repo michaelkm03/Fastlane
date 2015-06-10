8ff180015a6d7fe006121bae3c71f45e5e1825c7
Steven F Petteruti <Steven@Stevens-Mac-mini.local>
2015-06-10 10:21:22 -0700

    added documentation

--- victorious/victorious/UI/Common/Following/VHashtagResponder.h
+++ victorious/victorious/UI/Common/Following/VHashtagResponder.h
@@ -11,7 +11,22 @@
 
 @protocol VHashtagResponder <NSObject>
 
+/**
+ *  A command for the current user to follow a specific hashtag.
+ *
+ *  @param hashtag The hashtag to follow
+ *  @param success The success block
+ *  @param failure The failure block
+ */
 - (void)followHashtag:(NSString *)hashtag successBlock:(void (^)(NSArray *))success failureBlock:(void (^)(NSError *))failure;
+
+/**
+ *  A command for the current user to unfollow a specific hashtag.
+ *
+ *  @param hashtag The hashtag to unfollow
+ *  @param success The success block
+ *  @param failure The failure block
+ */
 - (void)unfollowHashtag:(NSString *)hashtag successBlock:(void (^)(NSArray *))success failureBlock:(void (^)(NSError *))failure;
 
 @end
