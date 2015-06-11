3cce532df83ad7c6997c08f7b0017898df0d6749
Patrick Lynch <patrick@getvictorious.com>
2015-06-10 20:05:04 -0700

    Some more work on content thumbnail collection view.

--- victorious/victorious/UI/Suggested Users/VContentThumbnailsDataSource.m
+++ victorious/victorious/UI/Suggested Users/VContentThumbnailsDataSource.m
@@ -20,6 +20,20 @@
 
 #pragma mark - UICollectionViewDataSource
 
+- (instancetype)init
+{
+    self = [super init];
+    if ( self != nil )
+    {
+        _sequences = @[ @"http://media-dev-public.s3-website-us-west-1.amazonaws.com/35c8f250240c9fbe6720f3d931028560.jpg",
+                        @"http://media-dev-public.s3-website-us-west-1.amazonaws.com/5eaa665d637e71d63948b7981834d87a/640x640.jpg",
+                        @"http://media-dev-public.s3-website-us-west-1.amazonaws.com/296608a4541efe0b03c0fc5ff01bab40.jpg",
+                        @"http://media-dev-public.s3-website-us-west-1.amazonaws.com/20ac5446ccd935df7d7abc94544cb881/640x640.jpg",
+                        @"http://media-dev-public.s3-website-us-west-1.amazonaws.com/5eaa665d637e71d63948b7981834d87a/640x640.jpg" ];
+    }
+    return self;
+}
+
 - (void)registerCellsWithCollectionView:(UICollectionView *)collectionView
 {
     NSString *identifier = [VContentThumbnailCell preferredReuseIdentifier];
@@ -33,16 +47,16 @@
     VContentThumbnailCell *cell = (VContentThumbnailCell *)[collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
     if ( cell != nil )
     {
-        cell.backgroundColor = [UIColor orangeColor];
+        NSString *url = self.sequences[ indexPath.row ];
+        [cell setImageURL:[NSURL URLWithString:url]];
         return cell;
     }
-    
     return nil;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return 10;
+    return 1 + arc4random() % self.sequences.count;
 }
 
 @end
