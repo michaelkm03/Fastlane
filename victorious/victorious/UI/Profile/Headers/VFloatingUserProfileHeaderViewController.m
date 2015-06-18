816b53b767eebc9cd0a014425f9e8183fe2f287a
Steven4294 <spetteruti@college.harvard.edu>
2015-06-18 10:10:41 -0700

    Update VFloatingUserProfileHeaderViewController.m
    float notation

--- victorious/victorious/UI/Profile/Headers/VFloatingUserProfileHeaderViewController.m
+++ victorious/victorious/UI/Profile/Headers/VFloatingUserProfileHeaderViewController.m
@@ -50,7 +50,7 @@ static const CGFloat kFloatProfileImageSize = 57.0f;
     [super viewDidLoad];
     
     self.gradientView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.5f];
-    self.gradientView.colors = @[ [UIColor clearColor], [[UIColor blackColor] colorWithAlphaComponent:.65] ];
+    self.gradientView.colors = @[ [UIColor clearColor], [[UIColor blackColor] colorWithAlphaComponent:0.65f] ];
     self.gradientView.locations = @[ @0.5f, @1.0f ];
     
     self.primaryActionButtonStartTop = self.primaryActionButtonTopConstraint.constant;
