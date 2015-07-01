b0339e7e267eb7ce1e3ee5f43a5ad5521436b516
feef <fifihypnophonic@gmail.com>
2015-07-01 13:26:02 -0700

    revised logic around action buttons in inset and sleek cells

--- victorious/victorious/UI/Streams/Cells/Template D/VSleekActionView.m
+++ victorious/victorious/UI/Streams/Cells/Template D/VSleekActionView.m
@@ -215,9 +215,8 @@ static NSUInteger const kMaxNumberOfActionButtons = 4;
     {
         [actionButtons addObject:self.gifButton];
     }
-    
-    //Don't add meme if we already have our kMaxNumberOfActionButtons buttons to show
-    if ( sequence.permissions.canMeme && actionButtons.count != kMaxNumberOfActionButtons)
+    //Only add meme for image content and if we don't already have our kMaxNumberOfActionButtons buttons to show
+    if ( sequence.permissions.canMeme && sequence.isImage && actionButtons.count != kMaxNumberOfActionButtons )
     {
         [actionButtons addObject:self.memeButton];
     }
