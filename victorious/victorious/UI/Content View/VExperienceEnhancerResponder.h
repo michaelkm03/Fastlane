3e0c656a8c96268e91c0bf398828ba41ac8b89e0
Patrick Lynch <patrick@getvictorious.com>
2015-07-08 10:37:57 -0700

    Fixed locked purchase/login logic and added comments.

--- victorious/victorious/UI/Content View/VExperienceEnhancerResponder.h
+++ victorious/victorious/UI/Content View/VExperienceEnhancerResponder.h
@@ -8,10 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Responder to handle events from experience enhancer/emotive ballistics UI.
+ */
 @protocol VExperienceEnhancerResponder <NSObject>
 
+/**
+ Presents a view controller in the appropriate place in the view controller
+ hierarcy that will take the user through purchasing the VVoteType instance provided.
+ */
 - (void)showPurchaseViewController:(VVoteType *)voteType;
 
+/**
+ Presents a login/signup view controller in the appropriate place in the view
+ controller hierarchy and calls the completion block when the process
+ is complete.  The BOOL parameter of the completion block indicates whether
+ or not the user completed the authorization process suggessfully and is now
+ logged in.
+ */
 - (void)authorizeWithCompletion:(void(^)(BOOL))completion;
 
 @end
