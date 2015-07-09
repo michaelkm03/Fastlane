285b3b70856cca105b3aa66fd88a68716e2ea594
Josh Hinman <josh@getvictorious.com>
2015-07-08 18:13:23 -0700

    #ifdef instead of #if

--- victorious/victorious/UI/Root View/VRootViewController.m
+++ victorious/victorious/UI/Root View/VRootViewController.m
@@ -436,7 +436,7 @@ typedef NS_ENUM(NSInteger, VAppLaunchState)
         return;
     }
     
-#if V_SWITCH_ENVIRONMENTS
+#ifdef V_SWITCH_ENVIRONMENTS
     NSError *environmentError = notification.userInfo[ VEnvironmentErrorKey ];
     if ( environmentError != nil )
     {
