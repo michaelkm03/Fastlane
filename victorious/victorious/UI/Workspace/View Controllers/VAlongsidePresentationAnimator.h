37ed735d35cfb3a76ddaa1e59a44bfa30f688f5c
Michael Sena <michaelsena@getvictorious.com>
2015-07-21 12:31:59 -0700

    Comment to animator

--- victorious/victorious/UI/Workspace/View Controllers/VAlongsidePresentationAnimator.h
+++ victorious/victorious/UI/Workspace/View Controllers/VAlongsidePresentationAnimator.h
@@ -10,12 +10,25 @@
 
 @protocol VAlongsidePresentation <NSObject>
 
+/**
+ *  Use this method to animate alongside presentation.
+ */
 - (void)alongsidePresentation;
 
+/**
+ *  Use this method to animate alongside dismissal.
+ */
 - (void)alongsideDismissal;
 
 @end
 
+/**
+ *  VAlongsidePresentationAnimator is a presentation animator that provides the destination
+ *  with hooks to add their own animations that occur alongside the presentation animation.
+ *  When a VAlongsidePresentationAnimator is the transition animator for the destinaiton
+ *  it should implement the VAlongsidePresentation protocol to customize their animations 
+ *  that occur alongside presentation/dismissal.
+ */
 @interface VAlongsidePresentationAnimator : NSObject <UIViewControllerAnimatedTransitioning>
 
 @property (nonatomic, assign) BOOL presenting;
