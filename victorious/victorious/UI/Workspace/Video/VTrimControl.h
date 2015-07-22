5f7cdc3266740b77f318872eeebe9addc0336026
feef <fifihypnophonic@gmail.com>
2015-07-22 10:24:06 -0700

    fixes for trim control layout issues

--- victorious/victorious/UI/Workspace/Video/VTrimControl.h
+++ victorious/victorious/UI/Workspace/Video/VTrimControl.h
@@ -11,9 +11,6 @@
 #import <CoreMedia/CMTime.h>
 #import <CoreMedia/CMTimeRange.h>
 
-// maximum height of the trim control
-extern CGFloat const VTrimControlMaximumHeight;
-
 // Sends UIControlEventValueChanged on new selection
 
 /**
@@ -22,19 +19,9 @@ extern CGFloat const VTrimControlMaximumHeight;
 @interface VTrimControl : UIControl
 
 /**
- *  Updates max duration that the trim control can select. Doesn't send control events.
- */
-@property (nonatomic, assign) CMTime maxDuration;
-
-/**
  *  The text to display in the thumb head.
  */
 @property (nonatomic, copy) NSAttributedString *attributedTitle;
-
-/**
- *  The current time duration selected by the trim control.
- */
-@property (nonatomic, readonly) CMTime selectedDuration;
 
 /**
  *  The height of the trimmer body plus the vertical inset of the body
