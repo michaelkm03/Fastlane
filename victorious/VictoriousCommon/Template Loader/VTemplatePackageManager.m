1466eef1de2c3eb3d0f637bed23ea95e2385ffb2
Josh Hinman <josh@getvictorious.com>
2015-07-22 17:50:07 -0700

    Create new static library target for OSX

--- victorious/VictoriousCommon/Template Loader/VTemplatePackageManager.m
+++ victorious/VictoriousCommon/Template Loader/VTemplatePackageManager.m
@@ -6,7 +6,6 @@
 //  Copyright (c) 2015 Victorious. All rights reserved.
 //
 
-#import "VDependencyManager.h"
 #import "VTemplateImage.h"
 #import "VTemplateImageMacro.h"
 #import "VTemplateImageSet.h"
---
+++ victorious/downloadtemplate/SwiftBridging.h
@@ -0,0 +1,9 @@
+//
+//  SwiftBridging.h
+//  victorious
+//
+//  Created by Josh Hinman on 7/22/15.
+//  Copyright (c) 2015 Victorious. All rights reserved.
+//
+
+#import "VTemplateDownloadOperation.h"
