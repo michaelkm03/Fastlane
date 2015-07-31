21aa71118efa8748345f8f094acbb2b191810465
Josh Hinman <josh@getvictorious.com>
2015-07-31 16:11:55 -0700

    Move call to exportSelectedItem() to its rightful place in didSelectItemAtIndexPath

--- victorious/victorious/UI/GIFSearch/GIFSearchViewController+Delegates.swift
+++ victorious/victorious/UI/GIFSearch/GIFSearchViewController+Delegates.swift
@@ -66,15 +66,14 @@ extension GIFSearchViewController : UICollectionViewDelegate {
                 self.showPreviewForResult( indexPath )
             }
         }
+        else if collectionView.cellForItemAtIndexPath( indexPath ) is GIFSearchPreviewCell {
+            self.exportSelectedItem( nil )
+        }
     }
     
     func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
         let cell = collectionView.cellForItemAtIndexPath( indexPath )
-        if cell is GIFSearchPreviewCell {
-            self.exportSelectedItem( nil )
-            return false
-        }
-        return cell is GIFSearchResultCell
+        return cell is GIFSearchResultCell || cell is GIFSearchPreviewCell
     }
     
     func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
