1dd917088fee151b95b7362989d6c52a30019aa7
Josh Hinman <josh@getvictorious.com>
2015-08-04 20:10:22 -0700

    Make VictoriousCommon into a Framework

---
+++ victorious/VictoriousCommon/VictoriousCommon.h
@@ -0,0 +1,19 @@
+//
+//  VictoriousCommon.h
+//  VictoriousCommon
+//
+//  Created by Josh Hinman on 8/4/15.
+//  Copyright (c) 2015 Victorious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VictoriousCommon.
+FOUNDATION_EXPORT double VictoriousCommonVersionNumber;
+
+//! Project version string for VictoriousCommon.
+FOUNDATION_EXPORT const unsigned char VictoriousCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VictoriousCommon/PublicHeader.h>
+
+
