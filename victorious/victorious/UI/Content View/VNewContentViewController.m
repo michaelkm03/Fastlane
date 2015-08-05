322f701ce9bf8bdd137ad0d42f6f02226ba597a4
Steven F Petteruti <Steven@Stevens-Mac-mini.local>
2015-08-05 13:46:14 -0700

    call reloadComments on view will appear

--- victorious/victorious/UI/Content View/VNewContentViewController.m
+++ victorious/victorious/UI/Content View/VNewContentViewController.m
@@ -462,6 +462,7 @@ static NSString * const kPollBallotIconKey = @"orIcon";
 {
     [super viewWillAppear:animated];
     
+    [self reloadComments];
     [self.dependencyManager trackViewWillAppear:self];
     
     
