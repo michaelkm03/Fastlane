26c90206b7e38a7a88a8774f0c040c3997d02687
Josh Hinman <josh@getvictorious.com>
2015-08-10 14:24:54 -0700

    Only download the template for the default environment being built

--- victorious/downloadtemplate/main.swift
+++ victorious/downloadtemplate/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 let appName = Process.arguments.first ?? "downloadtemplate"
-let usage = "Usage: \(appName.lastPathComponent) <path to application bundle>\n"
+let usage = "Usage: \(appName.lastPathComponent) <path to application bundle> [environment name]\n"
 let environmentsFilename = "environments.plist"
 
 if Process.arguments.count < 2 {
@@ -18,9 +18,14 @@ if Process.arguments.count < 2 {
 }
 let bundlePath = Process.arguments[1]
 
+var environmentName: String? = nil
+if Process.arguments.count >= 2 {
+    environmentName = Process.arguments[2]
+}
+
 if let bundleURL = NSURL(fileURLWithPath: bundlePath, isDirectory: true) {
     let cli = TemplateDownloadCLI(bundleURL: bundleURL)
-    cli.downloadTemplate()
+    cli.downloadTemplate(environmentName: environmentName)
 }
 else {
     println("Invalid bundle: \(bundlePath)\n")
