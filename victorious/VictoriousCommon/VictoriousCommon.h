2ab19975e1829c38b7b3af2f2302cccefec659fe
Josh Hinman <josh@getvictorious.com>
2015-08-10 18:37:51 -0700

    Create bridging header

--- victorious/VictoriousCommon/VictoriousCommon.h
+++ victorious/VictoriousCommon/VictoriousCommon.h
@@ -16,4 +16,23 @@ FOUNDATION_EXPORT const unsigned char VictoriousCommonVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <VictoriousCommon/PublicHeader.h>
 
-
+#import <VictoriousCommon/NSCharacterSet+VURLParts.h>
+#import <VictoriousCommon/NSString+VCrypto.h>
+#import <VictoriousCommon/NSString+VDataCacheID.h>
+#import <VictoriousCommon/NSURL+VDataCacheID.h>
+#import <VictoriousCommon/VBundleWriterDataCache.h>
+#import <VictoriousCommon/VDataCache.h>
+#import <VictoriousCommon/VEnvironment+VDataCacheID.h>
+#import <VictoriousCommon/VTemplateImage.h>
+#import <VictoriousCommon/VTemplateImageMacro.h>
+#import <VictoriousCommon/VTemplateImageSet.h>
+#import <VictoriousCommon/VURLMacroReplacement.h>
+#import <VictoriousCommon/VEnvironment.h>
+#import <VictoriousCommon/VExperimentSettings.h>
+#import <VictoriousCommon/VJSONHelper.h>
+#import <VictoriousCommon/VBulkDownloadOperation.h>
+#import <VictoriousCommon/VDownloadOperation.h>
+#import <VictoriousCommon/VTemplateDownloadOperation.h>
+#import <VictoriousCommon/VTemplatePackageManager.h>
+#import <VictoriousCommon/VTemplateSerialization.h>
+#import <VictoriousCommon/VAPIRequestDecorator.h>
--- victorious/VictoriousCommon/VictoriousCommonOSX.h
+++ victorious/VictoriousCommon/VictoriousCommonOSX.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Victorious. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for VictoriousCommon.
 FOUNDATION_EXPORT double VictoriousCommonVersionNumber;
@@ -14,6 +14,25 @@ FOUNDATION_EXPORT double VictoriousCommonVersionNumber;
 //! Project version string for VictoriousCommon.
 FOUNDATION_EXPORT const unsigned char VictoriousCommonVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <VictoriousCommon/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <VictoriousCommonOSX/PublicHeader.h>
 
-
+#import <VictoriousCommonOSX/NSCharacterSet+VURLParts.h>
+#import <VictoriousCommonOSX/NSString+VCrypto.h>
+#import <VictoriousCommonOSX/NSString+VDataCacheID.h>
+#import <VictoriousCommonOSX/NSURL+VDataCacheID.h>
+#import <VictoriousCommonOSX/VBundleWriterDataCache.h>
+#import <VictoriousCommonOSX/VDataCache.h>
+#import <VictoriousCommonOSX/VEnvironment+VDataCacheID.h>
+#import <VictoriousCommonOSX/VTemplateImage.h>
+#import <VictoriousCommonOSX/VTemplateImageMacro.h>
+#import <VictoriousCommonOSX/VTemplateImageSet.h>
+#import <VictoriousCommonOSX/VURLMacroReplacement.h>
+#import <VictoriousCommonOSX/VEnvironment.h>
+#import <VictoriousCommonOSX/VExperimentSettings.h>
+#import <VictoriousCommonOSX/VJSONHelper.h>
+#import <VictoriousCommonOSX/VBulkDownloadOperation.h>
+#import <VictoriousCommonOSX/VDownloadOperation.h>
+#import <VictoriousCommonOSX/VTemplateDownloadOperation.h>
+#import <VictoriousCommonOSX/VTemplatePackageManager.h>
+#import <VictoriousCommonOSX/VTemplateSerialization.h>
+#import <VictoriousCommonOSX/VAPIRequestDecorator.h>
