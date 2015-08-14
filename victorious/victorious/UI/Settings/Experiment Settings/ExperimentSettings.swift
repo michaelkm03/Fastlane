bac05e1900f98a429925237f886acbd369d37829
Josh Hinman <josh@getvictorious.com>
2015-08-14 15:41:18 -0700

    A Swift version of VExperimentSettings!

---
+++ victorious/victorious/UI/Settings/Experiment Settings/ExperimentSettings.swift
@@ -0,0 +1,59 @@
+//
+//  ExperimentSettings.swift
+//  victorious
+//
+//  Created by Josh Hinman on 8/14/15.
+//  Copyright (c) 2015 Victorious. All rights reserved.
+//
+
+import Foundation
+
+private let kActiveExperimentsKey = "com.getvictorious.experiments.active_experiments";
+
+/// Stores a list of active experiment IDs kept synched internally by NSUserDefaults.
+/// Multiple instances of this class can be used independenctly and NSUserDefaults
+/// will keep values synched.
+class ExperimentSettings {
+    
+    /// A set of the user-selected experiments to be active in all subsequent backend interactions
+    var activeExperiments: Set<Int>? {
+        get {
+            if let activeExperiments = NSUserDefaults.standardUserDefaults().objectForKey(kActiveExperimentsKey) as? [Int] {
+                return Set(activeExperiments)
+            }
+            return nil
+        }
+        set(experiments) {
+            let userDefaults = NSUserDefaults.standardUserDefaults()
+            if let experiments = experiments {
+                userDefaults.setObject(Array(experiments), forKey: kActiveExperimentsKey)
+            }
+            else {
+                userDefaults.removeObjectForKey(kActiveExperimentsKey)
+            }
+            userDefaults.synchronize()
+        }
+    }
+    
+    /// Removes all active experiments and clears value in NSUserDefaults.
+    /// This will return the application to membership in experiments as determined by the backend,
+    /// essentially undoing and previously user-selected experiment memberships.
+    func reset() {
+        activeExperiments = nil
+    }
+    
+    /// Returns a command-separated list of hte active experiments for use in request header.
+    func commaSeparatedList() -> String? {
+        
+        if let activeExperiments = self.activeExperiments {
+            // An empty string used in a header indicates to the backend that the user has
+            // manually opted out of all experiments
+            return ",".join( map(activeExperiments) { "\($0)" } )
+        }
+        else {
+            // A nil value used in a header indicates to the backend that the user does not wish
+            // to deviate from the default experiment membership
+            return nil
+        }
+    }
+}
