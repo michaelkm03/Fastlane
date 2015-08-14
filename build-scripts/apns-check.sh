c2c3101e1f4ccd3a59513fbb8d1ed0ac3e38de84
Lawrence Leach <lawrence@lawrenceleach.com>
2015-08-14 16:11:49 -0700

    CMS-1188 Removed debug echo comments

--- build-scripts/apns-check.sh
+++ build-scripts/apns-check.sh
@@ -6,11 +6,11 @@
 IPA_FILE=$1
 CURRENT_DIRECTORY=$(pwd)
 WORKING_DIRECTORY="$CURRENT_DIRECTORY/temp"
-CODESIGN_PLIST="$CURRENT_DIRECTORY/codesign.plist"
-SECURITY_PLIST="$CURRENT_DIRECTORY/security.plist"
+CODESIGN_PLIST="$CURRENT_DIRECTORY/temp/codesign.plist"
+SECURITY_PLIST="$CURRENT_DIRECTORY/temp/security.plist"
 
 showProperUsage(){
-    echo "Extracts an IPA archive and checks for APNS enablement."
+    echo "Extracts an IPA archive and checks for APNS entitlements."
     echo ""
     echo "Usage: `basename $0` <ipa file>"
     echo ""
@@ -23,32 +23,27 @@ fi
 
 if [ ${IPA_FILE: -4} == ".ipa" ]; then
 
-    mkdir $WORKING_DIRECTORY
+    if [ ! -d "$WORKING_DIRECTORY" ]; then
+        mkdir $WORKING_DIRECTORY
+    fi
     cp "$IPA_FILE" "$WORKING_DIRECTORY"
 
     NEW_IPA_FILE=$(basename "$IPA_FILE")
     mv "$WORKING_DIRECTORY/$NEW_IPA_FILE" "$WORKING_DIRECTORY/VictoriousApp.zip"
 
-    echo "Unarchiving ipa file ($IPA_FILE)"
     UNZIP=$(unzip -x "$WORKING_DIRECTORY/VictoriousApp.zip" -d "$WORKING_DIRECTORY" 2> /dev/null)
 
-    echo "Checking Binary Code Signing"
     codesign -d --entitlements :- $WORKING_DIRECTORY/Payload/victorious.app > $CODESIGN_PLIST
     CODESIGNING_ENTITLEMENT=$(/usr/libexec/PlistBuddy -c "Print:aps-environment" "$CODESIGN_PLIST" 2> /dev/null)
     echo "Code Signing Entitlement = $CODESIGNING_ENTITLEMENT"
     echo ""
 
-    echo "Checking Provisioning Profile"
     security cms -D < $WORKING_DIRECTORY/Payload/victorious.app/embedded.mobileprovision >$SECURITY_PLIST
     PROVISIONING_ENTITLEMENT=$(/usr/libexec/PlistBuddy -c "Print:Entitlements:aps-environment" "$SECURITY_PLIST" 2>/dev/null)
     echo "Provisioning Profile Entitlement = $PROVISIONING_ENTITLEMENT"
     echo ""
 
-    echo "Clean up... removing temp directory and plist files"
     rm -fdr $WORKING_DIRECTORY
-    rm $CODESIGN_PLIST
-    rm $SECURITY_PLIST
-    echo "DONE!"
 
 else
     showProperUsage
