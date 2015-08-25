47c9de26de4e75553bd883b66456f22eae028ff4
Cody Kolodziejzyk <cody@getvictorious.com>
2015-08-25 14:35:33 -0700

    adding loading background

--- victorious/victorious/UI/Streams/Cells/TrendingTopicContentCollectionViewCell.swift
+++ victorious/victorious/UI/Streams/Cells/TrendingTopicContentCollectionViewCell.swift
@@ -28,13 +28,13 @@ class TrendingTopicContentCollectionViewCell: VBaseCollectionViewCell, VStreamCe
             self.label.text = streamItem?.name ?? ""
             if let previewImageURL = (streamItem?.previewImagesObject as? String),
                 url = NSURL(string: previewImageURL)  {
-                imageView.sd_setImageWithURL(url, placeholderImage: nil, completed: {[weak self] (image, error, cacheType, url) -> Void in
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        if let strongSelf = self {
-                            strongSelf.image = image
-                        }
-                    })
-                })
+//                imageView.sd_setImageWithURL(url, placeholderImage: nil, completed: {[weak self] (image, error, cacheType, url) -> Void in
+//                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                        if let strongSelf = self {
+//                            strongSelf.image = image
+//                        }
+//                    })
+//                })
             }
         }
     }
@@ -123,7 +123,7 @@ class TrendingTopicContentCollectionViewCell: VBaseCollectionViewCell, VStreamCe
 extension TrendingTopicContentCollectionViewCell: VBackgroundContainer {
     
     func loadingBackgroundContainerView() -> UIView! {
-        return contentView
+        return screenView
     }
 }
 
