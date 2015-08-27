37827b3a79c2d53d202c698547232e89f9250e4a
Cody Kolodziejzyk <cody@getvictorious.com>
2015-08-26 17:33:05 -0700

    Fixed in-stream comment avatar not appearing

--- victorious/victorious/UI/Common/VDefaultProfileButton.m
+++ victorious/victorious/UI/Common/VDefaultProfileButton.m
@@ -87,7 +87,7 @@
                             UIImage *roundedImage = [image roundedImageWithCornerRadius:image.size.height / 2];
                             dispatch_async(dispatch_get_main_queue(), ^
                                            {
-                                               [strongSelf setImage:roundedImage forState:controlState];
+                                               [strongSelf setBackgroundImage:roundedImage forState:controlState];
                                            });
                         });
      }];
