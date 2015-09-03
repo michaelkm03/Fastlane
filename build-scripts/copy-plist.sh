53b936f96ff65c76990a80aea69aa4dcd82f9c9c
Josh Hinman <josh@getvictorious.com>
2015-09-02 17:26:18 -0700

    Generate all URL schemes at build time/configuration time

--- build-scripts/copy-plist.sh
+++ build-scripts/copy-plist.sh
@@ -5,12 +5,11 @@
 
 SOURCE=$1
 DESTINATION=$2
-APP_ID=$3
-P_FLAG=$4
-PRODUCT_PREFIX=$5
+P_FLAG=$3
+PRODUCT_PREFIX=$4
 
-if [ "$SOURCE" == "" -o "$DESTINATION" == "" -o "$APP_ID" == "" ]; then
-    echo "Usage: $0 <source> <destination> <app id> [-p <PRODUCT_PREFIX>]"
+if [ "$SOURCE" == "" -o "$DESTINATION" == "" ]; then
+    echo "Usage: $0 <source> <destination> [-p <PRODUCT_PREFIX>]"
     echo ""
     echo "PRODUCT_PREFIX, if supplied, will be used to replace instances of \${ProductPrefix}."
     exit 1
@@ -50,28 +49,3 @@ copyPListValue 'TWITTER_CONSUMER_KEY'
 copyPListValue 'TWITTER_CONSUMER_SECRET'
 copyPListValue 'FlurryAPIKey'
 copyPListValue 'CreatorSalutation'
-
-
-########### Generate Facebook URL Scheme
-## NOTE: This URL scheme will be modified at build time, but it needs to be set here initially.
-
-/usr/libexec/PlistBuddy -c "Delete CFBundleURLTypes" "$DESTINATION"
-/usr/libexec/PlistBuddy -c "Add CFBundleURLTypes Array" "$DESTINATION"
-
-FB_APPID=$(/usr/libexec/PlistBuddy -c "Print :FacebookAppID" "$SOURCE" 2> /dev/null)
-/usr/libexec/PlistBuddy -c "Add CFBundleURLTypes:0:CFBundleURLName string com.getvictorious.facebook" "$DESTINATION"
-/usr/libexec/PlistBuddy -c "Add CFBundleURLTypes:0:CFBundleURLSchemes Array" "$DESTINATION"
-/usr/libexec/PlistBuddy -c "Add CFBundleURLTypes:0:CFBundleURLSchemes: string fb${FB_APPID}${FB_SCHEME_SUFFIX}" "$DESTINATION"
-
-
-########### Generate Custom URL Scheme for app
-
-if [ "$P_FLAG" != "-p" ]; then
-    PRODUCT_PREFIX=""
-fi
-
-CUSTOM_SCHEME="${PRODUCT_PREFIX}vapp${APP_ID}"
-
-/usr/libexec/PlistBuddy -c "Add CFBundleURLTypes:1:CFBundleURLName string com.getvictorious.deeplinking.scheme" "$DESTINATION"
-/usr/libexec/PlistBuddy -c "Add CFBundleURLTypes:1:CFBundleURLSchemes Array" "$DESTINATION"
-/usr/libexec/PlistBuddy -c "Add CFBundleURLTypes:1:CFBundleURLSchemes: string $CUSTOM_SCHEME" "$DESTINATION"
