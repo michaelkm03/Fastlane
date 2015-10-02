5a45ae50a6c1ee9b0a37652fdaf1c6f88bb38c1e
Sky Lan <4dreams.sky@gmail.com>
2015-10-02 12:49:30 -0700

    Add a comment for our sin

--- victorious/victorious/UI/Content View/VContentViewFactory.m
+++ victorious/victorious/UI/Content View/VContentViewFactory.m
@@ -162,6 +162,7 @@ static NSString * const kSequenceIdKey = @"sequenceId";
 
 - (VDependencyManager *)contentViewDependencyManager
 {
+    // TODO: We should not be extracting a dictionary here. Will refactory later with correct use of templateValueOfType:forKey
     NSDictionary *configuration = [self templateValueOfType:[NSDictionary class] forKey:kContentViewComponentKey];
     VDependencyManager *dependencyManager = [[VDependencyManager alloc] initWithParentManager:nil
                                                                                 configuration:configuration
