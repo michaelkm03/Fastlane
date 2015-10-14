74d9478d9332ad5b98cf6c33b8a0052fc54ff5e5
Josh Hinman <josh@getvictorious.com>
2015-10-13 22:12:20 -0700

    pod update

--- victorious/Pods/Target Support Files/Nocilla/Nocilla-umbrella.h
+++ victorious/Pods/Target Support Files/Nocilla/Nocilla-umbrella.h
@@ -1,22 +1,17 @@
 #import <UIKit/UIKit.h>
 
-#import "Nocilla.h"
-#import "LSNocilla.h"
-#import "DSL.h"
+#import "NSData+Nocilla.h"
+#import "NSString+Nocilla.h"
 #import "LSStubRequestDSL.h"
 #import "LSStubResponseDSL.h"
 #import "LSBlockResponse.h"
+#import "LSNocilla.h"
 #import "LSMatcheable.h"
-#import "LSMatcher.h"
-#import "LSRegexMatcher.h"
-#import "LSStringMatcher.h"
+#import "NSData+Matcheable.h"
 #import "NSRegularExpression+Matcheable.h"
 #import "NSString+Matcheable.h"
 #import "LSHTTPBody.h"
-#import "NSString+Nocilla.h"
-#import "NSData+Nocilla.h"
-#import "LSHTTPClientHook.h"
-#import "LSASIHTTPRequestHook.h"
+#import "Nocilla.h"
 
 FOUNDATION_EXPORT double NocillaVersionNumber;
 FOUNDATION_EXPORT const unsigned char NocillaVersionString[];
