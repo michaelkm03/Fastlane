a2f94cbff7d8869db7d0b7cd73cf4483bce8d5dc
Cody Kolodziejzyk <cody@getvictorious.com>
2015-10-20 15:59:06 -0700

    Registration test

--- victorious/victoriousUITests/victoriousUITests.swift
+++ victorious/victoriousUITests/victoriousUITests.swift
@@ -63,5 +63,34 @@ class victoriousUITests: XCTestCase {
     
     func testRegister() {
         
+        app.buttons["Registration Email"].tap()
+        
+        let signupEmailFieldTextField = app.textFields["Signup Email Field"]
+        signupEmailFieldTextField.tap()
+        let randomEmail = "user\(self.generateRandomInteger())@user.com"
+        signupEmailFieldTextField.typeText(randomEmail)
+        
+        let signupPasswordFieldSecureTextField = app.secureTextFields["Signup Password Field"]
+        signupPasswordFieldSecureTextField.tap()
+        signupPasswordFieldSecureTextField.typeText("password")
+        app.typeText("\n")
+        
+        // Check if loading screen shows up
+        XCTAssert(app.navigationBars["victorious.ModernLoadingView"].exists)
+        
+        app.textFields["Signup Username Field"].typeText("Test")
+        app.navigationBars["VModernEnterNameView"].buttons["Next"].tap()
+        app.navigationBars["VEnterProfilePictureCameraView"].buttons["Next"].tap()
+        app.navigationBars["VSuggestedUsersView"].buttons["Done"].tap()
+        
+        // Make sure login screen still exists
+        let suggestedUsersScreen = app.navigationBars["VSuggestedUsersView"]
+        let doesNotExist = NSPredicate(format: "exists == false")
+        expectationForPredicate(doesNotExist, evaluatedWithObject: suggestedUsersScreen, handler: nil)
+        waitForExpectationsWithTimeout(loginTimeout, handler: nil)
+    }
+    
+    func generateRandomInteger() -> Int {
+        return 100000 + Int(arc4random_uniform(899999))
     }
 }
