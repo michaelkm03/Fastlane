9ada4ed6760e1e13c71e51b37df41662e173f1f3
Alex Tamoykin <alex@rightbalance.io>
2015-10-27 10:22:46 -0700

    Submit submission result

--- victorious/deployment/lib/vams/submission_result.rb
+++ victorious/deployment/lib/vams/submission_result.rb
@@ -1,7 +1,12 @@
 require 'active_support/json'
+require 'httparty'
 
 module VAMS
   class SubmissionResult < Hash
+    include HTTParty
+    base_uri 'example.com'
+    PATH = '/submission_result'
+
     attr_reader :id,
                 :status,
                 :datetime
@@ -10,6 +15,11 @@ module VAMS
       self['id']       = id
       self['status']   = status
       self['datetime'] = datetime
+    end
+
+    def submit
+      options = { body: self.to_json }
+      self.class.post(PATH, options)
     end
   end
 end
--- victorious/deployment/test/lib/submission_result_test.rb
+++ victorious/deployment/test/lib/submission_result_test.rb
@@ -3,11 +3,19 @@ require 'vams/submission_result'
 
 module VAMS
   class SubmissionResultTest < Minitest::Test
-    def test_response_json
+    def setup
       timestamp = Time.parse("2015-03-10 01:39:34")
-      result    = SubmissionResult.new(id: 1, status: 'All good', datetime: timestamp)
-      response_json = result.to_json
-      assert_equal('{"id":1,"status":"All good","datetime":"2015-03-10T01:39:34.000-07:00"}', response_json)
+      @result   = SubmissionResult.new(id: 1, status: 'All good', datetime: timestamp)
+    end
+
+    def test_json_payload
+      json_payload = @result.to_json
+      assert_equal('{"id":1,"status":"All good","datetime":"2015-03-10T01:39:34.000-07:00"}', json_payload)
+    end
+
+    def test_response_submission
+      response = @result.submit
+      assert_equal(404, response.code)
     end
   end
 end
