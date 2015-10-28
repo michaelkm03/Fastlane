51678fb538526f3a0fd0a703f4cbcae4994b557e
Alex Tamoykin <alex@rightbalance.io>
2015-10-27 22:48:24 -0700

    Stub http request in test using webmock

--- victorious/deployment/test/lib/submission_result_test.rb
+++ victorious/deployment/test/lib/submission_result_test.rb
@@ -6,16 +6,21 @@ module VAMS
     def setup
       timestamp = Time.parse("2015-03-10 01:39:34")
       @result   = SubmissionResult.new(id: 1, status: 'All good', datetime: timestamp)
+      @request_json = "{\"id\":1,\"status\":\"All good\",\"datetime\":\"2015-03-10T01:39:34.000-07:00\"}"
     end
 
     def test_json_payload
       json_payload = @result.to_json
-      assert_equal('{"id":1,"status":"All good","datetime":"2015-03-10T01:39:34.000-07:00"}', json_payload)
+      assert_equal(@request_json, json_payload)
     end
 
     def test_response_submission
+      response_code = 404
+      stub_request(:post, 'http://example.com/submission_result').
+        with(body: @request_json).
+        to_return(status: response_code, body: '')
       response = @result.submit
-      assert_equal(404, response.code)
+      assert_equal(response_code, response.code)
     end
   end
 end
--- victorious/deployment/test/test_helper.rb
+++ victorious/deployment/test/test_helper.rb
@@ -1,3 +1,4 @@
 require 'minitest/autorun'
 require 'minitest/rg'
+require 'webmock/minitest'
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
