eaa84a23401ca6afdbb16d2ab54638ec2e74829c
Alex Tamoykin <alex@rightbalance.io>
2015-10-28 00:55:35 -0700

    Integrate the apply_config.sh script into fastlane

--- victorious/fastlane/test/lib/client_test.rb
+++ victorious/fastlane/test/lib/client_test.rb
@@ -6,11 +6,11 @@ module VAMS
     def test_retrieves_apps_to_build
       apps = Client.apps_to_build
       first_app = apps.first
-      assert_equal(5,         apps.count)
-      assert_equal(1,         first_app.id)
-      assert_equal('app one', first_app.name)
-      assert_equal('AppOne',  first_app.build_name)
-      assert_equal(false,     first_app.submit_for_review)
+      assert_equal(2,               apps.count)
+      assert_equal(1,               first_app.id)
+      assert_equal('Leachy Peachy', first_app.name)
+      assert_equal('LeachyPeachy',  first_app.build_name)
+      assert_equal(false,           first_app.submit_for_review)
     end
   end
 end
