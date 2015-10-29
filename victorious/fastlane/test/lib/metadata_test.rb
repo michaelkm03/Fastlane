2c3ba7fdf873f8bff91e4cef2057887adbcbc8cb
Alex Tamoykin <alex@rightbalance.io>
2015-10-29 14:32:16 -0700

    Add metadata into test payload

--- victorious/fastlane/test/lib/metadata_test.rb
+++ victorious/fastlane/test/lib/metadata_test.rb
@@ -10,14 +10,14 @@ module VAMS
       app      = Client.app_by_build_name('LeachyPeachy')
       metadata = Metadata.new(app)
 
-      assert_equal(nil,                      metadata.copyright)
-      assert_equal(nil,                      metadata.ios_primary_category)
-      assert_equal(nil,                      metadata.ios_secondary_category)
-      assert_equal('Teach me how to peachy', metadata.ios_description)
-      assert_equal(nil,                      metadata.ios_keywords)
-      assert_equal('Leachy Peachy',          metadata.app_name)
-      assert_equal(nil,                      metadata.privacy_policy_url)
-      assert_equal(nil,                      metadata.support_url)
+      assert_equal('Victorious Inc',                    metadata.copyright)
+      assert_equal('Entertainment',                     metadata.ios_primary_category)
+      assert_equal('Education',                         metadata.ios_secondary_category)
+      assert_equal('Teach me how to peachy',            metadata.ios_description)
+      assert_equal('leachy,peachy,keywords',            metadata.ios_keywords)
+      assert_equal('Leachy Peachy',                     metadata.app_name)
+      assert_equal('http://example.com/privacy_policy', metadata.privacy_policy_url)
+      assert_equal('http://example.com/support',        metadata.support_url)
     end
   end
 end
