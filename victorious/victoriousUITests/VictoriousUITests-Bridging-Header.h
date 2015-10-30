7cd0806cb6f4660458ca1395ad4a29fff164a165
Cody Kolodziejzyk <cody@getvictorious.com>
2015-10-30 10:36:51 -0600

    removed UI testing target and UI tests

--- victorious/victoriousUITests/VictoriousUITests-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  VictoriousUITests-Bridging-Header.h
-//  victorious
-//
-//  Created by Cody Kolodziejzyk on 10/22/15.
-//  Copyright © 2015 Victorious. All rights reserved.
-//
-
-#import "VAutomation.h"
