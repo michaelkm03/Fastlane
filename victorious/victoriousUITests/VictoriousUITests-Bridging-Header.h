a9767bbaeeec23e8db6a58e5254a251986631b71
Cody Kolodziejzyk <cody@getvictorious.com>
2015-11-03 10:57:46 -0800

    Revert "removed UI testing target and UI tests"
    This reverts commit 7cd0806cb6f4660458ca1395ad4a29fff164a165.

---
+++ victorious/victoriousUITests/VictoriousUITests-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  VictoriousUITests-Bridging-Header.h
+//  victorious
+//
+//  Created by Cody Kolodziejzyk on 10/22/15.
+//  Copyright © 2015 Victorious. All rights reserved.
+//
+
+#import "VAutomation.h"
