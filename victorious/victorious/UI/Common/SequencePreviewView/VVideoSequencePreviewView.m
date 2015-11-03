abdcef6e236d3cab71668da5f9cb80c6a5b858ae
Cody Kolodziejzyk <cody@getvictorious.com>
2015-11-03 14:57:21 -0800

    got rid of loading indicator

--- victorious/victorious/UI/Common/SequencePreviewView/VVideoSequencePreviewView.m
+++ victorious/victorious/UI/Common/SequencePreviewView/VVideoSequencePreviewView.m
@@ -31,7 +31,6 @@ typedef NS_ENUM(NSUInteger, VVideoState)
 @property (nonatomic, strong) VPassthroughContainerView *videoUIContainer;
 @property (nonatomic, strong, readwrite, nullable) VideoToolbarView *toolbar;
 @property (nonatomic, strong) SoundBarView *soundIndicator;
-@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
 
 @property (nonatomic, assign) VVideoState state;
 @property (nonatomic, strong) NSURL *assetURL;
@@ -111,12 +110,6 @@ typedef NS_ENUM(NSUInteger, VVideoState)
                                                                      attribute:NSLayoutAttributeWidth
                                                                     multiplier:1.0f
                                                                       constant:0.0f]];
-    
-    self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
-    self.activityIndicator.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.videoUIContainer addSubview:self.activityIndicator];
-    [self.videoUIContainer v_addCenterHorizontallyConstraintsToSubview:self.activityIndicator];
-    [self.videoUIContainer v_addCenterVerticallyConstraintsToSubview:self.activityIndicator];
 }
 
 - (void)onContentTap
@@ -245,12 +238,9 @@ typedef NS_ENUM(NSUInteger, VVideoState)
                 self.largePlayButton.hidden = YES;
                 self.previewImageView.hidden = NO;
                 self.videoPlayer.view.hidden = YES;
-                self.activityIndicator.hidden = NO;
-                [self.activityIndicator startAnimating];
             }
             else
             {
-                self.activityIndicator.hidden = YES;
                 self.largePlayButton.hidden = YES;
                 self.videoPlayer.view.hidden = NO;
             }
