1499f34b8648151ad2797f53efe3814ff6868b37
Sky Lan <4dreams.sky@gmail.com>
2015-11-09 15:37:51 -0800

    Add Fanloyalty Model

---
+++ victorious/VictoriousIOSSDK/Models/FanLoyalty.swift
@@ -0,0 +1,36 @@
+//
+//  FanLoyalty.swift
+//  victorious
+//
+//  Created by Tian Lan on 11/9/15.
+//  Copyright © 2015 Victorious. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public struct FanLoyalty {
+    
+    public let points: Int64
+    public let level: Int
+    public let progress: Int
+    
+    public init(points: Int64, level: Int, progress: Int) {
+        self.points = points
+        self.level = level
+        self.progress = progress
+    }
+}
+
+extension FanLoyalty {
+    public init?(json: JSON) {
+        guard let points = json["points"].int64,
+            let level = json["level"].int,
+            let progress = json["progress"].int else {
+                return nil
+        }
+        self.points = points
+        self.level = level
+        self.progress = progress
+    }
+}
