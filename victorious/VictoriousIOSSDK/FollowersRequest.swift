c033a7e0a99866e9e50ddb7ca5da600647014b0b
Sky Lan <4dreams.sky@gmail.com>
2015-11-10 14:31:57 -0800

    Tests and Request for FollowersRequeest

---
+++ victorious/VictoriousIOSSDK/FollowersRequest.swift
@@ -0,0 +1,51 @@
+//
+//  FollowersRequest.swift
+//  victorious
+//
+//  Created by Tian Lan on 11/10/15.
+//  Copyright © 2015 Victorious. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public struct FollowersRequest: Pageable {
+    /// Followers will be retrieved by this user ID
+    public let mainUserID: Int64
+    
+    private let paginator: StandardPaginator
+    
+    public init(userID: Int64, pageNumber: Int = 1, itemsPerPage: Int = 40) {
+        self.init(userID: userID, paginator: StandardPaginator(pageNumber: pageNumber, itemsPerPage: itemsPerPage))
+    }
+    
+    private init(userID: Int64, paginator: StandardPaginator) {
+        self.mainUserID = userID
+        self.paginator = paginator
+    }
+    
+    public var urlRequest: NSURLRequest {
+        let url = NSURL(string: "api/follow/followers_list/\(mainUserID)")!
+        let request = NSMutableURLRequest(URL: url)
+        paginator.addPaginationArgumentsToRequest(request)
+        
+        return request
+    }
+    
+    public func parseResponse(response: NSURLResponse, toRequest request: NSURLRequest, responseData: NSData, responseJSON: JSON) throws -> (results: [User], nextPage: FollowersRequest?, previousPage: FollowersRequest?) {
+        guard let usersJSON = responseJSON["payload"]["users"].array else {
+            throw ResponseParsingError()
+        }
+        
+        let results = usersJSON.flatMap { User(json: $0) }
+        let nextPageRequest: FollowersRequest? = usersJSON.count > 0 ? FollowersRequest(userID: mainUserID, paginator: paginator.nextPage) : nil
+        let previousPageRequest: FollowersRequest?
+        
+        if let previousPage = paginator.previousPage {
+            previousPageRequest = FollowersRequest(userID: mainUserID, paginator: previousPage)
+        } else {
+            previousPageRequest = nil
+        }
+        return (results, nextPageRequest, previousPageRequest)
+    }
+}
