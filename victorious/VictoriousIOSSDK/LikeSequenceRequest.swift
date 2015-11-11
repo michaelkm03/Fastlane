b66e283366208fe50c45b1bb2908c9a0bb0f42d5
Sky Lan <4dreams.sky@gmail.com>
2015-11-10 17:14:45 -0800

    Add liking request

---
+++ victorious/VictoriousIOSSDK/LikeSequenceRequest.swift
@@ -0,0 +1,27 @@
+//
+//  LikeSequenceRequest.swift
+//  victorious
+//
+//  Created by Tian Lan on 11/10/15.
+//  Copyright © 2015 Victorious. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public struct LikeSequenceRequest: RequestType {
+    private let sequenceID: Int64
+    
+    public init (sequenceID: Int64) {
+        self.sequenceID = sequenceID
+    }
+    
+    public var urlRequest: NSURLRequest {
+        let urlRequest = NSMutableURLRequest(URL: NSURL(string: "/api/sequence/like")!)
+        urlRequest.HTTPMethod = "POST"
+        let likedSequenceInfo = [ "sequence_id": sequenceID ]
+        urlRequest.vsdk_addURLEncodedFormPost(likedSequenceInfo)
+        
+        return urlRequest
+    }
+}
