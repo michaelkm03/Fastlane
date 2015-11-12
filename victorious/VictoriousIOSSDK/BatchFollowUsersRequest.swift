0ef6352de9159953b3e1d2f3b036ea9d21abc89e
Cody Kolodziejzyk <cody@getvictorious.com>
2015-11-11 18:02:15 -0800

    using map instead of flatMap

--- victorious/VictoriousIOSSDK/BatchFollowUsersRequest.swift
+++ victorious/VictoriousIOSSDK/BatchFollowUsersRequest.swift
@@ -23,7 +23,7 @@ public struct BatchFollowUsersRequest: RequestType {
         let url = NSURL(string: "/api/follow/batchadd")!
         let request = NSMutableURLRequest(URL: url)
         request.HTTPMethod = "POST"
-        let converted = NSDictionary(dictionary: ["target_user_ids" : usersToFollow.flatMap { NSNumber(longLong: $0) }])
+        let converted = NSDictionary(dictionary: ["target_user_ids" : usersToFollow.map { NSNumber(longLong: $0) }])
         request.vsdk_addURLEncodedFormPost(converted)
         return request
     }
