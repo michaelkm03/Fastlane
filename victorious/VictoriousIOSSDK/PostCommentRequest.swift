c3fa2837e07b63c2079e491158fa2208c783865b
Cody Kolodziejzyk <cody@getvictorious.com>
2015-11-12 18:48:36 -0800

    post comment request and tests

---
+++ victorious/VictoriousIOSSDK/PostCommentRequest.swift
@@ -0,0 +1,81 @@
+//
+//  PostCommentRequest.swift
+//  victorious
+//
+//  Created by Cody Kolodziejzyk on 11/12/15.
+//  Copyright © 2015 Victorious. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+/// Adds a comment to a particular sequence
+public class PostCommentRequest: RequestType {
+    
+    private let sequenceID: Int64
+    private let text: String?
+    private let mediaURL: NSURL?
+    private let mediaType: MediaAttachmentType?
+    
+    public private(set) var urlRequest = NSURLRequest()
+    
+    private var bodyTempFile: NSURL?
+    
+    private func makeRequest() throws -> NSURLRequest {
+        let bodyTempFile = self.tempFile()
+        let writer = VMultipartFormDataWriter(outputFileURL: bodyTempFile)
+        
+        try writer.appendPlaintext(text ?? "", withFieldName: "text")
+        try writer.appendPlaintext("\(sequenceID)", withFieldName: "sequence_id")
+        
+        if let mediaURL = mediaURL,
+            let mediaType = mediaType,
+            let pathExtension = mediaURL.pathExtension,
+            let mimeType = mediaURL.vsdk_mimeType {
+                if mediaType == .GIF {
+                    try writer.appendPlaintext("true", withFieldName: "is_gif_style")
+                }
+                try writer.appendFileWithName("message_media.\(pathExtension)", contentType: mimeType, fileURL: mediaURL, fieldName: "media_data")
+        }
+        
+        try writer.finishWriting()
+        self.bodyTempFile = bodyTempFile
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "/api/comment/add")!)
+        request.HTTPMethod = "POST"
+        request.HTTPBodyStream = NSInputStream(URL: bodyTempFile)
+        return request
+    }
+    
+    private func tempFile() -> NSURL {
+        let tempDirectory = NSURL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+        return tempDirectory.URLByAppendingPathComponent(NSUUID().UUIDString)
+    }
+    
+    public init?(sequenceID: Int64, text: String?, mediaAttachmentType: MediaAttachmentType?, mediaURL: NSURL?) {
+        
+        self.sequenceID = sequenceID
+        self.text = text
+        self.mediaType = mediaAttachmentType
+        self.mediaURL = mediaURL
+        
+        do {
+            self.urlRequest = try makeRequest()
+        } catch {
+            return nil
+        }
+    }
+    
+    deinit {
+        if let bodyTempFile = bodyTempFile {
+            let _ = try? NSFileManager.defaultManager().removeItemAtURL(bodyTempFile)
+        }
+    }
+    
+    public func parseResponse(response: NSURLResponse, toRequest request: NSURLRequest, responseData: NSData, responseJSON: JSON) throws -> Comment {
+        guard let comment = Comment(json: responseJSON["payload"]) else {
+            throw ResponseParsingError()
+        }
+        return comment
+    }
+}
