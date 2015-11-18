289f332b3b5f795b1926f5cb09829aaa59f375c8
Alex Tamoykin <alex@rightbalance.io>
2015-11-18 14:22:50 -0800

    Add the ability to save screenshots from vams urls

--- victorious/fastlane/lib/vams/screenshots.rb
+++ victorious/fastlane/lib/vams/screenshots.rb
@@ -1,9 +1,32 @@
+require 'uri'
+require 'httparty'
+
 module VAMS
   class Screenshots < OpenStruct
     def save(location:)
-      each_pair do |language, screenshots|
-        language_dir_path = File.join(location.to_s, language.to_s)
-        FileUtils.mkdir_p(language_dir_path) unless File.directory?(language_dir_path)
+      each_pair do |language, screenshot_urls|
+        language_dir_path = create_language_dir_if_needed(location: location, language: language)
+        save_screenshots_into_dir(dir: language_dir_path, screenshot_urls: screenshot_urls)
+      end
+    end
+
+    private
+
+    def create_language_dir_if_needed(location:, language:)
+      language_dir_path = File.join(location.to_s, language.to_s)
+      FileUtils.mkdir_p(language_dir_path) unless File.directory?(language_dir_path)
+      language_dir_path
+    end
+
+    def save_screenshots_into_dir(dir:, screenshot_urls:)
+      screenshot_urls.each do |url|
+        uri      = URI.parse(url)
+        filename = File.basename(uri.path)
+        response = HTTParty.get(url)
+
+        File.open(File.join(dir, filename), File::WRONLY | File::CREAT) do |file|
+          file.write(response.body)
+        end
       end
     end
   end
--- victorious/fastlane/test/lib/screenshots_test.rb
+++ victorious/fastlane/test/lib/screenshots_test.rb
@@ -5,17 +5,22 @@ require 'fakefs/safe'
 module VAMS
   class ScreenshotsTest < Minitest::Test
     def test_saving_screenshots
+      test_dir  = File.join(__dir__, '..')
+      fake_file_content = File.read(File.join(test_dir, 'fixtures', 'screenshot.png'))
+      stub_request(:get, "https://s3.aws.amazon.com/victorious/en_US/1_screenshot.png").
+        to_return(:status => 200, :body => fake_file_content, :headers => {})
+
       FakeFS do
         screenshots_data = {
           en_US: [
             "https://s3.aws.amazon.com/victorious/en_US/1_screenshot.png",
-            "https://s3.aws.amazon.com/victorious/en_US/2_screenshot.png"
           ]
         }
         screenshots = Screenshots.new(screenshots_data)
-        location    = File.join(__dir__, '../tmp')
+        location    = File.join(test_dir, 'tmp')
         screenshots.save(location: location)
         assert_equal(true, File.directory?(File.join(location, 'en_US')))
+        assert_equal(true, File.exists?(File.join(location, 'en_US/1_screenshot.png')))
       end
     end
   end
