599047bbcaef596948890de85afe252571418145
Alex Tamoykin <alex@rightbalance.io>
2015-11-22 12:02:02 -0800

    Add steps explanations to a smoke test

--- victorious/fastlane/test/smoke_test_vams_api.rb
+++ victorious/fastlane/test/smoke_test_vams_api.rb
@@ -3,12 +3,13 @@ $LOAD_PATH.unshift File.expand_path('lib')
 require 'vams/client'
 environment = :development
 client = VAMS::Client.new(environment: environment)
+puts '=== Getting apps to build'
 puts client.apps_to_build
+puts '=== Getting an app by build name'
 app = client.app_by_build_name('leachypeachy')
-puts app
-puts "ID:" + app.app_id
 
 require 'vams/submission_result'
 result = VAMS::SubmissionResult.new(id: app.app_id, status: "App submission completed successfully", datetime: Time.now)
-puts result.to_json
+puts '=== Posting an app submission result'
+puts 'Payload to submit: ' + result.to_json
 puts client.submit_result(result: result, environment: environment)
