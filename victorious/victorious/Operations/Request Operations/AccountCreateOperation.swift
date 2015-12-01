705e4b22e3439e10e14fee00fe23c7df06b93e05
Patrick Lynch <patrick@getvictorious.com>
2015-12-01 14:11:34 -0800

    Removed some more todos and fixed up the poll results by user request.

--- victorious/victorious/Operations/Request Operations/AccountCreateOperation.swift
+++ victorious/victorious/Operations/Request Operations/AccountCreateOperation.swift
@@ -65,39 +65,3 @@ class AccountCreateOperation: RequestOperation<AccountCreateRequest> {
         }
     }
 }
-
-class PollResultByUserOperation: RequestOperation<PollResultByUserRequest> {
-    
-    private let persistentStore: PersistentStoreType = MainPersistentStore()
-    private let userID: Int64
-    
-    init( userID: Int64 ) {
-        self.userID = userID
-        super.init( request: PollResultByUserRequest(userID: userID) )
-    }
-    
-    override func onError(error: NSError, completion: () -> ()) {
-        completion()
-    }
-    
-    override func onComplete( response: PollResultByUserRequest.ResultType, completion:()->() ) {
-        
-        persistentStore.asyncFromBackground() { context in
-            let pollResults: [VPollResult] = response.flatMap {
-                let uniqueElements = [
-                    "answerId" : NSNumber(longLong: $0.answerID),
-                    "sequenceId" : String($0.sequenceID)
-                ]
-                let persistentResult: VPollResult = context.findOrCreateObject( uniqueElements )
-                persistentResult.populate(fromSourceModel: $0)
-                return persistentResult
-            }
-            guard let user: VUser = context.findObjects( [ "remoteId" :  NSNumber(longLong: self.userID) ] ).first else {
-                fatalError( "Could not load user with provided user ID" )
-            }
-            user.addObjects( pollResults, to: "pollResults" )
-            context.saveChanges()
-            completion()
-        }
-    }
-}
--- victorious/victorious/Operations/Request Operations/PollResultByUserOperation.swift
+++ victorious/victorious/Operations/Request Operations/PollResultByUserOperation.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import VictoriousIOSSDK
+
+class PollResultByUserOperation: RequestOperation<PollResultByUserRequest> {
+    
+    private let persistentStore: PersistentStoreType = MainPersistentStore()
+    private let userID: Int64
+    
+    init( userID: Int64 ) {
+        self.userID = userID
+        super.init( request: PollResultByUserRequest(userID: userID) )
+    }
+    
+    override func onError(error: NSError, completion: () -> ()) {
+        completion()
+    }
+    
+    override func onComplete( response: PollResultByUserRequest.ResultType, completion:()->() ) {
+        
+        persistentStore.asyncFromBackground() { context in
+            let pollResults: [VPollResult] = response.flatMap {
+                let uniqueElements = [
+                    "answerId" : NSNumber(longLong: $0.answerID),
+                    "sequenceId" : String($0.sequenceID)
+                ]
+                let persistentResult: VPollResult = context.findOrCreateObject( uniqueElements )
+                persistentResult.populate(fromSourceModel: $0)
+                return persistentResult
+            }
+            guard let user: VUser = context.findObjects( [ "remoteId" :  NSNumber(longLong: self.userID) ] ).first else {
+                fatalError( "Could not load user with provided user ID" )
+            }
+            user.addObjects( pollResults, to: "pollResults" )
+            context.saveChanges()
+            completion()
+        }
+    }
+}
