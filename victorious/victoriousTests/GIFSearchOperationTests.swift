da1d81351ff78c17cedfb1ba4affd452e1326347
Sky Lan <4dreams.sky@gmail.com>
2015-12-02 15:13:04 -0800

    Add another test case

--- victorious/victoriousTests/GIFSearchOperationTests.swift
+++ victorious/victoriousTests/GIFSearchOperationTests.swift
@@ -17,8 +17,28 @@ class GIFSearchOperationTests: XCTestCase {
         let result: GIFSearchRequest.ResultType = ([], nil, nil)
         let operation = GIFSearchOperation(searchText: "fun")
         operation.onComplete(result){ }
+        
         XCTAssertTrue(operation.searchResults.isEmpty)
         XCTAssertNil(operation.nextPageOperation)
         XCTAssertNil(operation.previousPageOperation)
+    }
+
+    func testValidResult() {
+        let mockJSON: JSON = [
+            "gif_url": "www.gif.com",
+            "mp4_url": "www.mp4.com",
+            "width": 101,
+            "height": 102,
+            "thumbnail_still": "www.still.com",
+            "remote_id": "1001"
+        ]
+        let result: GIFSearchRequest.ResultType = ([GIFSearchResult(json: mockJSON)!], GIFSearchRequest(searchTerm: "next"), GIFSearchRequest(searchTerm: "Prev"))
+        let operation = GIFSearchOperation(searchText: "fun")
+        operation.onComplete(result){ }
+        
+        XCTAssertEqual(operation.searchResults.count, 1)
+        XCTAssertEqual(operation.searchResults[0].remoteID, "1001")
+        XCTAssertNotNil(operation.nextPageOperation)
+        XCTAssertNotNil(operation.previousPageOperation)
     }
 }
