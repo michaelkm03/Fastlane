9f65e989f5121cc5cb7619ca3edd057ef66ffee1
Alex Tamoykin <alex@rightbalance.io>
2015-12-03 15:35:47 -0800

    Also submit a build number in smoke test

--- victorious/fastlane/test/smoke_test_vams_api.rb
+++ victorious/fastlane/test/smoke_test_vams_api.rb
@@ -9,7 +9,10 @@ puts '=== Getting an app by build name'
 app = client.app_by_build_name('leachypeachy')
 
 require 'vams/submission_result'
-result = VAMS::SubmissionResult.new(app_id: app.app_id, status: "App submission completed successfully", datetime: Time.now)
+result = VAMS::SubmissionResult.new(app_id:   app.app_id,
+                                    datetime: Time.now,
+                                    status:   VAMS::SubmissionResult::Status::GOOD,
+                                    build:    '3.5 (23123123)')
 puts '=== Posting an app submission result'
 puts 'Payload to submit: ' + result.to_json
 puts client.submit_result(result: result, environment: environment)
