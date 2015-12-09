eb423fa2cd867e9c3a18bb41a4e20b8c47e5a44f
Michael Sena <michaelsena@getvictorious.com>
2015-12-08 17:09:40 -0800

    Device Experiments Request

---
+++ victorious/VictoriousIOSSDK/DeviceExperimentsRequest.swift
@@ -0,0 +1,26 @@
+//
+//  DeviceExperimentsRequest.swift
+//  victorious
+//
+//  Created by Michael Sena on 12/8/15.
+//  Copyright © 2015 Victorious. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public struct DeviceExperimentsRequest: RequestType {
+    
+    public var urlRequest: NSURLRequest {
+        return NSURLRequest(URL: NSURL(string: "api/device/experiments")!)
+    }
+    
+    public func parseResponse(response: NSURLResponse, toRequest request: NSURLRequest, responseData: NSData, responseJSON: JSON) throws -> [DeviceExperiment]{
+        guard let experimentsJSON = responseJSON["payload"].array else {
+            throw ResponseParsingError()
+        }
+        
+        return experimentsJSON.flatMap { DeviceExperiment(json: $0) }
+    }
+    
+}
---
+++ victorious/VictoriousIOSSDK/Models/DeviceExperiment.swift
@@ -0,0 +1,41 @@
+//
+//  DeviceExperiment.swift
+//  victorious
+//
+//  Created by Michael Sena on 12/8/15.
+//  Copyright © 2015 Victorious. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public struct DeviceExperiment {
+    public let id: Int64
+    public let name: String
+    public let bucketType: String
+    public let numberOfBuckets: Int64?
+    public let layerID: Int64
+    public let layerName: String
+}
+
+extension DeviceExperiment {
+    public init?(json: JSON) {
+        if let experimentID = Int64(json["id"].stringValue), let layerID = Int64(json["layer_id"].stringValue) {
+            self.id = experimentID
+            self.layerID = layerID
+        }
+        else {
+            id = -1
+            name = ""
+            bucketType = ""
+            numberOfBuckets = -1
+            layerID = -1
+            layerName = ""
+            return nil
+        }
+        name = json["name"].stringValue
+        bucketType = json["bucket_type"].stringValue
+        numberOfBuckets = Int64(json["num_buckets"].stringValue)
+        layerName = json["layer_name"].stringValue
+    }
+}
