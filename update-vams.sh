33e5f27c04546e934b2ea545b88002f84bfeed81
Josh Hinman <josh@getvictorious.com>
2015-12-22 17:22:38 -0800

    storing the VAMS repo URL in the script so it doesn't depend on the remote being set up

--- update-vams.sh
+++ update-vams.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# NOTE: This script won't work if you don't have the vams-subtree remote set up:
-#  --> git remote add vams-subtree https://github.com/TouchFrame/VAMS.git
-
-git subtree pull --prefix=build-scripts/VAMS vams-subtree master --squash
+git subtree pull --prefix=build-scripts/VAMS https://github.com/TouchFrame/VAMS.git master --squash
