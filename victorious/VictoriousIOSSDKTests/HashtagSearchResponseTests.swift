3a1e128534dc3e4ebfc8ca7c55b7ed4ec5e38b18
Alex Tamoykin <alex@rightbalance.io>
2016-01-02 11:15:00 -0800

    Delete hashtagID from tests since it was deleted from a corresponding model

--- victorious/VictoriousIOSSDKTests/HashtagSearchResponseTests.swift
+++ victorious/VictoriousIOSSDKTests/HashtagSearchResponseTests.swift
@@ -24,9 +24,7 @@ class HashtagSearchResponseTests: XCTestCase {
             let hashtagSearch = HashtagSearchRequest(searchTerm: "surfer", paginator: paginator)
             let results = try hashtagSearch.parseResponse(NSURLResponse(), toRequest: hashtagSearch.urlRequest, responseData: mockData, responseJSON: JSON(data: mockData))
             XCTAssertEqual(results.count, 2)
-            XCTAssertEqual(results[0].hashtagID, 495)
             XCTAssertEqual(results[0].tag, "surfer")
-            XCTAssertEqual(results[1].hashtagID, 616)
             XCTAssertEqual(results[1].tag, "surfer2")
         } catch {
             XCTFail("Sorry, parseResponse should not throw here")
