920c24a25fae560dac0e160626940efd6c3dee35
Patrick Lynch <patrick@getvictorious.com>
2016-01-04 14:09:24 -0800

    Created NSBundle+UnitTests category and changed the way persistent store references are provided so that unit tests to not mess around with any local persistent stores used when the application is running normally.

--- victorious/victorious/UI/Streams/VStreamCollectionViewController.m
+++ victorious/victorious/UI/Streams/VStreamCollectionViewController.m
@@ -144,7 +144,7 @@ static NSString * const kStreamCollectionKey = @"destinationStream";
     NSDictionary *query = @{ @"apiPath" : apiPath };
     
     __block VStream *stream = nil;
-    id<PersistentStoreType>  persistentStore = [[MainPersistentStore alloc] init];
+    id<PersistentStoreType>  persistentStore = [PersistentStoreSelector mainPersistentStore];
     [persistentStore.mainContext performBlockAndWait:^void {
         stream = (VStream *)[persistentStore.mainContext v_findOrCreateObjectWithEntityName:[VStream entityName] queryDictionary:query];
         stream.name = [dependencyManager stringForKey:VDependencyManagerTitleKey];
