7dcd41b09979bcd2cafe951a6d7db7697c18fabf
Patrick Lynch <patrick@getvictorious.com>
2016-01-05 11:37:52 -0800

    Fixed up missing files in group.

--- victorious/victorious/UI/Comments/SequenceCommentsDataSource.swift
+++ victorious/victorious/UI/Comments/SequenceCommentsDataSource.swift
@@ -12,13 +12,33 @@ class SequenceCommentsDataSource : PaginatedDataSource {
     
     private let sequence: VSequence
     private let flaggedContent = VFlaggedContent()
+    private(set) var isLoadingComments: Bool = false
+    private var loadCommentsOperation: SequenceCommentsOperation?
+    
+    var commentsArray: [VComment] {
+        return self.sequence.comments.array as? [VComment] ?? []
+    }
     
     init(sequence: VSequence) {
         self.sequence = sequence
     }
     
+    var numberOfComments: Int {
+        return self.commentsArray.count
+    }
+    
+    func commentAtIndex(index: Int) -> VComment {
+        return self.commentsArray[index]
+    }
+    
+    func indexOfComment(comment: VComment) -> Int {
+        if let commentIndex = commentsArray.indexOf(comment) {
+            return commentIndex
+        }
+        return 0
+    }
+    
     func loadComments( pageType: VPageType, completion:((NSError?)->())? = nil ) {
-        
         self.loadPage( pageType,
             createOperation: {
                 return SequenceCommentsOperation(sequenceID: self.sequence.remoteId)
@@ -29,14 +49,15 @@ class SequenceCommentsDataSource : PaginatedDataSource {
         )
     }
     
-    func loadComments( atPageForCommentID commentID: NSNumber, completion:((Int?, NSError?)->())?) {
-        let operation = CommentFindOperation(sequenceID: self.sequence.remoteId, commentID: commentID.longLongValue )
-        operation.queue() { error in
-            if error == nil, let pageNumber = operation.pageNumber {
-                completion?(pageNumber, nil)
-            } else {
-                completion?(nil, error)
+    func loadComments( atPageForCommentID commentID: NSNumber, completion:((NSError?)->())? = nil ) {
+        // TODO:
+        /*self.loadPage( pageType,
+            createOperation: {
+                return CommentFindOperation(sequenceID: self.sequence.remoteId, commentID: commentID.longLongValue )
+            },
+            completion: { (operation, error) in
+                completion?(error)
             }
-        }
+        )*/
     }
 }
