a566005f2ec7922102df6524a4f502f0c678236b
Alex Tamoykin <alex@rightbalance.io>
2016-01-06 15:53:51 -0800

    Test the request and tracking on UnfollowUserOperation

--- victorious/victoriousPersistenceTests/UnfollowUserOperationTests.swift
+++ victorious/victoriousPersistenceTests/UnfollowUserOperationTests.swift
@@ -12,6 +12,8 @@ import XCTest
 class UnfollowUserOperationTests: XCTestCase {
     var operation: UnfollowUserOperation!
     var testStore: TestPersistentStore!
+    var testRequestExecutor: TestRequestExecutor!
+    var testTrackingManager: TestTrackingManager!
     let userID = 1
     let currentUserID = 2
     let screenName = "screenName"
@@ -19,14 +21,39 @@ class UnfollowUserOperationTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
+        continueAfterFailure = false
         testStore = TestPersistentStore()
+        testTrackingManager = TestTrackingManager()
+        testRequestExecutor = TestRequestExecutor()
+        VCurrentUser.persistentStore = testStore
         operation = UnfollowUserOperation(userID: userID, screenName: screenName)
+        operation.requestExecutor = testRequestExecutor
+        operation.trackingManager = testTrackingManager
     }
 
     func testUnfollowingAnExistingUser() {
+        let currentUser = operationHelper.createUser(remoteId: currentUserID, persistentStore: testStore)
+        currentUser.setAsCurrentUser()
+
+        let objectUser = operationHelper.createUser(remoteId: userID, persistentStore: testStore)
+        objectUser.isFollowedByMainUser = true
+        objectUser.numberOfFollowers = 1
+        currentUser.numberOfFollowing = 1
+        let uniqueElements = [ "subjectUser" : currentUser, "objectUser" : objectUser ]
+        let followedUser: VFollowedUser = testStore.mainContext.v_findOrCreateObject( uniqueElements )
+        followedUser.displayOrder = -1
+        followedUser.objectUser = objectUser
+        followedUser.subjectUser = currentUser
+        testStore.mainContext.v_save()
+
+        operation.main()
+
+        XCTAssertEqual(1, testTrackingManager.trackEventCalls.count)
+        XCTAssertEqual(VTrackingEventUserDidUnfollowUser, testTrackingManager.trackEventCalls[0].eventName!)
     }
 
     override func tearDown() {
         super.tearDown()
+        operationHelper.tearDownPersistentStore(store: testStore)
     }
 }
