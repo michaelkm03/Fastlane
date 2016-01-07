ca095e1051769968715dc84249d2001fd31cc274
Alex Tamoykin <alex@rightbalance.io>
2016-01-06 17:33:52 -0800

    Test updated numberOfFollowers and numberOfFollowing

--- victorious/victoriousPersistenceTests/UnfollowUserOperationTests.swift
+++ victorious/victoriousPersistenceTests/UnfollowUserOperationTests.swift
@@ -32,22 +32,33 @@ class UnfollowUserOperationTests: XCTestCase {
     }
 
     func testUnfollowingAnExistingUser() {
+        let objectUser = operationHelper.createUser(remoteId: userID, persistentStore: testStore)
         let currentUser = operationHelper.createUser(remoteId: currentUserID, persistentStore: testStore)
         currentUser.setAsCurrentUser()
 
-        let objectUser = operationHelper.createUser(remoteId: userID, persistentStore: testStore)
         objectUser.isFollowedByMainUser = true
         objectUser.numberOfFollowers = 1
         currentUser.numberOfFollowing = 1
         let uniqueElements = [ "subjectUser" : currentUser, "objectUser" : objectUser ]
         let followedUser: VFollowedUser = testStore.mainContext.v_findOrCreateObject( uniqueElements )
-        followedUser.displayOrder = -1
         followedUser.objectUser = objectUser
         followedUser.subjectUser = currentUser
+        followedUser.displayOrder = -1
         testStore.mainContext.v_save()
 
         operation.main()
 
+        guard let updatedUser = testStore.mainContext.objectWithID(objectUser.objectID) as? VUser else {
+            XCTFail("No user to follow found after following a user")
+            return
+        }
+        guard let updatedCurrentUser = VCurrentUser.user() else {
+            XCTFail("No current user found after following a user")
+            return
+        }
+
+        XCTAssertEqual(0, updatedUser.numberOfFollowers)
+        XCTAssertEqual(0, currentUser.numberOfFollowing)
         XCTAssertEqual(1, testTrackingManager.trackEventCalls.count)
         XCTAssertEqual(VTrackingEventUserDidUnfollowUser, testTrackingManager.trackEventCalls[0].eventName!)
     }
