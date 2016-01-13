9da62863d6e1acb55b847376b4bf0e697fdfe554
Alex Tamoykin <alex@rightbalance.io>
2016-01-12 19:42:45 -0800

    Simplify the test bundle logic

--- victorious/victorious/NSBundle+TestBundle.swift
+++ victorious/victorious/NSBundle+TestBundle.swift
@@ -9,6 +9,6 @@
 extension NSBundle {
     static var isTestBundle: Bool {
         let environment = NSProcessInfo.processInfo().environment
-        return (environment["XCInjectBundleInto"] != nil || environment["XCInjectBundle"] != nil) ? true : false
+        return environment["XCInjectBundleInto"] != nil || environment["XCInjectBundle"] != nil
     }
 }
