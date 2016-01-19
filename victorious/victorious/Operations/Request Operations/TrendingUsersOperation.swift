59c9332bf8dcc21ccb6479210d46d8daa56c7cd3
Sky Lan <4dreams.sky@gmail.com>
2016-01-19 11:16:53 -0800

    A slow version implemented. Will try FetchUserOperation

--- victorious/victorious/Operations/Request Operations/TrendingUsersOperation.swift
+++ victorious/victorious/Operations/Request Operations/TrendingUsersOperation.swift
@@ -12,22 +12,29 @@ import VictoriousIOSSDK
 class TrendingUsersOperation: RequestOperation {
     
     let request = TrendingUsersRequest()
-    private(set) var results: [UserSearchResultObject]?
+    private(set) var results: [VUser]?
     
     override func main() {
         requestExecutor.executeRequest(request, onComplete: onComplete, onError: nil)
     }
     
     func onComplete( networkResult: TrendingUsersRequest.ResultType, completion: () -> () ) {
-        self.results = networkResult.map { UserSearchResultObject(user: $0) }
-        completion()
+        self.results = []
         
-        storedBackgroundContext = persistentStore.createBackgroundContext().v_performBlock() { context in
+        persistentStore.createBackgroundContext().v_performBlockAndWait() { context in
             for networkUser in networkResult {
-                let localUser: VUser = context.v_findOrCreateObject([ "remoteId" : networkUser.userID])
-                localUser.populate(fromSourceModel: networkUser)
+                let persistentUser: VUser = context.v_findOrCreateObject([ "remoteId" : networkUser.userID ])
+                persistentUser.populate(fromSourceModel: networkUser)
+                context.v_save()
+                
+                let objectID = persistentUser.objectID
+                self.persistentStore.mainContext.v_performBlock() { context in
+                    if let user = context.objectWithID( objectID ) as? VUser {
+                        self.results?.append(user)
+                    }
+                }
             }
-            context.v_save()
         }
+        completion()
     }
 }
