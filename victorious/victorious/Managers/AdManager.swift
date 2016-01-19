65282edd165303aede0eb1da3ff5ec9f2832fa70
Alex Tamoykin <alex@rightbalance.io>
2016-01-19 15:32:53 -0800

    Implement AdsManager and AdsLoader delegate methods

--- victorious/victorious/Managers/AdManager.swift
+++ victorious/victorious/Managers/AdManager.swift
@@ -9,7 +9,7 @@
 import AVFoundation
 import GoogleInteractiveMediaAds
 
-@objc class AdManager: NSObject, IMAAdsLoaderDelegate {
+@objc class AdManager: NSObject, IMAAdsLoaderDelegate, IMAAdsManagerDelegate {
     let player: AVPlayer
     let contentPlayhead: IMAAVPlayerContentPlayhead
     let adsLoader: IMAAdsLoader
@@ -42,9 +42,39 @@ import GoogleInteractiveMediaAds
         }
     }
 
+    // MARK - IMAAdsLoaderDelegate methods
+
     func adsLoader(loader: IMAAdsLoader!, adsLoadedWithData adsLoadedData: IMAAdsLoadedData!) {
+        let adsManager = adsLoadedData.adsManager
+        adsManager.delegate = self
+        let adsRenderingSettings = IMAAdsRenderingSettings()
+        adsRenderingSettings.webOpenerPresentingController = UIViewController()
+        adsManager.initializeWithAdsRenderingSettings(adsRenderingSettings)
     }
 
     func adsLoader(loader: IMAAdsLoader!, failedWithErrorData adErrorData: IMAAdLoadingErrorData!) {
+        print("Failed to load adds with error: \(adErrorData.adError.message)")
+        player.play()
+    }
+
+    // MARK - IMAAdsManagerDelegate methods
+
+    func adsManager(adsManager: IMAAdsManager!, didReceiveAdEvent event: IMAAdEvent!) {
+        if event.type == IMAAdEventType.LOADED {
+            adsManager.start()
+        }
+    }
+
+    func adsManager(adsManager: IMAAdsManager!, didReceiveAdError error: IMAAdError!) {
+        print("IMAAdsManager has an error: \(error.message)")
+        player.play()
+    }
+
+    func adsManagerDidRequestContentPause(adsManager: IMAAdsManager!) {
+        player.pause()
+    }
+
+    func adsManagerDidRequestContentResume(adsManager: IMAAdsManager!) {
+        player.play()
     }
 }
