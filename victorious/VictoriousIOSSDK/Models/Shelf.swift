837d43d85494642b656e46e0fbd6486ca305b3b4
Sky Lan <4dreams.sky@gmail.com>
2016-01-22 10:56:35 -0800

    Build network models for shelves

---
+++ victorious/VictoriousIOSSDK/Models/Shelf.swift
@@ -0,0 +1,11 @@
+//
+//  Shelf.swift
+//  victorious
+//
+//  Created by Tian Lan on 1/22/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
+
+public typealias Shelf = Stream
