f69e8c264a62dd6a21c24659c081aed50c499ee8
Michael Sena <michaelsena@getvictorious.com>
2016-01-22 20:25:30 -0800

    Add deletable tests

---
+++ victorious/victoriousTests/VConversationTests.swift
@@ -0,0 +1,31 @@
+//
+//  VConversationTests.swift
+//  victorious
+//
+//  Created by Michael Sena on 1/22/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import XCTest
+@testable import victorious
+
+class VConversationTests: XCTestCase {
+
+    var conversation: VConversation!
+    
+    override func setUp() {
+        super.setUp()
+        conversation = PersistentStoreTestHelper(persistentStore:TestPersistentStore()).createConversation()
+    }
+    
+    func testMarkedForDeletionPredicates() {
+        let markedForDeletionPredicate = VConversation.markedForDeletionPredicate
+        self.conversation.markedForDeletion = true
+        XCTAssertTrue(markedForDeletionPredicate.evaluateWithObject(self.conversation))
+
+        let notMarkedForDeletionPredicate = VConversation.notMarkedForDeletionPredicate
+        self.conversation.markedForDeletion = false
+        XCTAssertTrue(notMarkedForDeletionPredicate.evaluateWithObject(self.conversation))
+    }
+
+}
