d384c72508d6b58faa3938561f6110d0b335b589
Patrick Lynch <patrick@getvictorious.com>
2016-01-29 18:36:25 -0800

    Changed order of how streams parsing strategies are attempted and cleaned up code.

--- victorious/VictoriousIOSSDK/StreamRequest.swift
+++ victorious/VictoriousIOSSDK/StreamRequest.swift
@@ -44,18 +44,27 @@ public struct StreamRequest: PaginatorPageable, ResultBasedPageable {
         
         let stream: Stream
         
-        if let streamFromObject = Stream(json: responseJSON["payload"]) {
-            stream = streamFromObject // Regular Streams
-        } else if responseJSON["payload"].array != nil,
+        if responseJSON["payload"].array != nil,
             let streamFromItems = Stream(json: JSON([ "id" : responseJSON["stream_id"], "items" : responseJSON["payload"] ])) {
-                stream = streamFromItems // User profile and other streams with `stream_id` in the response
+                
+                // User profile and other streams with `stream_id` in the response
+                stream = streamFromItems
+                
         } else if responseJSON["payload"]["content"].array != nil,
             let streamFromItems = Stream(json: JSON([ "id" : "anonymous:stream", "items" : responseJSON["payload"]["content"] ])) {
-                stream = streamFromItems // Liked posts, and other weird responses with no `stream_id` information
+                
+                // Liked posts, and other weird responses with no `stream_id` information
+                stream = streamFromItems
+                
+        } else if let streamFromObject = Stream(json: responseJSON["payload"]) {
+            
+            // Regular Streams
+            stream = streamFromObject
+            
         } else {
             throw ResponseParsingError()
         }
-    
+        
         return stream
     }
 }
