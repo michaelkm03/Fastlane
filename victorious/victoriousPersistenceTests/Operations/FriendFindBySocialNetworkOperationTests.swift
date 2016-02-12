a32a04218aa8f710331fb943f0201b0cc1bc31cc
Sky Lan <4dreams.sky@gmail.com>
2016-02-12 13:09:31 -0800

    Add tests for Friend find by social network operation

--- victorious/victoriousPersistenceTests/Operations/FriendFindBySocialNetworkOperationTests.swift
+++ victorious/victoriousPersistenceTests/Operations/FriendFindBySocialNetworkOperationTests.swift
@@ -7,7 +7,36 @@
 //
 
 import XCTest
+import VictoriousIOSSDK
+@testable import victorious
 
-class FriendFindBySocialNetworkOperationTests: XCTestCase {
+class FriendFindBySocialNetworkOperationTests: BaseRequestOperationTestCase {
+    let testUserID = 1
+    let facebookToken = "testFacebookToken"
     
+    func testResults() {
+        let operation = FriendFindBySocialNetworkOperation(token: facebookToken)
+        operation.persistentStore = testStore
+        operation.requestExecutor = testRequestExecutor
+        
+        let expectation = expectationWithDescription("FriendFindBySocialNetworkFinished")
+        
+        let user = User(userID: testUserID)
+        operation.onComplete([user]) {
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(expectationThreshold) { error in
+            guard let results = operation.results,
+                let firstResult = results.first as? VUser else {
+                    XCTFail("We should have results here")
+                    return
+            }
+            guard let remoteId = firstResult.remoteId?.integerValue else {
+                XCTFail("First result should have a removeId")
+                return
+            }
+            XCTAssertEqual(remoteId, self.testUserID)
+        }
+    }
 }
