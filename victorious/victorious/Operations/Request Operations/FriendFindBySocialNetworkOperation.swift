04f90055590a6f2dde7181bdc8c520c22ddc6f8f
Sky Lan <4dreams.sky@gmail.com>
2016-02-11 19:00:51 -0800

    Implement FriendFindBySocialNetworkOperation

---
+++ victorious/victorious/Operations/Request Operations/FriendFindBySocialNetworkOperation.swift
@@ -0,0 +1,32 @@
+//
+//  FriendFindBySocialNetworkOperation.swift
+//  victorious
+//
+//  Created by Tian Lan on 2/11/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
+import VictoriousIOSSDK
+
+class FriendFindBySocialNetworkOperation: RequestOperation {
+    
+    private var request: FriendFindBySocialNetworkRequest
+    
+    convenience init(platformName: String, token: String) {
+        let request = FriendFindBySocialNetworkRequest(socialNetwork: .Facebook(platformName: platformName, accessToken: token))
+        self.init(request: request)
+    }
+    
+    private init(request: FriendFindBySocialNetworkRequest) {
+        self.request = request
+    }
+    
+    override func main() {
+        requestExecutor.executeRequest(request, onComplete: self.onComplete, onError: nil)
+    }
+    
+    func onComplete( results: FriendFindByEmailRequest.ResultType, completion:()->() ) {
+        
+    }
+}
---
+++ victorious/victoriousPersistenceTests/Operations/FriendFindBySocialNetworkOperationTests.swift
@@ -0,0 +1,13 @@
+//
+//  FriendFindBySocialNetworkOperationTests.swift
+//  victorious
+//
+//  Created by Tian Lan on 2/11/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import XCTest
+
+class FriendFindBySocialNetworkOperationTests: XCTestCase {
+    
+}
