588fe8047a4503cf7a7e646b9ede7d70d4762781
Patrick Lynch <patrick@getvictorious.com>
2016-02-12 23:31:15 -0800

    Changed to queueable.

--- victorious/victorious/Operations/Request Operations/AccountCreateOperation.swift
+++ victorious/victorious/Operations/Request Operations/AccountCreateOperation.swift
@@ -32,7 +32,7 @@ class AccountCreateOperation: RequestOperation {
     
     func onComplete( response: AccountCreateResponse, completion:()->() ) {
         let successOperation = LoginSuccessOperation(response: response, parameters: self.parameters)
-        successOperation.queueAfter(self, rechain: true)
+        successOperation.queueAfter(self)
         completion()
     }
 }
