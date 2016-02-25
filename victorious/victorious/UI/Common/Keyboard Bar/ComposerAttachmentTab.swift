dc3ac66e94813a9f55cf84513743ef9808c4a352
feef <fifihypnophonic@gmail.com>
2016-02-25 14:44:17 -0800

    start of composer delegate

---
+++ victorious/victorious/UI/Common/Keyboard Bar/ComposerAttachmentTab.swift
@@ -0,0 +1,13 @@
+//
+//  ComposerAttachmentTab.swift
+//  victorious
+//
+//  Created by Sharif Ahmed on 2/25/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
+
+enum ComposerAttachmentTab {
+    case Camera, Library, Gif
+}
---
+++ victorious/victorious/UI/Common/Keyboard Bar/ComposerAttachmentType.swift
@@ -0,0 +1,9 @@
+//
+//  ComposerAttachmentType.swift
+//  victorious
+//
+//  Created by Sharif Ahmed on 2/25/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
