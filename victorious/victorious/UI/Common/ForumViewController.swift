48eaaf7da7ad722cc9685af50eeba2b54b81b204
feef <fifihypnophonic@gmail.com>
2016-03-03 15:23:33 -0800

    start of container

---
+++ victorious/victorious/UI/Common/ForumViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ForumViewController.swift
+//  victorious
+//
+//  Created by Sharif Ahmed on 3/3/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import UIKit
+
+class ForumViewController: UIViewController {
+
+    @IBOutlet private var chatFeedController: ChatFeedController!
+    
+    @IBOutlet private var composerController: ComposerController!
+    
+    @IBOutlet private var stageController: StageController!
+}
