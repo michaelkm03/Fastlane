251a2ea2f796081a16654e6e4d41be89bb742807
Sky Lan <4dreams.sky@gmail.com>
2016-03-22 10:52:11 -0700

    Update legal prompt localized string on Age Gate

--- victorious/victorious/UI/Root View/Age Gate View/AgeGateViewController.swift
+++ victorious/victorious/UI/Root View/Age Gate View/AgeGateViewController.swift
@@ -130,7 +130,7 @@ class AgeGateViewController: UIViewController {
     private func setUpLegalInfoContainer() {
         let legalAttributesUnderline = [NSUnderlineStyleAttributeName: NSUnderlineStyle.StyleSingle.rawValue]
         
-        let legalPrompt = NSLocalizedString("By continuing you are agreeing to our", comment: "Legal prompt on age gate view")
+        let legalPrompt = NSLocalizedString("Legal Prompt on Age Gate", comment: "Legal prompt on age gate view")
         let tosText = NSAttributedString(string: NSLocalizedString("terms of service", comment: "") , attributes: legalAttributesUnderline)
         let ppText = NSAttributedString(string: NSLocalizedString("privacy policy", comment: ""), attributes: legalAttributesUnderline)
         
