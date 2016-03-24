a001fb2a28b0648c5179c0f0561b3cb4888ac598
Sebastian Nystorm <abeansits@gmail.com>
2016-03-24 15:30:58 -0700

    Removed testing code. Set an outlet to private.

--- victorious/victorious/UI/Forum/Stage/StageViewController.swift
+++ victorious/victorious/UI/Forum/Stage/StageViewController.swift
@@ -25,7 +25,7 @@ class StageViewController: UIViewController, Stage, VVideoPlayerDelegate {
     /// The content view that is grows and shrinks depending on the content it is displaying.
     /// Is is also this size that will be broadcasted to the stage delegate.
     @IBOutlet private weak var mainContentView: UIView!
-    @IBOutlet weak var mainContentViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var mainContentViewBottomConstraint: NSLayoutConstraint!
     
     @IBOutlet private weak var imageView: UIImageView!
     
@@ -51,90 +51,6 @@ class StageViewController: UIViewController, Stage, VVideoPlayerDelegate {
         terminateInterrupterTimer()
     }
     
-    // TODO: remove this before merge into dev !!! !!! !!!
-    private func senasDemoCode() {
-        
-        let gradient = VLinearGradientView()
-        gradient.setColors([UIColor.lightGrayColor(), UIColor.blueColor()])
-        gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
-        gradient.endPoint = CGPoint(x: 0.5, y: 1.0)
-        self.view.addSubview(gradient)
-        gradient.frame = self.view.bounds
-        self.view.sendSubviewToBack(gradient)
-        
-        
-        var gifAssetJson: JSON = JSON([
-            "mimeType": "MP4",
-            //            "data": "http://media2.giphy.com/media/FiGiRei2ICzzG/giphy.mp4",
-            "height": "200",
-            "width": "100",
-            "bitrate": "200",
-            "duration": 8,
-            "start_time": 100,
-            //            "endTime": "12345678",
-            "resourceLocation": "https://media.giphy.com/media/mbgpTdJmNkLAs/giphy.mp4"
-            ])
-        
-        let gifAsset = GifAsset(json: gifAssetJson)
-        
-        gifAssetJson["resourceLocation"] = "https://media.giphy.com/media/PXXf6yHelzoXu/giphy.mp4"
-        let gifAsset2 = GifAsset(json: gifAssetJson)
-        
-        gifAssetJson["resourceLocation"] = "https://media.giphy.com/media/vfz5C2BWduo36/giphy.mp4"
-        let gifAsset3 = GifAsset(json: gifAssetJson)
-        
-        var videoAssetJson: JSON = JSON([
-            "mimeType": "MP4",
-            "data": "http://media2.giphy.com/media/FiGiRei2ICzzG/giphy.mp4",
-            "height": "200",
-            "width": "100",
-            "bitrate": "200",
-            "duration": 10,
-            "start_time": 100,
-            //            "endTime": "12345678",
-            "resourceLocation": "http://media2.giphy.com/media/FiGiRei2ICzzG/giphy.mp4"
-            ])
-        
-        let videoAsset = VideoAsset(json: videoAssetJson)
-        
-        videoAssetJson["resourceLocation"] = "http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8"
-        let videoAsset2 = VideoAsset(json: videoAssetJson)
-        
-        videoAssetJson["resourceLocation"] = "http://cdn-fms.rbs.com.br/vod/hls_sample1_manifest.m3u8"
-        let videoAsset3 = VideoAsset(json: videoAssetJson)
-        
-        var imageAssetJson: JSON = JSON([
-            "image_url": "http://media.mydogspace.com.s3.amazonaws.com/wp-content/uploads/2013/08/puppy-500x350.jpg",
-            "height": 200,
-            "width": 100,
-            "duration": 10,
-            "type": "image",
-            //            "endTime": "12345678",
-            //            "resourceLocation": "http://media2.giphy.com/media/FiGiRei2ICzzG/giphy.mp4"
-            ])
-        
-        let imageAsset = ImageAsset(json: imageAssetJson)
-        
-        imageAssetJson["image_url"] = "http://moto.zombdrive.com/images/motocross-1.jpg"
-        let imageAsset2 = ImageAsset(json: imageAssetJson)
-        
-        imageAssetJson["image_url"] = "http://moto.zombdrive.com/images/motocross-2.jpg"
-        let imageAsset3 = ImageAsset(json: imageAssetJson)
-        
-        let assets: [Stageable] = ([videoAsset, gifAsset, imageAsset, gifAsset2, videoAsset2, imageAsset2, gifAsset3, imageAsset3, videoAsset3] as [Stageable?]).flatMap { $0 }
-        
-        var delay: NSTimeInterval = 5
-        
-        print("-------------- NEW RANDOMIZED STAGE SCHEDULE -----------------")
-        for asset in assets {
-            dispatch_after(delay) { [weak self] in
-                self?.startPlayingMedia(asset)
-            }
-            delay += NSTimeInterval(10)
-//            delay += NSTimeInterval(5 + arc4random_uniform(5))
-        }
-    }
-
 
     //MARK: - Stage
     
@@ -180,7 +96,7 @@ class StageViewController: UIViewController, Stage, VVideoPlayerDelegate {
     
     private func setupVideoView(containerView: UIView) -> VVideoView {
         let videoPlayer = VVideoView(frame: self.videoContentView.bounds)
-        videoPlayer.useAspectFit = true
+        videoPlayer.useAspectFit = false
         videoPlayer.delegate = self
         
         containerView.addSubview(videoPlayer.view)
