65ca8703a36e6fda76a005e649c05e0944c3261a
feef <fifihypnophonic@gmail.com>
2016-04-01 11:39:39 -0700

    fixed creation type test

---
+++ victorious/victoriousTests/CreationFlowTypeHelperTests.swift
@@ -0,0 +1,26 @@
+//
+//  CreationFlowTypeHelperTests.swift
+//  victorious
+//
+//  Created by Sharif Ahmed on 3/17/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import XCTest
+@testable import victorious
+
+class CreationFlowTypeHelperTests: XCTestCase {
+
+    func testCreationTypeForIdentifier() {
+        
+        XCTAssertEqual(CreationFlowTypeHelper.creationFlowTypeForIdentifier("Create Image"), VCreationFlowType.Image)
+        XCTAssertEqual(CreationFlowTypeHelper.creationFlowTypeForIdentifier("Create Video"), VCreationFlowType.Video)
+        XCTAssertEqual(CreationFlowTypeHelper.creationFlowTypeForIdentifier("Create Poll"), VCreationFlowType.Poll)
+        XCTAssertEqual(CreationFlowTypeHelper.creationFlowTypeForIdentifier("Create Text"), VCreationFlowType.Text)
+        XCTAssertEqual(CreationFlowTypeHelper.creationFlowTypeForIdentifier("Create GIF"), VCreationFlowType.GIF)
+        XCTAssertEqual(CreationFlowTypeHelper.creationFlowTypeForIdentifier("Create from Library"), VCreationFlowType.Library)
+        XCTAssertEqual(CreationFlowTypeHelper.creationFlowTypeForIdentifier("Create from Mixed Media Camera"), VCreationFlowType.MixedMediaCamera)
+        XCTAssertEqual(CreationFlowTypeHelper.creationFlowTypeForIdentifier("Create from Native Camera"), VCreationFlowType.NativeCamera)
+        XCTAssertEqual(CreationFlowTypeHelper.creationFlowTypeForIdentifier("Invalid Key"), VCreationFlowType.Unknown)
+    }
+}
