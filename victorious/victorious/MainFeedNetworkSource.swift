3a66d10d7604ae029c2f241a1dd1c41889226e66
Jarod Long <jarod@getvictorious.com>
2016-05-18 11:32:30 -0700

    Adds a stub MainFeedNetworkSource class.

---
+++ victorious/victorious/MainFeedNetworkSource.swift
@@ -0,0 +1,47 @@
+//
+//  MainFeedNetworkSource.swift
+//  victorious
+//
+//  Created by Jarod Long on 5/18/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+class MainFeedNetworkSource: NetworkSource {
+    // MARK: - Initialization
+    
+    init(dependencyManager: VDependencyManager) {
+        self.dependencyManager = dependencyManager
+    }
+    
+    // MARK: - Dependency manager
+    
+    private let dependencyManager: VDependencyManager
+    
+    // MARK: - ForumNetworkSource
+    
+    func setUp() {
+        // Nothing to set up.
+    }
+    
+    func tearDown() {
+        // Nothing to tear down.
+    }
+    
+    func addChildReceiver(receiver: ForumEventReceiver) {
+        childEventReceivers.append(receiver)
+    }
+    
+    func removeChildReceiver(receiver: ForumEventReceiver) {
+        if let index = childEventReceivers.indexOf({ $0 === receiver }) {
+            childEventReceivers.removeAtIndex(index)
+        }
+    }
+    
+    // MARK: - ForumEventSender
+    
+    private(set) weak var nextSender: ForumEventSender?
+    
+    // MARK: - ForumEventReceiver
+    
+    private(set) var childEventReceivers = [ForumEventReceiver]()
+}
