07f46938d2517d6bd06f8d76bffe9e1df2c2bf90
Jarod Long <jarod@getvictorious.com>
2016-05-19 15:04:55 -0700

    Renames MainFeedForumNetworkSource to RESTForumNetworkSource.

--- victorious/victorious/RESTForumNetworkSource.swift
+++ victorious/victorious/RESTForumNetworkSource.swift
@@ -1,12 +1,12 @@
 //
-//  MainFeedForumNetworkSource.swift
+//  RESTForumNetworkSource.swift
 //  victorious
 //
 //  Created by Jarod Long on 5/18/16.
 //  Copyright © 2016 Victorious. All rights reserved.
 //
 
-class MainFeedForumNetworkSource: NSObject, ForumNetworkSource {
+class RESTForumNetworkSource: NSObject, ForumNetworkSource {
     // MARK: - Initialization
     
     init(dependencyManager: VDependencyManager) {
