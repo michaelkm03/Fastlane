3de5a20759dec2e60a8346a959d8af08a6fe25bd
Darvish Kamalia <darvish.kamalia@gmail.com>
2016-05-26 10:35:16 -0700

    - Added new component file

---
+++ victorious/victorious/UI/Common/Background/VSystemBlurredImageBackground.swift
@@ -0,0 +1,32 @@
+//
+//  VSystemBlurredImageBackground.swift
+//  victorious
+//
+//  Created by Darvish Kamalia on 5/25/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
+
+class VSystemBlurredImageBackground : VBackground {
+//
+//    private let backgroundImage: UIImage
+//    private let dependencyManager: VDependencyManager
+//    private let VSystemBlurredImageBackgroundImageKey = "VSystemBlurredImageBackgroundImageKey"
+//
+//    required init (dependencyManager: VDependencyManager) {
+//        self.backgroundImage = dependencyManager.imageForKey(VSystemBlurredImageBackgroundImageKey)
+//        self.dependencyManager = dependencyManager
+//        super.init()
+//    }
+//
+//    override func viewForBackground() -> UIView! {
+//        let backgroundImageView = UIImageView(image: backgroundImage)
+//        let blurEffect = UIBlurEffect(style: .Light)
+//        let effectView = UIVisualEffectView(effect: blurEffect)
+//        effectView.frame = backgroundImageView.frame
+//        backgroundImageView.addSubview(backgroundImageView)
+//        return backgroundImageView
+//    }
+
+}
