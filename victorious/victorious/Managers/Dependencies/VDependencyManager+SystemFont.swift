2d4441749c4aec0b97febac5fe5f8347288af05b
Feef <fifihypnophonic@gmail.com>
2016-06-01 16:43:12 -0700

    added system font handling

---
+++ victorious/victorious/Managers/Dependencies/VDependencyManager+SystemFont.swift
@@ -0,0 +1,41 @@
+//
+//  VDependencyManager+SystemFont.swift
+//  victorious
+//
+//  Created by Sharif Ahmed on 6/1/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
+
+extension VDependencyManager {
+    
+    private func weightOfSystemFont(name: String) -> CGFloat? {
+        
+        let normalizedName = name.lowercaseString
+        guard normalizedName.hasPrefix("systemfont-") else {
+            return nil
+        }
+        
+        switch normalizedName {
+        case "systemfont-light":
+            return UIFontWeightLight
+        case "systemfont-regular":
+            return UIFontWeightRegular
+        case "systemfont-medium":
+            return UIFontWeightMedium
+        case "systemfont-bold":
+            return UIFontWeightBold
+        default:
+            return UIFontWeightRegular
+        }
+    }
+    
+    /// Returns a system font when appropriate
+    func font(name: String, size: CGFloat) -> UIFont? {
+        if let weight = weightOfSystemFont(name) {
+            return UIFont.systemFontOfSize(size, weight: weight)
+        }
+        return UIFont(name: name, size: size)
+    }
+}
