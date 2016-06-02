2cab9bd26000cdbe0d7df1f553f6fc1d4cc69725
Vincent Ho <hovincen@usc.edu>
2016-06-02 16:10:14 -0700

    POST instead of GET

--- victorious/VictoriousIOSSDK/ContentUnupvoteRequest.swift
+++ victorious/VictoriousIOSSDK/ContentUnupvoteRequest.swift
@@ -11,15 +11,13 @@ import Foundation
 public struct ContentUnupvoteRequest: RequestType {
     public var urlRequest: NSURLRequest {
         let request = NSMutableURLRequest(URL: contentUnupvoteURL)
+        request.HTTPMethod = "POST"
         return request
     }
     
-    private let contentID: String
     private let contentUnupvoteURL: NSURL
     
     public init?(contentID: String, contentUnupvoteURL: String) {
-        self.contentID = contentID
-
         let replacementDictionary: [String: String] = ["%%CONTENT_ID%%": contentID]
         let replacedURL = VSDKURLMacroReplacement().urlByReplacingMacrosFromDictionary(replacementDictionary, inURLString: contentUnupvoteURL)
         guard let validURL = NSURL(string: replacedURL) else {
--- victorious/VictoriousIOSSDK/ContentUpvoteRequest.swift
+++ victorious/VictoriousIOSSDK/ContentUpvoteRequest.swift
@@ -11,15 +11,13 @@ import Foundation
 public struct ContentUpvoteRequest: RequestType {
     public var urlRequest: NSURLRequest {
         let request = NSMutableURLRequest(URL: contentUpvoteURL)
+        request.HTTPMethod = "POST"
         return request
     }
     
-    private let contentID: String
     private let contentUpvoteURL: NSURL
     
     public init?(contentID: String, contentUpvoteURL: String) {
-        self.contentID = contentID
-        
         let replacementDictionary: [String: String] = ["%%CONTENT_ID%%": contentID]
         let replacedURL = VSDKURLMacroReplacement().urlByReplacingMacrosFromDictionary(replacementDictionary, inURLString: contentUpvoteURL)
         guard let validURL = NSURL(string: replacedURL) else {
