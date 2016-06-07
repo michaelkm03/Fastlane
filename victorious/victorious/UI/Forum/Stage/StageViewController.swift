b54a45e40068d1d83cf5f56db4c365549e37eabb
Darvish Kamalia <darvish.kamalia@gmail.com>
2016-06-06 17:23:14 -0700

    - Removed test code

--- victorious/victorious/UI/Forum/Stage/StageViewController.swift
+++ victorious/victorious/UI/Forum/Stage/StageViewController.swift
@@ -41,23 +41,6 @@ class StageViewController: UIViewController, Stage, VVideoPlayerDelegate {
         return dataSource
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let imageContent = Content(id: "1", createdAt: NSDate(), type: .image, text: "1", assets: [ContentMediaAsset(contentType: .image, url: NSURL(string: "http://www.w3schools.com/css/trolltunga.jpg")!)!], author: User(id: 1))
-        self.addContent(imageContent)
-        
-        dispatch_after(3) {
-            let imageContent = Content(id: "1", createdAt: NSDate(), type: .image, text: "1", assets: [ContentMediaAsset(contentType: .image, url: NSURL(string: "https://newevolutiondesigns.com/images/freebies/cool-iphone-wallpaper-1.jpg")!)!], author: User(id: 1))
-            self.addContent(imageContent)
-        }
-        
-        dispatch_after(6) {
-            let imageContent = Content(id: "1", createdAt: NSDate(), type: .image, text: "1", assets: [ContentMediaAsset(contentType: .image, url: NSURL(string: "http://media2.popsugar-assets.com/files/thumbor/Pn_lLpOa4pBvH6orCLvJcuMi6b8=/fit-in/1024x1024/2015/06/16/763/n/1922507/f703b42c_edit_img_image_3125473_1434372300_Daisies/i/iPhone-Wallpaper.jpg")!)!], author: User(id: 1))
-            self.addContent(imageContent)
-        }
-        
-    }
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         mediaContentView.videoCoordinator?.playVideo()
