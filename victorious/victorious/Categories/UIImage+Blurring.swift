e8f0e58a0d50f64e5b10451a13ea9ca85dfbf0d6
Sky Lan <4dreams.sky@gmail.com>
2016-06-10 16:07:08 -0700

    UIImage extension for blurring

---
+++ victorious/victorious/Categories/UIImage+Blurring.swift
@@ -0,0 +1,32 @@
+//
+//  UIImage+Blurring.swift
+//  victorious
+//
+//  Created by Tian Lan on 6/10/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    func applyBlur(withRadius radius: CGFloat, in bounds: CGRect) -> UIImage? {
+        guard let blurFilter = CIFilter(name: "CIGaussianBlur") else {
+            return nil
+        }
+        
+        blurFilter.setValue(UIKit.CIImage(image: self), forKey: kCIInputImageKey)
+        blurFilter.setValue(radius, forKey: kCIInputRadiusKey)
+        
+        let ciContext  = CIContext(options: nil)
+        
+        guard let result = blurFilter.valueForKey(kCIOutputImageKey) as? UIKit.CIImage else {
+            return nil
+        }
+        
+        let cgImage = ciContext.createCGImage(result, fromRect: bounds)
+        
+        let filteredImage = UIImage(CGImage: cgImage)
+        
+        return filteredImage
+    }
+}
--- victorious/victorious/Categories/UIImageView+Blurring.m
+++ victorious/victorious/Categories/UIImageView+Blurring.m
@@ -11,6 +11,7 @@
 #import "UIImage+Resize.h"
 #import <SDWebImage/SDWebImageManager.h>
 #import <objc/runtime.h>
+#import "victorious-Swift.h"
 
 @import AVFoundation;
 
@@ -206,10 +207,7 @@ static NSString * const kBlurredImageCachePathExtension = @"blurred";
                 return;
             }
             
-            UIImage *blurredImage = [image applyBlurWithRadius:blurRadius
-                                                     tintColor:nil
-                                         saturationDeltaFactor:1.0
-                                                     maskImage:nil];
+            UIImage *blurredImage = [image applyBlurWithRadius:blurRadius in:self.frame];
             
             dispatch_async(dispatch_get_main_queue(), ^
             {
