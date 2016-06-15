68737c47ae136845bd266e2a9c8e331dccaec202
Michael Sena <michaelsena@getvictorious.com>
2016-06-15 16:14:48 -0700

    Update comment formatting

--- victorious/victorious/Managers/Dependencies/AccessoryScreensKeyProvider.swift
+++ victorious/victorious/Managers/Dependencies/AccessoryScreensKeyProvider.swift
@@ -8,16 +8,15 @@
 
 import Foundation
 
-/**
- Objects in the responder chain that conform to this protocol have an opportunity to provide a new key
- for the accessory screens infrastructure to read from in the dependencyManager.
- */
+/// Objects in the responder chain that conform to this protocol have an opportunity to provide a new key 
+/// for the accessory screens infrastructure to read from in the dependencyManager.
 @objc protocol AccessoryScreensKeyProvider {
    
-    /**
-     Implment this to read from a custom accessory screens key. Such as when a screen component's screens are
-     based on some state like representing the current user or another user.
-     */
+    /// Implment this to read from a custom accessory screens key. Such as when a screen component's screens are
+    /// based on some state like representing the current user or another user.
+    ///
+    /// - Returns: An appropriate key to use for finding accessory screens.
+    ///
     func accessoryScreensKey() -> String?
     
 }
