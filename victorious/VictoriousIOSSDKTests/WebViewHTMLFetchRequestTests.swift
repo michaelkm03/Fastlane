1e37e35102daae14a1a48c67fb82490f78f5b5a0
Darvish Kamalia <darvish.kamalia@gmail.com>
2016-06-17 12:22:21 -0700

    - Removed old tests

---
+++ victorious/VictoriousIOSSDKTests/WebViewHTMLFetchRequestTests.swift
@@ -0,0 +1,9 @@
+//
+//  WebViewHTMLFetchRequestTests.swift
+//  victorious
+//
+//  Created by Darvish Kamalia on 6/17/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
