45e8aeb4f5ffe87347f641cb4382f68bf5f16a7a
Darvish Kamalia <darvish.kamalia@gmail.com>
2016-06-17 14:30:28 -0700

    - Fixed tests

--- victorious/VictoriousIOSSDKTests/WebViewHTMLFetchRequestTests.swift
+++ victorious/VictoriousIOSSDKTests/WebViewHTMLFetchRequestTests.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import XCTest
+
+@testable import VictoriousIOSSDK
+
+class WebViewHTMLFetchRequestTests: XCTest {
+    func testResponseParsing() {
+        guard let mockResponseDataURL = NSBundle(forClass: self.dynamicType).URLForResource("PrivacyPolicy", withExtension: "html"),
+            let mockData = NSData(contentsOfURL: mockResponseDataURL) else {
+                XCTFail("Error reading mock html data")
+                return
+        }
+        do {
+            let ppRequest = WebViewHTMLFetchRequest(urlPath: mockResponseDataURL.path!)
+            let result = try ppRequest.parseResponse(NSURLResponse(), toRequest: ppRequest.urlRequest, responseData: mockData, responseJSON: JSON(data:mockData))
+            XCTAssertEqual(result, "<html>testHTML</html>")
+        } catch {
+            XCTFail("Sorry, parseResponse should not throw here")
+        }
+    }
+    
+    func testTOSParsing() {
+        guard let mockResponseDataURL = NSBundle(forClass: self.dynamicType).URLForResource("tos", withExtension: "json"),
+            let mockData = NSData(contentsOfURL: mockResponseDataURL) else {
+                XCTFail("Error reading mock json data")
+                return
+        }
+        do {
+            let tosRequest = TermsOfServiceRequest()
+            let htmlString = try tosRequest.parseResponse(NSURLResponse(), toRequest: tosRequest.urlRequest, responseData: mockData, responseJSON: JSON(data:mockData))
+            XCTAssertEqual(htmlString, "<html>testHTML</html>")
+        } catch {
+            XCTFail("Sorry, parseResponse should not throw here")
+        }
+    }
+}
