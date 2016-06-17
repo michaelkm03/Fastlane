913a768ad45d7b18e1616bbcd31ef26885d773b7
Darvish Kamalia <darvish.kamalia@gmail.com>
2016-06-17 11:21:43 -0700

    - Removed invite friends

--- victorious/victorious/UI/Settings/VSettingsViewController.m
+++ victorious/victorious/UI/Settings/VSettingsViewController.m
@@ -35,7 +35,6 @@ typedef NS_ENUM(NSInteger, VSettingsAction)
 {
     VSettingsActionEditProfile,
     VSettingsActionLikedContent,
-    VSettingsActionInviteFriends,
     VSettingsActionChangePassword,
     VSettingsActionNotifications,
     VSettingsActionResetPurchases,
