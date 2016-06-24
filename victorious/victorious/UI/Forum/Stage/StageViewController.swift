a6d54b1ebe7c043715cd8e360c6ccc86c2117eb0
Darvish Kamalia <darvish.kamalia@gmail.com>
2016-06-24 16:26:47 -0700

    pls

--- victorious/victorious/UI/Forum/Stage/StageViewController.swift
+++ victorious/victorious/UI/Forum/Stage/StageViewController.swift
@@ -85,7 +85,6 @@ class StageViewController: UIViewController, Stage, AttributionBarDelegate {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-    
         mediaContentView.allowsVideoControls = false
         showStage(animated)
     }
