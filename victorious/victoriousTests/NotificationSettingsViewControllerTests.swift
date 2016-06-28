52e63e2589871fc92b253a8f3a41877421ef08a8
Darvish Kamalia <darvish.kamalia@gmail.com>
2016-06-28 14:33:38 -0700

    Replaced tests

---
+++ victorious/victoriousTests/NotificationSettingsViewControllerTests.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationSettingsViewControllerTests.swift
+//  victorious
+//
+//  Created by Darvish Kamalia on 6/28/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
