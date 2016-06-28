db29e8f93a1b9c750be30d512fbd7e18c841d0e4
Sky Lan <4dreams.sky@gmail.com>
2016-06-27 20:43:16 -0700

    Simplify logic by embedding destination information in DeeplinkDestination enum

--- victorious/victorious/UI/Routing/Router.swift
+++ victorious/victorious/UI/Routing/Router.swift
@@ -10,8 +10,8 @@ import Foundation
 
 /// FUTURE:
 /// - Queueing deep links (before scaffold is ready)
-/// - Unify navigating to `ContentModel` and `ContentID` by refactoring `ShowCloseUpOperation`
 /// - Remove DeepLinkReceiver
+
 
 struct Router {
     private weak var originViewController: UIViewController!
@@ -27,52 +27,37 @@ struct Router {
             case .image, .video, .gif:
                 showCloseUpView(for: content)
             case .link:
-                guard
-                    let linkedURL = content.linkedURL,
-                    let destination = DeeplinkDestination(url: linkedURL)
-                else {
+                guard let linkedURL = content.linkedURL else {
                     showError()
                     return
                 }
-                navigate(to: destination, targetContentWrapper: .id(contentID: linkedURL.v_firstNonSlashPathComponent()))
+                navigate(to: linkedURL)
             case .text:
-                // We currently don't support tapping on text content
+                // FUTURE: We currently don't support tapping on text content
                 break
         }
     }
     
     func navigate(to url: NSURL) {
-        guard
-            let destination = DeeplinkDestination(url: url),
-            let contentID = url.v_firstNonSlashPathComponent()
-        else {
+        guard let destination = DeeplinkDestination(url: url) else {
             showError()
             return
         }
         
-        navigate(to: destination, targetContentWrapper: .id(contentID: contentID))
-    }
-    
-    private func navigate(to destination: DeeplinkDestination, targetContentWrapper contentWrapper: ContentWrapper) {
         switch destination {
-        case .profile: break
-        case .closeUp:
-            switch contentWrapper {
-                case .content(let content):
-                    showCloseUpView(for: content)
-                case .id(let contentID):
-                    showCloseUpView(for: contentID)
-            }
-        case .vipForum:
-            showVIPForum()
-        case .externalURL:
-            // Show web content
-            break
+            case .profile(let userID):
+                showProfile(for: userID)
+            case .closeUp(let contentID):
+                showCloseUpView(for: contentID)
+            case .vipForum:
+                showVIPForum()
+            case .externalURL:
+                // FUTURE: Show Web Content
+                break
         }
     }
     
     typealias ContentID = String
-    
     private func showCloseUpView(for contentID: ContentID) {
         let displayModifier = ShowCloseUpDisplayModifier(dependencyManager: dependencyManager, originViewController: originViewController)
         ShowCloseUpOperation.showOperation(forContentID: contentID, displayModifier: displayModifier).queue()
@@ -87,6 +72,11 @@ struct Router {
         ShowForumOperation(originViewController: originViewController, dependencyManager: dependencyManager, showVIP: true, animated: true).queue()
     }
     
+    typealias UserID = Int
+    private func showProfile(for userID: UserID) {
+        ShowProfileOperation(originViewController: originViewController, dependencyManager: dependencyManager, userId: userID).queue()
+    }
+    
     private func showError() {
         let title = NSLocalizedString("Missing Content", comment: "The title of the alert saying we can't find a piece of content")
         let message = NSLocalizedString("Missing Content Message", comment: "A deep linked content has a wrong destination URL that we can't navigate to")
@@ -94,45 +84,36 @@ struct Router {
     }
 }
 
-private struct DeeplinkMapper {
-    private static var urlHostToDestinationMapping: [String: DeeplinkDestination] {
-        return [
-            "content": .closeUp,
-            "profile": .profile,
-            "vipForum": .vipForum,
-        ]
-    }
-    
-    typealias URLHostComponent = String
-    
-    static func deeplinkDestination(for host: URLHostComponent?) -> DeeplinkDestination? {
-        guard let host = host else {
-            return nil
-        }
-        return urlHostToDestinationMapping[host]
-    }
-}
-
 private enum DeeplinkDestination {
-    case profile
-    case closeUp
+    case profile(userID: Int)
+    case closeUp(contentID: String)
     case vipForum
     case externalURL
     
     init?(url: NSURL) {
         switch url.scheme.lowercaseString {
-        case "vthisapp":
-            guard let destination = DeeplinkMapper.deeplinkDestination(for: url.host) else {
-                return nil
-            }
-            self = destination
-        default:
-            self = .externalURL
+            case "vthisapp":
+                guard let host = url.host else {
+                    return nil
+                }
+                switch host {
+                    case "content":
+                        guard let contentID = url.v_firstNonSlashPathComponent() else {
+                            return nil
+                        }
+                        self = .closeUp(contentID: contentID)
+                    case "profile":
+                        guard let userID = Int(url.v_firstNonSlashPathComponent()) else {
+                            return nil
+                        }
+                        self = .profile(userID: userID)
+                    case "vipForum":
+                        self = .vipForum
+                    default:
+                        self = .externalURL
+                }
+            default:
+                self = .externalURL
         }
     }
-}
-
-private enum ContentWrapper {
-    case content(content: ContentModel)
-    case id(contentID: String)
 }
