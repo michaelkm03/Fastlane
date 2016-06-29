45e588baf72d5250524939862d90629a25c9e589
Sky Lan <4dreams.sky@gmail.com>
2016-06-29 15:34:52 -0700

    Break out DeeplinkDestination and implement showTrophyCase()

---
+++ victorious/victorious/UI/Routing/DeeplinkDestination.swift
@@ -0,0 +1,76 @@
+//
+//  DeeplinkDestination.swift
+//  victorious
+//
+//  Created by Tian Lan on 6/29/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
+
+/// A deeplink destination that we can naviagte to within the app, or an external URL
+/// e.g. A piece of content, a user, or a specific screen that is deep linked to.
+enum DeeplinkDestination {
+    case profile(userID: Int)
+    case closeUp(contentID: String)
+    case vipForum
+    case trophyCase
+    case externalURL
+    
+    typealias ContentID = String
+    typealias UserID = Int
+    
+    init?(url: NSURL) {
+        let scheme = url.scheme.lowercaseString
+        
+        // First check if we have a deep link URL or an external URL
+        guard scheme == "vthisapp" else {
+            self = .externalURL
+            return
+        }
+        
+        // Then find out where we would like to deep link to
+        guard let host = url.host else {
+            assertionFailure("We got a deep link URL but no host component, so we don't know where to navigate")
+            return nil
+        }
+        
+        switch host {
+        case "content":
+            guard let contentID = url.v_firstNonSlashPathComponent() else { return nil }
+            self = .closeUp(contentID: contentID)
+        case "profile":
+            guard let userID = Int(url.v_firstNonSlashPathComponent()) else { return nil }
+            self = .profile(userID: userID)
+        case "vipForum":
+            self = .vipForum
+        case "trophyCase":
+            self = .trophyCase
+        default:
+            assertionFailure("Unrecgonized host for the deep link URL")
+            return nil
+        }
+    }
+    
+    init?(content: ContentModel) {
+        switch content.type {
+        case .image, .video, .gif:
+            guard let contentID = content.id else { return nil }
+            self = .closeUp(contentID: contentID)
+        case .link:
+            guard
+                let url = content.linkedURL,
+                let validDestination = DeeplinkDestination(url: url)
+                else {
+                    return nil
+            }
+            self = validDestination
+        case .text:
+            return nil
+        }
+    }
+    
+    init(userID: UserID) {
+        self = .profile(userID: userID)
+    }
+}
--- victorious/victorious/UI/Routing/Router.swift
+++ victorious/victorious/UI/Routing/Router.swift
@@ -8,9 +8,7 @@
 
 import Foundation
 
-/// A Router object that is able to navigate to a destination in the app,
-/// e.g. A piece of content, a user, or a specific screen that is deep linked to.
-/// Use one of the `navigate(to:) APIs to perform the navigation.
+/// A Router object that is able to navigate to a deeplink destination in the app
 struct Router {
     
     // MARK: - Initialization
@@ -67,77 +65,13 @@ struct Router {
     }
     
     private func showTrophyCase() {
-        
+        guard let originViewController = self.originViewController else { return }
+        ShowTrophyCaseOperation(originViewController: originViewController, dependencyManager: dependencyManager).queue()
     }
     
     private func showError() {
         let title = NSLocalizedString("Missing Content", comment: "The title of the alert saying we can't find a piece of content")
         let message = NSLocalizedString("Missing Content Message", comment: "A deep linked content has a wrong destination URL that we can't navigate to")
         originViewController?.v_showAlert(title: title, message: message)
-    }
-}
-
-enum DeeplinkDestination {
-    case profile(userID: Int)
-    case closeUp(contentID: String)
-    case vipForum
-    case trophyCase
-    case externalURL
-    
-    typealias ContentID = String
-    typealias UserID = Int
-    
-    init?(url: NSURL) {
-        let scheme = url.scheme.lowercaseString
-        
-        // First check if we have a deep link URL or an external URL
-        guard scheme == "vthisapp" else {
-            self = .externalURL
-            return
-        }
-        
-        // Then find out where we would like to deep link to
-        guard let host = url.host else {
-            assertionFailure("We got a deep link URL but no host component, so we don't know where to navigate")
-            return nil
-        }
-        
-        switch host {
-            case "content":
-                guard let contentID = url.v_firstNonSlashPathComponent() else { return nil }
-                self = .closeUp(contentID: contentID)
-            case "profile":
-                guard let userID = Int(url.v_firstNonSlashPathComponent()) else { return nil }
-                self = .profile(userID: userID)
-            case "vipForum":
-                self = .vipForum
-            case "trophyCase":
-                self = .trophyCase
-            default:
-                assertionFailure("Unrecgonized host for the deep link URL")
-                return nil
-        }
-    }
-    
-    init?(content: ContentModel) {
-        switch content.type {
-            case .image, .video, .gif:
-                guard let contentID = content.id else { return nil }
-                self = .closeUp(contentID: contentID)
-            case .link:
-                guard
-                    let url = content.linkedURL,
-                    let validDestination = DeeplinkDestination(url: url)
-                else { 
-                    return nil
-                }
-                self = validDestination
-            case .text:
-                return nil
-        }
-    }
-    
-    init(userID: UserID) {
-        self = .profile(userID: userID)
     }
 }
