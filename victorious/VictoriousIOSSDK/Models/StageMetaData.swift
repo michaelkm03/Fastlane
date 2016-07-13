d158d1397072dbcb09cfc3e8771f3cd107dcf46f
Sebastian Nystorm <abeansits@gmail.com>
2016-07-13 13:25:05 -0700

    Updated documentation

--- victorious/VictoriousIOSSDK/Models/StageMetaData.swift
+++ victorious/VictoriousIOSSDK/Models/StageMetaData.swift
@@ -8,7 +8,13 @@
 
 import Foundation
 
+///
 /// Contains meta data about the content currently on stage right now.
+/// 
+/// Since this struct holds data from two separate parts of the stage update flow, it is populated in two steps.
+/// 1. When a stage refresh message is received from the backend.
+/// 2. When the actual content has been fetched.
+///
 public struct StageMetaData {
 
     let title: String?
@@ -21,7 +27,7 @@ public struct StageMetaData {
         author = nil
     }
 
-    /// The StageMetaData struct is constructed at a two step process and aggregates data from the stage refresh even and the content fetching event.
+    /// Populates the StageMetaData struct with information about the content and it's author.
     public mutating func populateWith(content: ContentModel) {
         self.description = content.text
         self.author = content.author
