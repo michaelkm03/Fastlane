6fb177f96546d82f5445f1f9acbbedea12fdcbad
Feef <fifihypnophonic@gmail.com>
2016-07-15 12:07:52 -0700

    revert tab menu changes

--- victorious/victorious/UI/Root View/Tab Menu/VTabMenuShim.m
+++ victorious/victorious/UI/Root View/Tab Menu/VTabMenuShim.m
@@ -74,13 +74,8 @@
         if ([menuItem.destination isKindOfClass:[UIViewController class]])
         {
             UIViewController *viewController = (UIViewController *)menuItem.destination;
-            UIViewController *containedNavigationController = viewController;
-            if (![containedNavigationController isKindOfClass:[UINavigationController class]])
-            {
-                VNavigationController *navigationController = [[VNavigationController alloc] initWithDependencyManager:self.dependencyManager];
-                [navigationController.innerNavigationController pushViewController:viewController animated:NO];
-                containedNavigationController = navigationController;
-            }
+            VNavigationController *containedNavigationController = [[VNavigationController alloc] initWithDependencyManager:self.dependencyManager];
+            [containedNavigationController.innerNavigationController pushViewController:viewController animated:NO];
             shimViewController.containedViewController = containedNavigationController;
         }
         
