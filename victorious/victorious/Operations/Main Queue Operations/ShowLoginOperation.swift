09a708cb668ba376500f1414d03714420c336bb7
Vincent Ho <hovincen@usc.edu>
2016-07-18 16:50:18 -0700

    add fix otherwise if you are logged in nothing happens.

--- victorious/victorious/Operations/Main Queue Operations/ShowLoginOperation.swift
+++ victorious/victorious/Operations/Main Queue Operations/ShowLoginOperation.swift
@@ -34,12 +34,14 @@ class ShowLoginOperation: MainQueueOperation {
         
         // Don't show login when running unit tests
         guard !cancelled && !VAutomation.shouldAlwaysShowLoginScreen() else {
+            loginCompletion?()
             finishedExecuting()
             return
         }
         
         // Don't show login if the user is already logged in
         guard VCurrentUser.user() == nil else {
+            loginCompletion?()
             finishedExecuting()
             return
         }
