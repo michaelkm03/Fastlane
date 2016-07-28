32f92cb8a1c635c9e57de1c4c7f18b540fb47c39
Alex Tamoykin <alex@rigthbalance.io>
2016-07-28 15:35:11 -0700

    Fixed AccountUpdateRequestTests

--- victorious/VictoriousIOSSDKTests/AccountUpdateRequestTests.swift
+++ victorious/VictoriousIOSSDKTests/AccountUpdateRequestTests.swift
@@ -11,21 +11,10 @@ import XCTest
 
 class AccountUpdateRequestTests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testRequestWithProfile() {
         let updateRequest = AccountUpdateRequest(
             profileUpdate: ProfileUpdate(
-                email: "joe@example.com",
-                name: "Joe Victorious",
+                displayName: "Joe Victorious",
                 location: "Bethesda, MD",
                 tagline: "Example",
                 profileImageURL: nil
@@ -53,8 +42,7 @@ class AccountUpdateRequestTests: XCTestCase {
         }
         
         guard let updateRequest = AccountUpdateRequest(profileUpdate: ProfileUpdate(
-            email: "joe@example.com",
-            name: "Joe Victorious",
+            displayName: "Joe Victorious",
             location: "Bethesda, MD",
             tagline: "Example",
             profileImageURL: nil
