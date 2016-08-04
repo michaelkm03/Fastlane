8b0f0271e37cc3c34f26cb2b8e3a3fd916b908ff
Sky Lan <4dreams.sky@gmail.com>
2016-08-03 17:16:25 -0700

    Revert "Always fetch content when going into close up view."
    861f4579729dcfd2235727b4c35db6b47fc929b8

--- victorious/victorious/UI/Routing/Router.swift
+++ victorious/victorious/UI/Routing/Router.swift
@@ -34,7 +34,7 @@ struct Router {
         
         switch destination {
             case .profile(let userID): showProfile(for: userID)
-            case .closeUp(let contentID): showCloseUpView(for: contentID)
+            case .closeUp(let contentWrapper): showCloseUpView(for: contentWrapper)
             case .vipForum: showVIPForum()
             case .externalURL(let url, let addressBarVisible): showWebView(for: url, addressBarVisible: addressBarVisible)
         }
@@ -42,10 +42,19 @@ struct Router {
     
     // MARK: - Private Helper Functions
     
-    private func showCloseUpView(for contentID: Content.ID) {
+    private func showCloseUpView(for contentWrapper: CloseUpContentWrapper) {
         guard let originViewController = self.originViewController else { return }
         let displayModifier = ShowCloseUpDisplayModifier(dependencyManager: dependencyManager, originViewController: originViewController)
-        ShowCloseUpOperation.showOperation(forContentID: contentID, displayModifier: displayModifier).queue()
+
+        switch contentWrapper {
+            case .content(let content):
+                guard content.type != .text else {
+                    return
+                }
+                ShowCloseUpOperation.showOperation(forContent: content, displayModifier: displayModifier).queue()
+            case .contentID(let id):
+                ShowCloseUpOperation.showOperation(forContentID: id, displayModifier: displayModifier).queue()
+        }
     }
     
     private func showVIPForum() {
