4a25b78608ea29ee962c5ad696ca73ae1dfd5f70
Jarod Long <jarod@getvictorious.com>
2016-08-05 12:20:21 -0700

    Removes unused SDK models and search operations.

--- victorious/VictoriousIOSSDK/NSPredicate+Pagination.swift
+++ victorious/VictoriousIOSSDK/NSPredicate+Pagination.swift
@@ -8,16 +8,16 @@
 
 import Foundation
 
-public extension NSPredicate {
-	
-	public convenience init(vsdk_format format: String, vsdk_argumentArray argumentArray: [AnyObject]?, vsdk_paginator paginator: NumericPaginator ) {
-		let connector = format.isEmpty ? "" : " && "
-		let paginationFormat = connector + "displayOrder >= %@ && displayOrder < %@"
-		let paginationArguments: [AnyObject] = [paginator.displayOrderRangeStart, paginator.displayOrderRangeEnd]
-		self.init(format: format + paginationFormat, argumentArray: (argumentArray ?? []) + paginationArguments)
-	}
-	
-	public convenience init(vsdk_paginator paginator: NumericPaginator ) {
-		self.init(vsdk_format: "", vsdk_argumentArray: [], vsdk_paginator: paginator)
-	}
-}
+//public extension NSPredicate {
+//	
+//	public convenience init(vsdk_format format: String, vsdk_argumentArray argumentArray: [AnyObject]?, vsdk_paginator paginator: NumericPaginator ) {
+//		let connector = format.isEmpty ? "" : " && "
+//		let paginationFormat = connector + "displayOrder >= %@ && displayOrder < %@"
+//		let paginationArguments: [AnyObject] = [paginator.displayOrderRangeStart, paginator.displayOrderRangeEnd]
+//		self.init(format: format + paginationFormat, argumentArray: (argumentArray ?? []) + paginationArguments)
+//	}
+//	
+//	public convenience init(vsdk_paginator paginator: NumericPaginator ) {
+//		self.init(vsdk_format: "", vsdk_argumentArray: [], vsdk_paginator: paginator)
+//	}
+//}
