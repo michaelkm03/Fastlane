b056ec96a0489e3adb39f27516d03ebb76522eef
Feef <fifihypnophonic@gmail.com>
2016-08-11 11:08:11 -0700

    remove testing code

--- victorious/victorious/UI/Forum/ForumViewController.swift
+++ victorious/victorious/UI/Forum/ForumViewController.swift
@@ -233,8 +233,6 @@ class ForumViewController: UIViewController, Forum, VBackgroundContainer, VFocus
             
             self.forumNetworkSource = forumNetworkSource
         }
-        
-        ShowVIPFlowOperation(originViewController: self, dependencyManager: dependencyManager).queue()
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
