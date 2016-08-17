b3efad504bc4815aaacc290d09bc2e52e8d0dfc9
Jarod Long <jarod@getvictorious.com>
2016-08-17 11:46:06 -0700

    Adds new dependency manager extensions for accessing accessory screens.

---
+++ victorious/victorious/Managers/Dependencies/VDependencyManager+AccessoryScreens.swift
@@ -0,0 +1,75 @@
+//
+//  VDependencyManager+AccessoryScreens.swift
+//  victorious
+//
+//  Created by Jarod Long on 8/16/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import UIKit
+
+extension VDependencyManager {
+    /// Returns all of the accessory screens contained in the dependency manager at the given `key`, or nil if no
+    /// accessory screens exist.
+    @warn_unused_result func accessoryScreens(for key: String = "accessoryScreens") -> [AccessoryScreen]? {
+        guard let accessoryScreenDependencyManagers = childDependencies(for: key + "fake") else {
+            logger.warning("Tried to get accessory screens from dependency manager for key '\(key)', but a value for that key was not found.")
+            return nil
+        }
+        
+        return accessoryScreenDependencyManagers.flatMap { AccessoryScreen(dependencyManager: $0) }
+    }
+}
+
+struct AccessoryScreen {
+    // MARK: - Initializing
+    
+    init?(dependencyManager: VDependencyManager) {
+        guard let id = dependencyManager.stringForKey("identifier") else {
+            logger.warning("Tried to initialize an accessory screen without an identifier.")
+            return nil
+        }
+        
+        self.id = id
+        title = dependencyManager.stringForKey("title")
+        icon = dependencyManager.imageForKey("icon")
+        destinationID = dependencyManager.stringForKey("destination")
+        position = AccessoryScreenPosition(string: dependencyManager.stringForKey("position") ?? "") ?? .left
+    }
+    
+    // MARK: - Accessing values
+    
+    /// A string that uniquely identifies a particular accessory screen used in the app.
+    var id: String
+    
+    /// The title of the accessory screen.
+    var title: String?
+    
+    /// An icon that can be used to display a button that navigates to this accessory screen.
+    var icon: UIImage?
+    
+    /// The component ID of the accessory screen's destination, if any.
+    var destinationID: String?
+    
+    /// The position of the accessory screen's button when displayed in a navigation bar.
+    var position: AccessoryScreenPosition
+    
+    // MARK: - Loading the destination
+    
+    func loadDestination() {
+        // TODO: Implement me!
+    }
+}
+
+/// An enum for the position that an accessory screen's button can be located at.
+enum AccessoryScreenPosition {
+    case left, right
+    
+    init?(string: String) {
+        switch string {
+            case "left": self = .left
+            case "right": self = .right
+            default: return nil
+        }
+    }
+}
--- victorious/victorious/Managers/Dependencies/VDependencyManager+Subcomponent.swift
+++ victorious/victorious/Managers/Dependencies/VDependencyManager+Subcomponent.swift
@@ -9,15 +9,24 @@
 import Foundation
 
 extension VDependencyManager {
-    
     /// Using the provided key, attempts to retrieve a dictionary used to intantiate
     /// a child dependency manager for a subcomponent, such as a "cell" component
     /// of a ".screen" component with a table or collection view.
     func childDependencyForKey(key: String) -> VDependencyManager? {
-    
-        if let configuration = templateValueOfType(NSDictionary.self, forKey: key) as? [NSObject: AnyObject] {
-            return self.childDependencyManagerWithAddedConfiguration(configuration)
+        guard let configuration = templateValueOfType(NSDictionary.self, forKey: key) as? [NSObject: AnyObject] else {
+            return nil
         }
-        return nil
+        
+        return childDependencyManagerWithAddedConfiguration(configuration)
+    }
+    
+    /// Returns an array of child dependency managers located at the given `key`, or nil if an array doesn't exist at
+    /// that key.
+    func childDependencies(for key: String) -> [VDependencyManager]? {
+        guard let array = arrayForKey(key) as? [[NSObject: AnyObject]] else {
+            return nil
+        }
+        
+        return array.flatMap { childDependencyManagerWithAddedConfiguration($0) }
     }
 }
