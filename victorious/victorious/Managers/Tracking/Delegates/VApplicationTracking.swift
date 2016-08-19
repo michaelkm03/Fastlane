e986075c590ce15c38956eb7c044c223aa66d470
Sky Lan <4dreams.sky@gmail.com>
2016-08-19 10:27:23 -0700

    Get all tracking on the background queue.

--- victorious/victorious/Managers/Tracking/Delegates/VApplicationTracking.swift
+++ victorious/victorious/Managers/Tracking/Delegates/VApplicationTracking.swift
@@ -13,15 +13,14 @@ extension VApplicationTracking {
     
     func sendRequest(url: NSURL, eventIndex: Int, completion: NSError? -> Void) {
         let request = ApplicationTrackingRequest(trackingURL: url, eventIndex: eventIndex)
-        dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)) {
-            MainRequestExecutor().executeRequest(request,
-                onComplete: { _ in
-                    completion(nil)
-                },
-                onError: { error in
-                    completion(error)
-                }
-            )
-        }
+        
+        MainRequestExecutor().executeRequest(request,
+            onComplete: { _ in
+                completion(nil)
+            },
+            onError: { error in
+                completion(error)
+            }
+        )
     }
 }
--- victorious/victorious/Managers/Tracking/VTrackingManager.m
+++ victorious/victorious/Managers/Tracking/VTrackingManager.m
@@ -196,7 +196,9 @@
     
     [self.delegates enumerateObjectsUsingBlock:^(id<VTrackingDelegate> delegate, NSUInteger idx, BOOL *stop)
      {
-         [delegate trackEventWithName:eventName parameters:completeParams];
+         dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
+             [delegate trackEventWithName:eventName parameters:completeParams];
+         });
      }];
 }
 
