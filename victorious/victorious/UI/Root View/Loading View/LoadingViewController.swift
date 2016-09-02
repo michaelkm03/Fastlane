8fac95fe243060dcc6f79966a54eacbd59123a00
Vincent Ho <hovincen@usc.edu>
2016-09-02 12:25:58 -0700

    private loading helper

--- victorious/victorious/UI/Root View/Loading View/LoadingViewController.swift
+++ victorious/victorious/UI/Root View/Loading View/LoadingViewController.swift
@@ -16,21 +16,117 @@ extension VLoadingViewController {
         }
         isLoading = true
         
-        let loadingOperation = StartLoadingOperation()
-        loadingOperation.completion = { [weak self] in
+        let loadingHelper = LoadingHelper()
+        loadingHelper.completion = { [weak self] in
             self?.isLoading = false
             self?.progressHUD.taskInProgress = false
             self?.progressHUD.hide(true)
-            guard let template = loadingOperation.template else {
+            guard let template = loadingHelper.template else {
                 return
             }
             self?.onDoneLoadingWithTemplateConfiguration(template as [NSObject: AnyObject])
         }
-        loadingOperation.execute()
+        loadingHelper.execute()
         
         progressHUD = MBProgressHUD.showHUDAddedTo(view, animated: true)
         progressHUD.mode = .Indeterminate
         progressHUD.graceTime = 2.0
         progressHUD.taskInProgress = true
+    }
+}
+
+private class LoadingHelper: NSObject, VTemplateDownloadOperationDelegate {
+    var completion: (Void -> Void)?
+    var template: NSDictionary? {
+        if let templateConfiguration = self.templateDownloadOperation.templateConfiguration {
+            return templateConfiguration
+        }
+        else {
+            let environmentManager = VEnvironmentManager.sharedInstance()
+            guard let buildNumber = NSBundle.mainBundle().objectForInfoDictionaryKey(kCFBundleVersionKey as String) as? String else {
+                return nil
+            }
+            let templateCache = TemplateCache(dataCache: VDataCache(),
+                                              environment: environmentManager.currentEnvironment,
+                                              buildNumber: buildNumber)
+            self.templateDownloadOperation.templateCache = templateCache
+            
+            guard let cachedTemplateData = templateCache.cachedTemplateData() else {
+                return nil
+            }
+            let cachedTemplate = VTemplateSerialization.templateConfigurationDictionaryWithData(cachedTemplateData)
+            
+            return cachedTemplate
+        }
+    }
+    
+    private lazy var templateDownloadOperation: VTemplateDownloadOperation = {
+        VTemplateDownloadOperation(downloader: PersistenceTemplateDownloader(), andDelegate: self)
+    }()
+    
+    private var dependencyManager: VDependencyManager {
+        var defaultDependencyManager = VDependencyManager.dependencyManagerWithDefaultValuesForColorsAndFonts()
+        if let template = template as? [NSObject: AnyObject] {
+            let parentManager = VDependencyManager(
+                parentManager: defaultDependencyManager,
+                configuration: nil,
+                dictionaryOfClassesByTemplateName: nil
+            )
+            
+            defaultDependencyManager = VDependencyManager(
+                parentManager: parentManager,
+                configuration: template,
+                dictionaryOfClassesByTemplateName: nil
+            )
+        }
+        
+        return defaultDependencyManager
+    }
+    
+    private lazy var loginOperation: StoredLoginOperation = {
+        return StoredLoginOperation(dependencyManager: self.dependencyManager)
+    }()
+    
+    func execute() {
+        TempDirectoryCleanupOperation().queue()
+        
+        if template == nil {
+            templateDownloadOperation.completionBlock = { [weak self] in
+                self?.loginOperation.queue() { _ in
+                    self?.completion?()
+                }
+            }
+        }
+        else {
+            templateDownloadOperation.after(loginOperation)
+            loginOperation.queue() { [weak self] _ in
+                self?.completion?()
+            }
+        }
+        
+        let backgroundQueue = Queue.background.operationQueue
+        backgroundQueue.addOperation(templateDownloadOperation)
+    }
+    
+    // MARK: - VTemplateDownloadOperationDelegate
+    
+    @objc private func templateDownloadOperationFailed(downloadOperation: VTemplateDownloadOperation) {
+        dispatch_async(dispatch_get_main_queue()) {
+            /// If the template download failed and we're using a user environment, then we should switch back to the default
+            if VEnvironmentManager.sharedInstance().currentEnvironment.isUserEnvironment {
+                downloadOperation.cancel()
+                VEnvironmentManager.sharedInstance().revertToPreviousEnvironment()
+                
+                let userInfo = [
+                    VEnvironmentDidFailToLoad: true
+                ]
+                
+                NSNotificationCenter.defaultCenter().postNotificationName(
+                    VSessionTimerNewSessionShouldStart,
+                    object: self,
+                    userInfo: userInfo
+                )
+            }
+        }
     }
 }
