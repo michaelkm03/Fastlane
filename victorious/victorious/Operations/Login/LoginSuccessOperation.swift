eecedbcbf1c5aeccd3c884cb046759e9342be60c
Sky Lan <4dreams.sky@gmail.com>
2016-09-13 18:03:00 -0700

    Fetch user on login success.

--- victorious/victorious/Operations/Login/LoginSuccessOperation.swift
+++ victorious/victorious/Operations/Login/LoginSuccessOperation.swift
@@ -32,15 +32,39 @@ class LoginSuccessOperation: SyncOperation<Void> {
     
     override func execute() -> OperationResult<Void> {
         let currentUser = response.user
-            
+        
+        // Store current user's login information to singleton object
         VCurrentUser.loginType = parameters.loginType
         VCurrentUser.token = response.token
         VCurrentUser.accountIdentifier = parameters.accountIdentifier
         VCurrentUser.isNewUser = response.newUser
-        VCurrentUser.update(to: currentUser)
         
+        VCurrentUser.update(to: currentUser)
         updateStoredCredentials(currentUser)
         VLoginType(rawValue: parameters.loginType.rawValue)?.trackSuccess(VCurrentUser.isNewUser?.boolValue ?? false)
+        
+        // Fetch Current User's information
+        guard
+            let apiPath = dependencyManager.networkResources?.userFetchAPIPath,
+            let request = UserInfoRequest(apiPath: apiPath, userID: currentUser.id)
+        else {
+            let error = NSError(domain: "StoredLoginOperation-BadUserFetchAPIPath", code: -1, userInfo: ["DependencyManager": dependencyManager])
+            Log.warning("Unable to initialize first user info fetch during LoginSuccessOperation with error: \(error)")
+            return .failure(error)
+        }
+        
+        let operation = RequestOperation(request: request)
+        operation.after(self).queue { result in
+            switch result {
+                case .success(let user):
+                    VCurrentUser.update(to: user)
+                case .failure(let error):
+                    Log.warning("User info fetch failed with error: \(error)")
+                case .cancelled:
+                    break
+            }
+        }
+        
         return .success()
     }
     
