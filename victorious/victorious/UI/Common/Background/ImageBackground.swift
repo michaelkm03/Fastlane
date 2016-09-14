3058df62ceb06227b79bfc67a59d2a98bcc4f181
Vincent Ho <hovincen@usc.edu>
2016-09-14 16:30:08 -0700

    add image background

---
+++ victorious/victorious/UI/Common/Background/ImageBackground.swift
@@ -0,0 +1,24 @@
+//
+//  ImageBackground.swift
+//  victorious
+//
+//  Created by Vincent Ho on 9/14/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
+
+class ImageBackground: VBackground {
+    private let backgroundImage: UIImage?
+    private static let BackgroundImageKey = "image"
+    
+    required init(dependencyManager: VDependencyManager) {
+        backgroundImage = dependencyManager.imageForKey(ImageBackground.BackgroundImageKey)
+        super.init()
+    }
+    
+    override func viewForBackground() -> UIView {
+        let backgroundImageView = UIImageView(image: backgroundImage)
+        return backgroundImageView
+    }
+}
