39b79f3b6d3739fba25976f236e7c4492851c1f7
Sebastian Nystorm <abeansits@gmail.com>
2016-09-21 19:01:53 -0700

    Added an extension on NSData to detect image types.

---
+++ victorious/victorious/Categories/NSData.swift
@@ -0,0 +1,52 @@
+//
+//  NSData.swift
+//  victorious
+//
+//  Created by Sebastian Nystorm on 21/9/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
+
+extension NSData {
+
+    ///
+    /// This function will do it's best with reasonable amounts of resources to guess the content type of the possible image.
+    /// Supported formats are:
+    /// - jpeg
+    /// - png
+    /// - gif
+    /// - tiff
+    ///
+    /// NOTE: Calling this function on an NSData not generated from an image could give unexpected results.
+    ///
+    func imageType() -> (contentType: String, fileExtension: String)? {
+        var byte: UInt8 = 0
+        getBytes(&byte, length: sizeof(UInt8))
+
+        var contentType: String?
+        var fileExtension: String?
+
+        switch byte {
+            case 0xFF:
+                contentType = "image/jpeg"
+                fileExtension = "jpeg"
+            case 0x89:
+                contentType = "image/png"
+                fileExtension = "png"
+            case 0x47:
+                contentType = "image/gif"
+                fileExtension = "gif"
+            case 0x49, 0x4D:
+                contentType = "image/tiff"
+                fileExtension = "tiff"
+            default: ()
+        }
+
+        if let contentType = contentType, let fileExtension = fileExtension {
+            return (contentType: contentType, fileExtension: fileExtension)
+        }
+
+        return nil
+    }
+}
