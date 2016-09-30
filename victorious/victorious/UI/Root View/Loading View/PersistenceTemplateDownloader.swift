4d10f96daf837f3cf4f7ada12ddd24ff982d38da
Jarod Long <git@lng.la>
2016-09-29 17:59:43 -0700

    Updated PersistenceTemplateDownloader for Swift 3.

--- victorious/victorious/UI/Root View/Loading View/PersistenceTemplateDownloader.swift
+++ victorious/victorious/UI/Root View/Loading View/PersistenceTemplateDownloader.swift
@@ -10,12 +10,12 @@ import Foundation
 import VictoriousIOSSDK
 
 class PersistenceTemplateDownloader: NSObject, VTemplateDownloader {
+    private let requestExecutor = MainRequestExecutor()
     
-    fileprivate let requestExecutor = MainRequestExecutor()
-    
-    func downloadTemplateWithCompletion(_ completion: VTemplateDownloaderCompletion) {
+    func downloadTemplate(completion: @escaping VTemplateDownloaderCompletion) {
         let templateRequest = TemplateRequest()
-        dispatch_get_global_queue(DispatchQueue.GlobalQueuePriority.default, 0).async {
+        
+        DispatchQueue.global().async {
             self.requestExecutor.executeRequest(templateRequest,
                 onComplete: { templateData in
                     completion(templateData, nil)
