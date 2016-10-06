adb5512cc56ccc6f1cf69661c726e6290ae8882e
Alex Tamoykin <alex@rigthbalance.io>
2016-10-05 18:15:30 -0700

    Document ListMenuSectionDataSource

--- victorious/victorious/UI/List Menu/ListMenuSectionDataSource.swift
+++ victorious/victorious/UI/List Menu/ListMenuSectionDataSource.swift
@@ -14,19 +14,34 @@ enum ListMenuDataSourceState {
     case noContent
 }
 
+/// A generic data source for a section in ListMenu. You'll need to specialize it with two types:
+/// - Item: Item that it manages
+/// - Operation: Any operation that conforms to Queueable
 class ListMenuSectionDataSource<Item, Operation: Queueable> {
 
     // MARK: - Initialization
     typealias CellConfigurationCallback = (cell: ListMenuSectionCell, item: Item) -> Void
     typealias CreateOperationCallback = () -> Operation?
     typealias ProcessOutputCallback = (output: Operation.Output) -> [Item]
+    /// Used for configing the cell after it's creation
     let cellConfiguration: CellConfigurationCallback
+    /// Defines how an operation should be created
     let createOperation: CreateOperationCallback
+    /// Defines how operation output should be processed into items
     let processOutput: ProcessOutputCallback
+    /// The section that this data source represents
     let section: ListMenuSection
+    /// The delegate to be notified when data get updated
     weak var delegate: ListMenuSectionDataSourceDelegate?
+    /// The current state of the data source based on its results
     var state: ListMenuDataSourceState = .loading
 
+    /// Initializer
+    /// - parameter dependencyManager: A dependencyManager for a section
+    /// - parameter cellConfiguration: This callback will be called to configure a cell while dequeueing it.
+    /// - parameter createOperation: Specify how your operation need to be created
+    /// - parameter processOutput: This block defines how operation output is parsed to a list of items
+    /// - parameter section: section which this data source represents
     init(dependencyManager: VDependencyManager, cellConfiguration: CellConfigurationCallback, createOperation: CreateOperationCallback, processOutput: ProcessOutputCallback, section: ListMenuSection) {
         self.dependencyManager = dependencyManager
         self.cellConfiguration = cellConfiguration
@@ -37,18 +52,22 @@ class ListMenuSectionDataSource<Item, Operation: Queueable> {
 
     // MARK - Dependency manager
 
+    /// The data source's dependency manager
     let dependencyManager: VDependencyManager
 
+    /// APIPath for a stream behind the item
     var streamAPIPath: APIPath {
         return dependencyManager.apiPathForKey("streamURL") ?? APIPath(templatePath: "")
     }
 
+    /// Tracking APIPaths
     var streamTrackingAPIPaths: [APIPath] {
         return dependencyManager.trackingAPIPaths(forEventKey: "view") ?? []
     }
 
     // MARK: - Cell Lifecycle
 
+    /// Dequeue a cell that represents an item with data
     func dequeueItemCell(from collectionView: UICollectionView, at indexPath: NSIndexPath) -> ListMenuSectionCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(ListMenuSectionCell.defaultReuseIdentifier, forIndexPath: indexPath) as! ListMenuSectionCell
         cellConfiguration(cell: cell, item: visibleItems[indexPath.row])
@@ -56,11 +75,13 @@ class ListMenuSectionDataSource<Item, Operation: Queueable> {
         return cell
     }
 
+    /// Performs all work necessary after initialization to get the data source ready for use
     func setupDataSource(with delegate: ListMenuSectionDataSourceDelegate) {
         self.delegate = delegate
         fetchData()
     }
 
+    /// Kick off an operation to fetch data and fill `visibleItems`
     func fetchData(success success: ((with: [Item]) -> Void)? = nil, failure: ((with: ErrorType) -> Void)? = nil, cancelled: (Void -> Void)? = nil) {
         let operation = createOperation()
         operation?.queue() { [weak self] result in
@@ -90,6 +111,7 @@ class ListMenuSectionDataSource<Item, Operation: Queueable> {
         }
     }
 
+    /// The visible items fetched from backend and should be displayed
     var visibleItems: [Item] = [Item]() {
         didSet {
             state = visibleItems.isEmpty ? .noContent : .items
@@ -97,6 +119,7 @@ class ListMenuSectionDataSource<Item, Operation: Queueable> {
         }
     }
 
+    /// The number of items to show for this data source, based on its `state` and `visibleItems`
     var numberOfItems: Int {
         switch state {
         case .loading, .failed, .noContent:
