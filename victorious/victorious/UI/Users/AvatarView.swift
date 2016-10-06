03f8283bf00c4e246432cc788ac9146cb1c8bfbe
Sky Lan <4dreams.sky@gmail.com>
2016-10-06 13:15:58 -0700

    Use size.value instead of bounds.size.

--- victorious/victorious/UI/Users/AvatarView.swift
+++ victorious/victorious/UI/Users/AvatarView.swift
@@ -282,15 +282,14 @@ class AvatarView: UIView {
         
         imageView.image = nil
         imageView.backgroundColor = user?.color
-        let size = bounds.size
         
-        if let imageAsset = user?.previewImage(ofMinimumSize: size) {
+        let requestedImageSize = size.value
+        
+        if let imageAsset = user?.previewImage(ofMinimumSize: requestedImageSize) {
             imageView.getImageAsset(imageAsset) { [weak self] result in
                 switch result {
                     case .success(let image):
-                        guard
-                            let strongSelf = self, strongSelf.user?.previewImage(ofMinimumSize: size)?.url == imageAsset.url
-                        else {
+                        guard self?.user?.previewImage(ofMinimumSize: requestedImageSize)?.url == imageAsset.url else {
                             return
                         }
                         
