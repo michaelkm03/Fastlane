9b09a970e43c9e6c7a83a05e292a5b9ac9396ea8
Jarod Long <git@lng.la>
2016-10-10 16:08:03 -0700

    Uses new LocalizedError type for user validation.

--- victorious/VictoriousIOSSDK/Models/User+Validation.swift
+++ victorious/VictoriousIOSSDK/Models/User+Validation.swift
@@ -23,20 +23,15 @@ extension User {
         let trimmedUsername = username.trimmingCharacters(in: .whitespacesAndNewlines)
         
         guard !trimmedUsername.isEmpty else {
-            return validationError(withDescription: NSLocalizedString("EmptyUsername", comment: ""))
+            return UsernameValidationError.empty
         }
         
         guard trimmedUsername.isValidUserName else {
-            return validationError(withDescription: {
-                switch errorFormat {
-                    case .long: return NSLocalizedString("InvalidUsernameCharactersLong", comment: "")
-                    case .short: return NSLocalizedString("InvalidUsernameCharactersShort", comment: "")
-                }
-            }())
+            return UsernameValidationError.invalidCharacters(format: errorFormat)
         }
         
         guard trimmedUsername.characters.count <= Constants.maxUsernameLength else {
-            return validationError(withDescription: NSLocalizedString("UsernameTooLong", comment: ""))
+            return UsernameValidationError.tooLong
         }
         
         return nil
@@ -46,25 +41,11 @@ extension User {
         let trimmedDisplayName = displayName.trimmingCharacters(in: .whitespacesAndNewlines)
         
         guard trimmedDisplayName.characters.count <= Constants.maxDisplayNameLength else {
-            return validationError(withDescription: NSLocalizedString("DisplayNameTooLong", comment: ""))
+            return DisplayNameValidationError.tooLong
         }
         
         return nil
     }
-    
-    // MARK: - Generating errors
-    
-    private static func validationError(withDescription description: String) -> Error {
-        // FUTURE: We should create `LocalizedError` enums for each validated property once we move to Swift 3.
-        return NSError(domain: "UserValidationError", code: -1, userInfo: [
-            NSLocalizedDescriptionKey: description
-        ])
-    }
-}
-
-/// An enum for different formats of error messages returned from validating `User` properties.
-public enum UserValidationErrorFormat {
-    case long, short
 }
 
 private extension String {
@@ -73,5 +54,42 @@ private extension String {
         let regex = "\\A\\w+\\z"
         let test = NSPredicate(format:"SELF MATCHES %@", regex)
         return test.evaluate(with: self)
+    }
+}
+
+/// An enum for different formats of error messages returned from validating `User` properties.
+public enum UserValidationErrorFormat {
+    case long, short
+}
+
+/// Errors that can occur when validating a username.
+public enum UsernameValidationError: LocalizedError {
+    case empty
+    case tooLong
+    case alreadyTaken
+    case invalidCharacters(format: UserValidationErrorFormat)
+    
+    public var errorDescription: String? {
+        switch self {
+            case .empty: return NSLocalizedString("EmptyUsername", comment: "")
+            case .tooLong: return NSLocalizedString("UsernameTooLong", comment: "")
+            case .alreadyTaken: return NSLocalizedString("UsernameTaken", comment: "")
+            case .invalidCharacters(let format):
+                switch format {
+                    case .short: return NSLocalizedString("InvalidUsernameCharactersShort", comment: "")
+                    case .long: return NSLocalizedString("InvalidUsernameCharactersLong", comment: "")
+                }
+        }
+    }
+}
+
+/// Errors that can occur when validating a display name.
+public enum DisplayNameValidationError: LocalizedError {
+    case tooLong
+    
+    public var errorDescription: String? {
+        switch self {
+            case .tooLong: return NSLocalizedString("DisplayNameTooLong", comment: "")
+        }
     }
 }
