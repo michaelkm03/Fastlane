ab5a87a8349cb2588ee412d4e928e8455011dd08
Alex Tamoykin <alex@rigthbalance.io>
2016-10-17 17:20:09 -0700

    Fix provisioning profiles and add a test stub

---
+++ victorious/victoriousUITests/UI/Signup and Login/SignupUITests.swift
@@ -0,0 +1,14 @@
+//
+//  SignupUITests.swift
+//  victorious
+//
+//  Created by Alex Tamoykin on 10/17/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import XCTest
+
+class SignupUITests: XCTestCase {
+    func testEmailAndPasswordSignup() {
+    }
+}
