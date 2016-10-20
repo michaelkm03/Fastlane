31247e99507e6f6d04bfe2f1ecc27b23f92ce02c
Vincent Ho <vincent.ho.1994@gmail.com>
2016-10-20 13:54:28 -0700

    add CreatorQuestionResponseController

---
+++ victorious/victorious/UI/Forum/Q&A/CreatorQuestionResponseController.swift
@@ -0,0 +1,18 @@
+//
+//  CreatorQuestionResponseController.swift
+//  victorious
+//
+//  Created by Vincent Ho on 10/20/16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import Foundation
+import VictoriousIOSSDK
+
+class CreatorQuestionResponseController {
+    private let dependencyManager: VDependencyManager
+    
+    init(dependencyManager: VDependencyManager) {
+        self.dependencyManager = dependencyManager
+    }
+}
